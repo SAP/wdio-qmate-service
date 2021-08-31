36cb8571f0e313b2c992e9f40375b2730a599a69
Margarita Krivorot <margarita.krivorot@sap.com>
2021-08-31 19:13:54 +0300

    Fix and adjust navigation tests. Separate 'static' tests

--- tests/reuseApi/tests/reuse/ui5/specs/navigation/navigateToApplicationAndRetryRefresh.spec.js
+++ tests/reuseApi/tests/reuse/ui5/specs/navigation/navigateToApplicationAndRetryRefresh.spec.js
@@ -1,40 +1,46 @@
 "use strict";
+const queryToClosePopups = "help-readCatalog=false&help-stateUACP=PRODUCTION"; // from private function 'generateUrlParams'
 
-describe("navigation - navigateToApplicationAndRetryRefresh (s4)", function () {
+describe("navigation - navigateToApplicationAndRetryRefresh with preventPopups=false", function () {
   it("Preparation", async function () {
+    // First navigation - to #Shell-home
+    // "http://localhost:34099/ui?help-readCatalog=false&help-stateUACP=PRODUCTION#Shell-home" - as preventPopups=true
+
     await ui5.common.navigation.navigateToApplicationAndRetryRefresh("Shell-home", false);
+    const urlExpected = `${await utilities.browser.getBaseUrl()}#Shell-home`;
+    await ui5.common.assertion.expectUrlToBe(urlExpected);
   });
 
   it("Execution", async function () {
+    // Second navigation - to #PurchaseOrder-manage
     await ui5.common.navigation.navigateToApplicationAndRetryRefresh("PurchaseOrder-manage", false);
   });
 
   it("Verification", async function () {
-    // Note: currentUrl can contain system specific query params
-    const currentUrl = await browser.getUrl();
-    expect(currentUrl).toContain("#PurchaseOrder-manage");
-    expect(currentUrl).toContain(browser.config.baseUrl);
-  });
-
-  it("Clean Up", async function () {
-
-    // Reset baseUrl from config file
-    browser.config.baseUrl = "https://sapui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html";
+    const urlExpected = `${await utilities.browser.getBaseUrl()}#PurchaseOrder-manage`;
+    await ui5.common.assertion.expectUrlToBe(urlExpected);
   });
 });
 
-describe("navigation - navigateToApplicationAndRetryRefresh (demo url)", function () {
-  const scannerIntent = "/category/SC";
-  const graphicsCardIntent = "/category/GC";
+describe("navigation - navigateToApplicationAndRetryRefresh with preventPopups=true", function () {
+  it("Preparation", async function () {
+    // First navigation - to #Shell-home
+    // "http://localhost:34099/ui?help-readCatalog=false&help-stateUACP=PRODUCTION#Shell-home" - as preventPopups=true
 
-  it("Execution and Verification", async function () {
-    await ui5.common.navigation.navigateToApplicationAndRetryRefresh(scannerIntent, false);
+    await ui5.common.navigation.navigateToApplicationAndRetryRefresh("Shell-home"); // preventPopups = true by default
+    const urlExpected = `${await utilities.browser.getBaseUrl()}?${queryToClosePopups}#Shell-home`;
 
-    await ui5.common.assertion.expectUrlToBe(`${browser.config.baseUrl}#${scannerIntent}`);
+    await ui5.common.assertion.expectUrlToBe(urlExpected);
+  });
 
-    await ui5.common.navigation.navigateToApplicationAndRetryRefresh(graphicsCardIntent, false);
+  it("Execution", async function () {
+    // Second navigation - to #PurchaseOrder-manage
+    await ui5.common.navigation.navigateToApplicationAndRetryRefresh("PurchaseOrder-manage");
+  });
 
-    await ui5.common.assertion.expectUrlToBe(`${browser.config.baseUrl}#${graphicsCardIntent}`);
+  it("Verification", async function () {
+    const urlExpected = `${await utilities.browser.getBaseUrl()}?${queryToClosePopups}#PurchaseOrder-manage`;
+    await ui5.common.assertion.expectUrlToBe(urlExpected);
   });
 });
 
@@ -42,18 +48,14 @@ describe("navigation - navigateToApplicationAndRetryRefresh wrong navigation int
   const wrongApplication = { strange: "intent" };
   const application = "Shell-home";
 
-  it("Preparation", async function () {
-    browser.config.baseUrl = "http://localhost:34099/ui";
-  });
-
   it("Execution and Verification", async function () {
     await ui5.common.navigation.navigateToApplicationAndRetryRefresh(application, false);
 
-    await ui5.common.navigation.navigateToApplicationAndRetryRefresh(wrongApplication, false, false); // verify = false
-    const currentUrl = await browser.getUrl();
-    expect(currentUrl).toContain(browser.config.baseUrl); // check you are still at the qs9-715 page
+    await ui5.common.navigation.navigateToApplicationAndRetryRefresh(wrongApplication, false, false); // preventPopups = false, verify = false
+    const urlExpected = `${await utilities.browser.getBaseUrl()}#[object%20Object]`;
+    await ui5.common.assertion.expectUrlToBe(urlExpected);
 
-    await expect(ui5.common.navigation.navigateToApplicationAndRetryRefresh(wrongApplication, false)) // verify = true by default
+    await expect(ui5.common.navigation.navigateToApplicationAndRetryRefresh(wrongApplication, false)) // preventPopups = false, verify = true by default
       .rejects.toThrow(/Navigation failed/);
   });
 });
--- tests/reuseApi/tests/reuse/ui5/specs/navigation/navigateToApplicationWithQueryParamsAndRetry.spec.js
+++ tests/reuseApi/tests/reuse/ui5/specs/navigation/navigateToApplicationWithQueryParamsAndRetry.spec.js
@@ -1,5 +1,4 @@
 "use strict";
-const queryToAvoidPopups = "help-readCatalog=false&help-stateUACP=PRODUCTION"; // 'navigateToApplication()' method will provide you special url to avoid pop-ups.
 
 describe("navigation - navigateToApplicationWithQueryParamsAndRetry with query param in url", function () {
   const query = "?sap-language=RU";
@@ -7,42 +6,42 @@ describe("navigation - navigateToApplicationWithQueryParamsAndRetry with query p
 
   it("Preparation", async function () {
     await ui5.common.navigation.navigateToApplication("Shell-home", false);
+    const urlExpected = `${await utilities.browser.getBaseUrl()}#Shell-home`;
+    await ui5.common.assertion.expectUrlToBe(urlExpected);
   });
 
   it("Execution", async function () {
-    await ui5.common.navigation.navigateToApplicationWithQueryParamsAndRetry(intent, `${query}&${queryToAvoidPopups}`, false);
+    await ui5.common.navigation.navigateToApplicationWithQueryParamsAndRetry(intent, query, false);
   });
 
   it("Verification", async function () {
-    // Note: currentUrl can contain system specific query params
-    const currentUrl = await browser.getUrl();
-    expect(currentUrl).toContain(intent);
-    expect(currentUrl).toContain(query);
-    expect(currentUrl).toContain(browser.config.baseUrl);
+    const urlExpected = `${await utilities.browser.getBaseUrl()}${query}#${intent}`;
+    await ui5.common.assertion.expectUrlToBe(urlExpected);
   });
 });
 
-describe("navigation - navigateToApplicationWithQueryParamsAndRetry with non-existing param in url", function () {
-  const query = "?unknownParam=value";
+describe("navigation - navigateToApplicationWithQueryParamsAndRetry with wrong param in url", function () {
+  const query = "unknownParam=value"; // query without '?' mark
   const intent = "PurchaseOrder-manage";
 
   it("Preparation", async function () {
-    await ui5.common.navigation.navigateToApplication(intent, false);
+    await ui5.common.navigation.navigateToApplication("Shell-home", false);
+    const urlExpected = `${await utilities.browser.getBaseUrl()}#Shell-home`;
+    await ui5.common.assertion.expectUrlToBe(urlExpected);
   });
 
   it("Execution", async function () {
-    await ui5.common.navigation.navigateToApplicationWithQueryParamsAndRetry(intent, `${query}&${queryToAvoidPopups}`, false);
+    await ui5.common.navigation.navigateToApplicationWithQueryParamsAndRetry(intent, query, false);
   });
 
 
   it("Verification", async function () {
-    // Note: currentUrl can contain system specific query params
-    const currentUrl = await browser.getUrl();
+    const urlExpected = `${await utilities.browser.getBaseUrl()}${query}#${intent}`;
+    await ui5.common.assertion.expectUrlToBe(urlExpected);
 
-    // No errors
-    expect(currentUrl).toContain(query);
-    expect(currentUrl).toContain(intent);
-    expect(currentUrl).toContain(browser.config.baseUrl);
+    // if 'query' includes no "?", url will be interpreted as an another (unsupported) mount
+    await expect(non_ui5.common.locator.getElementById("parseUrl"))
+      .rejects.toThrow(/Element with id "parseUrl" not found/);
   });
 });
 
@@ -50,18 +49,16 @@ describe("navigation - navigateToApplicationWithQueryParamsAndRetry with empty p
   const intent = "PurchaseOrder-manage";
 
   it("Preparation", async function () {
-    await ui5.common.navigation.navigateToApplication("Shell-home", false);
+    await ui5.common.navigation.navigateToApplication("Shell-home");
   });
 
   it("Execution", async function () {
     await ui5.common.navigation.navigateToApplicationWithQueryParamsAndRetry(intent, "", false);
   });
 
-
   it("Verification", async function () {
-    // Note: currentUrl can contain system specific query params
-    const currentUrl = await browser.getUrl();
-    expect(currentUrl).toContain(intent);
-    expect(currentUrl).toContain(browser.config.baseUrl);
+    // No query
+    const urlExpected = `${await utilities.browser.getBaseUrl()}#${intent}`;
+    await ui5.common.assertion.expectUrlToBe(urlExpected);
   });
 });
