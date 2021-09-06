1fbeec3352e2ea247ee28680c32ef6c91058627c
D056896 <marvin.gruessinger@sap.com>
2021-09-06 16:48:38 +0200

    refactor: non_ui5.common.navigation and non_ui5.common.locator (2/2)

--- reuse/modules/common/navigation.js
+++ reuse/modules/common/navigation.js
@@ -12,8 +12,25 @@ const Navigation = function () {
    * @example await common.navigation.navigateToUrl("www.sap.com");
    */
   this.navigateToUrl = async function (url) {
-    await browser.navigateTo(url);
-    await this.printCurrentUrl();
+    if (url) {
+      await browser.navigateTo(url);
+      await this.printCurrentUrl();
+    } else {
+      throw new Error("Function 'navigateToUrl' failed: Please provide an url as argument.");
+    }
+  };
+
+  /**
+  * @function navigateToUrlAndRetry
+  * @memberOf common.navigation
+  * @description Navigates to the passed url and retries the function in case of a failure.
+  * @param {String} url - The url to navigate to.
+  * @param {Integer} [retries=3] - The number of retries, can be set in config for all functions under params stepsRetries.
+  * @param {Integer} [interval=5000] - The interval of the retries (ms), can be set in config for all functions under params stepRetriesIntervals.
+  * @example await common.navigation.navigateToUrlAndRetry("www.sap.com");
+  */
+  this.navigateToUrlAndRetry = async function (url, retries = 3, interval = 5000) {
+    return await utilities.function.retry(this.navigateToUrl, [url], retries, interval, this);
   };
 
   /**
--- scripts/reuse/nonUi5/common/modules/navigation.js
+++ scripts/reuse/nonUi5/common/modules/navigation.js
@@ -4,36 +4,6 @@
 */
 var Navigation = function () {
 
-  /**
-  * @function navigateToUrl
-  * @memberOf non_ui5.common.navigation
-  * @description Navigates to the passed url.
-  * @param {String} url - The url to navigate to.
-  * @example await non_ui5.common.navigation.navigateToUrl("www.ariba.com");
-  */
-  this.navigateToUrl = async function (url) {
-    if (url) {
-      await browser.navigateTo(url);
-      const urlLocalHost = await browser.getUrl();
-      console.log("Current URL after navigation-->" + urlLocalHost);
-    } else {
-      throw new Error("Please provide an URL as first argument (must be of type 'string').");
-    }
-  };
-
-  /**
-  * @function navigateToUrlAndRetry
-  * @memberOf non_ui5.common.navigation
-  * @description Navigates to the passed url.
-  * @param {String} url - The url to navigate to.
-  * @param {Integer} retries - The number of retries, can be set in config for all functions under params stepsRetries. Default is 3 times.
-  * @param {Integer} interval - The interval of the retries, can be set in config for all functions under params stepRetriesIntervals. Default is 5 secs.
-  * @example await non_ui5.common.navigation.navigateToUrlAndRetry("www.ariba.com");
-  */
-  this.navigateToUrlAndRetry = async function (url, retries, interval) {
-    return await utilities.function.retry(this.navigateToUrl, [url], retries, interval, this);
-  };
-
   // Note: missed methods getQmatePath and navigateToQmatePath were skipped
 };
 module.exports = new Navigation();
