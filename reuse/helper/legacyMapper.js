b4cd0f8c50f177e577eba2f91b6564b9be59b712
I503232 <benjamin.warth@sap.com>
2021-09-09 15:45:10 +0200

    fix logic of legacyMapper

--- reuse/helper/legacyMapper.js
+++ reuse/helper/legacyMapper.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const utils = require("./utils");
 
 module.exports = function mapOldNamespacesToNewNamespaces() {
@@ -7,48 +6,61 @@ module.exports = function mapOldNamespacesToNewNamespaces() {
     const currentObject = legacyMappingObjects[i];
     const oldNamespace = currentObject.old;
     const newNamespace = currentObject.new;
-    setGlobalValue(oldNamespace, getGlobalValue(newNamespace), newNamespace);
+    setGlobal(oldNamespace, newNamespace);
   }
 };
 
-function getGlobalValue(namespace) {
+function getGlobalObjectFor(namespace) {
   const namespaceParts = namespace.split(".");
   let currentGlobalValue = global;
-  for (let i = 0; i < namespaceParts.length; i++) {
-    const value = currentGlobalValue[namespaceParts[i]];
+
+  for (const i of namespaceParts) {
+    const value = currentGlobalValue[i];
     currentGlobalValue = value;
   }
+
   return currentGlobalValue;
 }
 
-function setGlobalValue(oldNamespace, value, newNamespace) {
+function assignWrapperFunction(fct, oldNamespace, newNamespace) {
+  return async function () {
+    util.console.warn(`"${oldNamespace}" is deprecated. Please use "${newNamespace}" instead.`);
+    return fct(...arguments);
+  };
+}
+
+function setGlobal(oldNamespace, newNamespace) {
+  const newGlobalObject = getGlobalObjectFor(newNamespace);
   const namespaceParts = oldNamespace.split(".");
   let currentGlobalValue = global;
-  for (let i = 0; i < namespaceParts.length; i++) {
-    if (i === namespaceParts.length - 1) {
 
-      if (typeof value === "object") {
-        const newValue = {};
-        for (const f in value) {
-          const currentFct = value[f];
-          newValue[f] = function () {
-            util.console.warn(`Namespace "${oldNamespace}" is deprecated. Please use "${newNamespace}" instead.`);
-            currentFct(arguments[0], arguments[1], arguments[2], arguments[3]);
-          };
-        }
-        currentGlobalValue[namespaceParts[i]] = newValue;
-      } else if (typeof value === "function") {
-        currentGlobalValue[namespaceParts[i]] = function () {
-          util.console.warn(`Function "${oldNamespace}" is deprecated. Please use "${newNamespace}" instead.`);
-          value(arguments[0], arguments[1], arguments[2], arguments[3]);
-        };
-      }
+  for (const i in namespaceParts) {
 
-    } else {
+    if (i < namespaceParts.length - 1) {
       if (!currentGlobalValue[namespaceParts[i]]) {
         currentGlobalValue[namespaceParts[i]] = {};
       }
       currentGlobalValue = currentGlobalValue[namespaceParts[i]];
+
+    } else {
+      if (typeof newGlobalObject === "object") {
+        const newValue = {};
+        for (const j in newGlobalObject) {
+          const currentValue = newGlobalObject[j];
+          if (typeof currentValue === "object") {
+            const innerObject = {};
+            for (const f in currentValue) {
+              innerObject[f] = assignWrapperFunction(currentValue[f], oldNamespace, newNamespace);
+            }
+            newValue[j] = innerObject;
+          } else {
+            newValue[j] = assignWrapperFunction(currentValue, oldNamespace, newNamespace);
+          }
+        }
+        currentGlobalValue[namespaceParts[i]] = newValue;
+      } else if (typeof newGlobalObject === "function") {
+        currentGlobalValue[namespaceParts[i]] = assignWrapperFunction(newGlobalObject, oldNamespace, newNamespace);
+      }
     }
   }
 }
