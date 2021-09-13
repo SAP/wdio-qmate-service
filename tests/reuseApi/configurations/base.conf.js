689e679d2064ebef39972e12b63d89864c42bd9a
Margarita Krivorot <margarita.krivorot@sap.com>
2021-09-13 18:29:07 +0300

    Fix failing staticServerConfig test

--- tests/reuseApi/configurations/base.conf.js
+++ tests/reuseApi/configurations/base.conf.js
@@ -189,7 +189,7 @@ exports.config = {
   //afterCommand: function (commandName, args, result, error) {
   //},
   /**
-   * Gets executed after all tests are done. You still have access to all global constiables from
+   * Gets executed after all tests are done. You still have access to all global variables from
    * the test.
    * @param {Number} result 0 - test pass, 1 - test fail
    * @param {Array.<Object>} capabilities list of capabilities details
