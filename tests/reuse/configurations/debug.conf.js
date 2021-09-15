3ca1a651265150894c80323f2d72c61ff0abe2dc
Margarita Krivorot <margarita.krivorot@sap.com>
2021-09-15 16:29:59 +0300

    Refactor tests structure

--- tests/reuse/configurations/debug.conf.js
+++
@@ -1,9 +0,0 @@
-const merge = require("deepmerge");
-const chromeConfig = require("./chrome.conf.js");
-exports.config = merge(chromeConfig.config, {
-  // Options to be passed to Mocha.
-  // See the full list at http://mochajs.org/
-  mochaOpts: {
-    timeout: 900000,
-  },
-});
