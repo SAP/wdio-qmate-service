1f26747ad516e213218d071c2f4e31e49827aef9
I503232 <benjamin.warth@sap.com>
2021-09-16 15:44:35 +0200

    moved and refactored tests: ui5.common.formatter

--- tests/reuseApi/tests/reuse/ui5/specs/formatter/addRemoveLeadingZeros.spec.js
+++ tests/reuseApi/tests/reuse/ui5/specs/formatter/addRemoveLeadingZeros.spec.js
@@ -2,28 +2,17 @@
 
 describe("formatter - addRemoveLeadingZeros - add zeros", function () {
   it("Execution and Verification", function () {
-    const newValue = ui5.common.formatter.addRemoveLeadingZeros(10, 5);
-    ui5.common.assertion.expectEqual(newValue, "00010");
-
-    ui5.common.assertion.expectEqual(
-      newValue,
-      ui5.common.formatter.addRemoveLeadingZeros("10", 5)
-    );
-
-    ui5.common.assertion.expectEqual(
-      newValue,
-      ui5.common.formatter.addRemoveLeadingZeros(new Number(10), 5)
-    );
+    ui5.common.assertion.expectEqual(ui5.common.formatter.addRemoveLeadingZeros(10, 5), "00010");
+    ui5.common.assertion.expectEqual(ui5.common.formatter.addRemoveLeadingZeros("12", 4), "0012");
+    ui5.common.assertion.expectEqual(ui5.common.formatter.addRemoveLeadingZeros("0014", 5), "00014");
+    ui5.common.assertion.expectEqual(ui5.common.formatter.addRemoveLeadingZeros("11", 2), "11");
+    ui5.common.assertion.expectEqual(ui5.common.formatter.addRemoveLeadingZeros(new Number(10), 5), "00010");
   });
 });
 
-
 describe("formatter - addRemoveLeadingZeros - remove zeros", function () {
   it("Execution and Verification", function () {
-    const newValue = ui5.common.formatter.addRemoveLeadingZeros("0000010", 2);
-    const newShortValue = ui5.common.formatter.addRemoveLeadingZeros("0000010", 1);
-
-    ui5.common.assertion.expectEqual(newValue, "10");
-    ui5.common.assertion.expectEqual(newShortValue, "0");
+    ui5.common.assertion.expectEqual(ui5.common.formatter.addRemoveLeadingZeros("0000010", 2), "10");
+    ui5.common.assertion.expectEqual(ui5.common.formatter.addRemoveLeadingZeros("0000010", 1), "0");
   });
 });
