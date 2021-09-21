28ce96c9315f4e828dcdf64dd45f952aee8f9582
I503232 <benjamin.warth@sap.com>
2021-09-21 11:43:41 +0200

    moved and refactored tests: nonUi5.common.navigation

--- reuse/tests/common/navigation/navigateToUrl.spec.js
+++ reuse/tests/common/navigation/navigateToUrl.spec.js
@@ -1,25 +1,28 @@
-"use strict";
+describe("navigateToUrl", function () {
 
-describe("navigation - navigateToUrl", function () {
+  const url = "https://sapui5.hana.ondemand.com/";
+
   it("Execution", async function () {
-    await common.navigation.navigateToUrl(browser.config.baseUrl);
+    await common.navigation.navigateToUrl(url);
   });
 
-  it("Verification", async function () {
-    await common.assertion.expectUrlToBe(browser.config.baseUrl);
+  it("Execution and Verification", async function () {
+    await common.assertion.expectUrlToBe(url);
   });
 });
 
-describe("navigation - navigateToUrl for empty url (unhappy case)", function () {
+describe("navigateToUrl with wrong url", function () {
+
+  it("Execution and Verification", async function () {
+    await expect(common.navigation.navigateToUrl("sd"))
+      .rejects.toThrow("invalid argument");
+  });
+});
+
+describe("navigateToUrl with wrong parameter", function () {
+
   it("Execution and Verification", async function () {
     await expect(common.navigation.navigateToUrl())
       .rejects.toThrow("Function 'navigateToUrl' failed: Please provide an url as argument.");
-  });
-});
-
-describe("navigation - navigateToUrl for part of url (unhappy case)", function () {
-  it("Execution and Verification", async function () {
-    await expect(common.navigation.navigateToUrl("#/category"))
-      .rejects.toThrow(/invalid argument/);
   });
 });
