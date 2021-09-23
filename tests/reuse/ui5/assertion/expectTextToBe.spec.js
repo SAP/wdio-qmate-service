eaca71a31ef517e23861d47019a74db364b63c14
I503232 <benjamin.warth@sap.com>
2021-09-23 12:05:25 +0200

    adjusting ui5 tests: assertions 2/2

--- tests/reuse/ui5/assertion/expectTextToBe.spec.js
+++ tests/reuse/ui5/assertion/expectTextToBe.spec.js
@@ -34,6 +34,6 @@ describe("assertion - expectTextToBe with wrong selector", function () {
       .rejects.toThrow(/uiControlExecuteLocator\(\): No visible elements found/);
     wrongSelector = 123;
     await expect(ui5.assertion.expectTextToBe(wrongSelector, "Watch"))
-      .rejects.toThrow("waitUntil condition failed with the following reason: javascript error: Cannot read properties of null (reading 'getMetadata')");
+      .rejects.toThrow("Function 'expectAttributeToContain' failed:Error:");
   });
 });
--- tests/reuse/ui5/assertion/expectValueToBeDefined.spec.js
+++ tests/reuse/ui5/assertion/expectValueToBeDefined.spec.js
@@ -6,7 +6,7 @@ const {
 describe("assertion - expectValueToBeDefined", function () {
 
   it("Preparation", async function () {
-    await ui5.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/#/entity/sap.m.Input/sample/sap.m.sample.InputDescription");
+    await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/#/entity/sap.m.Input/sample/sap.m.sample.InputDescription");
     await handleCookiesConsent();
   });
 
@@ -21,11 +21,11 @@ describe("assertion - expectValueToBeDefined", function () {
     await ui5.assertion.expectValueToBeDefined(selector);
 
     await ui5.userInteraction.clearAndFill(selector, "New value");
-    await ui5.userInteraction.pressEnter();
+    await common.userInteraction.pressEnter();
     await ui5.assertion.expectValueToBeDefined(selector);
 
     await ui5.userInteraction.clearAndFill(selector, "");
-    await ui5.userInteraction.pressEnter();
+    await common.userInteraction.pressEnter();
     await expect(ui5.assertion.expectValueToBeDefined(selector)).rejects.toThrow(/Expect\w*|\d*not\w*|\d*""/);
   });
 });
