4018bbb5e079bae0b4c3b981c8cc1e617846e7ab
D056896 <marvin.gruessinger@sap.com>
2021-09-23 15:59:19 +0200

    fix namespaces

--- tests/frameworks/cucumberFramework/specs/step-definitions/click.js
+++ tests/frameworks/cucumberFramework/specs/step-definitions/click.js
@@ -8,7 +8,7 @@ const selectorForDialog = {
 };
 
 Given(/I am on opened confirmation dialog/, async () => {
-  await non_ui5.common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/#/entity/sap.m.MessageBox/sample/sap.m.sample.MessageBoxInitialFocus");
+  await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/#/entity/sap.m.MessageBox/sample/sap.m.sample.MessageBoxInitialFocus");
   await handleCookiesConsent();
 
   const openDialogButton = {
@@ -19,25 +19,25 @@ Given(/I am on opened confirmation dialog/, async () => {
     }
   };
 
-  await ui5.common.userInteraction.click(openDialogButton);
+  await ui5.userInteraction.click(openDialogButton);
 
     // Check Dialog window opened
-  await ui5.common.locator.getDisplayedElement(selectorForDialog);
+  await ui5.element.getDisplayedElement(selectorForDialog);
 });
 
 When(/I click (\w+) on confirmation dialog$/, async (button) => {
   if (button.toLowerCase() === "yes") {
-    await ui5.common.confirmationDialog.clickYes();
+    await ui5.confirmationDialog.clickYes();
   } else if (button.toLowerCase() === "no") {
-    await ui5.common.confirmationDialog.clickNo();
+    await ui5.confirmationDialog.clickNo();
   }
 });
 
 Then(/I should check that no confirmation dialog any more, error message is (.*)$/, async (message) => {
   try {
-    await ui5.common.locator.getDisplayedElement(selectorForDialog);
+    await ui5.element.getDisplayedElement(selectorForDialog);
   } catch (e) {
-    await ui5.common.assertion.expectTrue(!!e.message.match(message));
+    await common.assertion.expectTrue(!!e.message.match(message));
   }
 });
 
