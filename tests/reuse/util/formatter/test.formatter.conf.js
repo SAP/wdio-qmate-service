53868239fd0d9ea34f120ad2a7d7bf1bf112370b
Uladzislau Dauhashei <uladzislau.dauhashei@sap.com>
2021-09-30 17:08:56 +0300

    added spec for sliceStringAt

---
+++ tests/reuse/util/formatter/test.formatter.conf.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const merge = require("deepmerge");
+const qmateConfig = require("../../../helper/configurations/chrome.headless.conf");
+exports.config = merge(qmateConfig.config, {
+  maxInstances: 6,
+  bail: 1,
+  baseUrl: "https://sapui5.hana.ondemand.com/",
+
+  specs: [
+    path.resolve(__dirname, "sliceStringAt.spec.js")
+  ]
+});
