5bb51d7eb651d8ca39767f7339ebf4b75dd22bad
I503232 <benjamin.warth@sap.com>
2021-10-07 16:25:22 +0200

    fix reuse configs

--- tests/reuse/nonUi5/element/test.element.conf.js
+++ tests/reuse/nonUi5/element/test.element.conf.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const merge = require("deepmerge");
-const qmateConfig = require("../../configurations/chrome.headless.conf.js");
-exports.config = merge(qmateConfig.config, {
+const qmateConfiguration = require("../../../helper/configurations/chrome.headless.conf");
+exports.config = merge(qmateConfiguration.config, {
   maxInstances: 6,
   bail: 1,
   baseUrl: "https://sapui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html",
