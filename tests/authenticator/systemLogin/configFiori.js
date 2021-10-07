60dade5d6686ebf06a8c497d390becf1071b939b
I503232 <benjamin.warth@sap.com>
2021-10-07 17:11:44 +0200

    move and fix authenticator tests

---
+++ tests/authenticator/systemLogin/configFiori.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const merge = require("deepmerge");
+const fioriConfig = require("../../helper/configurations/chrome.headless.conf.js");
+exports.config = merge(fioriConfig.config, {
+  maxInstances: 1,
+  bail: 1,
+
+  specs: [
+    path.resolve(__dirname, "specs/fiori.spec.js")
+  ],
+  exclude: [],
+
+  reporters: ["spec"],
+  params: {
+    auth: {
+      formType: "fiori-form",
+      username: "PURCHASER",
+      password: "super-duper-sensitive-pw"
+    }
+  },
+
+  baseUrl: "https://super-sensitive.domain.name/ui"
+
+});
--- tests/legacyReuse/authenticator/staticLogin/specs/custom.spec.js
+++
@@ -1,6 +0,0 @@
-describe("authenticator - custom", function () {
-  it("Verification", async function () {
-    const elem = await non_ui5.common.locator.getElementById("shellAppTitle");
-    await non_ui5.common.assertion.expectToBeVisible(elem);
-  });
-});
