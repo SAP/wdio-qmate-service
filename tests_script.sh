fec4038f7d7a7402bdfee44f8bda136c4833e206
Margarita Krivorot <margarita.krivorot@sap.com>
2021-10-11 18:43:43 +0300

    Remove npm i from script

--- tests_script.sh
+++ tests_script.sh
@@ -2,8 +2,6 @@ echo "1. Run 'npm i' in root folder"
 npm config set unsafe-perm true
 export CHROMEDRIVER_PORT=4444
 export CHROMEDRIVER_FILEPATH=/usr/bin/chromedriver
-npm i
-npm run chromedriver-update
 echo ''
 echo "2. set environment variables for chromedriver"
 
