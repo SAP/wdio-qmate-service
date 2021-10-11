c245bf3ed2376c2405128a76e92449fd557e9467
Margarita Krivorot <margarita.krivorot@sap.com>
2021-10-11 19:14:35 +0300

    Revert log level and test_script

--- tests/helper/configurations/base.conf.js
+++ tests/helper/configurations/base.conf.js
@@ -37,7 +37,7 @@ exports.config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: "silent",
+  logLevel: "error",
   //
   // Set specific log levels per logger
   // loggers:
