dc66aae93e51bf7f0f45c6a957934c2c237f190b
Margarita Krivorot <margarita.krivorot@sap.com>
2021-10-11 18:24:45 +0300

    Fix test script

--- tests_script.sh
+++ tests_script.sh
@@ -1,9 +1,11 @@
-echo "1. Run 'npm ci' in root folder"
-npm ci
+echo "1. Run 'npm i' in root folder"
+npm i
 echo ''
 echo "2. set environment variables for chromedriver"
 export CHROMEDRIVER_PORT=4444
 export CHROMEDRIVER_FILEPATH=/usr/bin/chromedriver
 echo "Run tests"
-
-wdio tests/reuse/nonUi5/assertion/test.assertion.conf.js
+node -v
+npm -v
+npm i npx
+npx wdio ./tests/reuse/nonUi5/assertion/test.assertion.conf.js
