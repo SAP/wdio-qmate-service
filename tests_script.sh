3d97c5c330e2e0bd36c949330e09d9d78e726720
D056896 <marvin.gruessinger@sap.com>
2021-10-11 11:38:45 +0200

    enhance .sh for pipe

--- tests_script.sh
+++ tests_script.sh
@@ -1,27 +1,22 @@
 cd ./tests
-
+echo "1. set environment variables for chromedriver"
+export CHROMEDRIVER_PORT=4444
+export DBUS_SESSION_BUS_ADDRESS=/dev/null
+export OPENSSL_BIN='/usr/bin/openssl'
 echo "List test folders"
 echo
 for d in */ ; do
-    echo "$d"
+  echo "$d"
 done
 for d in */ ; do
-    echo
-    echo "$d" " test folder"
-    cd $d
+  echo
+  echo "$d" " test folder"
+  cd $d
 
-    echo "1 Install dependencies and devDependencies"
-    npm i
-
-    IS_CHROMEDRIVER_UPDATE_REQUIRED="grep 'chromedriver-update' package.json"
-
-    if [[ $IS_CHROMEDRIVER_UPDATE_REQUIRED ]]
-    then
-      echo "1.1 Run chromedriver-update script"
-      npm run chromedriver-update
-    fi
-
-    echo "2. Run tests"
-    npm run test
+  echo "2. Install dependencies and devDependencies"
+  npm i
+  
+  echo "3. Run tests"
+  npm run test
 
 done
