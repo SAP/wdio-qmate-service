bf2f9f0c7083ff9d6997e9eefd0744575a03a5d0
D056896 <marvin.gruessinger@sap.com>
2021-11-16 16:07:28 +0100

    undo changes

--- tests/reuse/ui5/assertion/test.assertion.conf.js
+++ tests/reuse/ui5/assertion/test.assertion.conf.js
@@ -2,26 +2,27 @@ const path = require("path");
 const merge = require("deepmerge");
 const qmateConfiguration = require("../../../helper/configurations/chrome.headless.conf");
 exports.config = merge(qmateConfiguration.config, {
-  // maxInstances: 6,
+  maxInstances: 6,
   bail: 1,
   baseUrl: "https://sapui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html",
+  profile: "base",
 
   specs: [
     path.resolve(__dirname, "expectAttributeToBe.spec.js"),
-    // path.resolve(__dirname, "expectValidationError.spec.js"),
-    // path.resolve(__dirname, "expectValidationSuccess.spec.js"),
-    // path.resolve(__dirname, "expectAttributeToContain.spec.js"),
-    // path.resolve(__dirname, "expectBindingPathToBe.spec.js"),
-    // path.resolve(__dirname, "expectBindingContextPathToBe.spec.js"),
-    // path.resolve(__dirname, "expectTextToBe.spec.js"),
-    // path.resolve(__dirname, "expectValueToBe.spec.js"),
-    // path.resolve(__dirname, "expectValueToBeDefined.spec.js"),
-    // path.resolve(__dirname, "expectToBeNotEnabled.spec.js"),
-    // path.resolve(__dirname, "expectToBeEnabled.spec.js"),
-    // path.resolve(__dirname, "expectToBeVisible.spec.js"),
-    // path.resolve(__dirname, "expectToBeNotVisible.spec.js"),
-    // path.resolve(__dirname, "expectToBeVisibleInViewport.spec.js"),
-    // path.resolve(__dirname, "expectMessageToastTextToBe.spec.js")
+    path.resolve(__dirname, "expectValidationError.spec.js"),
+    path.resolve(__dirname, "expectValidationSuccess.spec.js"),
+    path.resolve(__dirname, "expectAttributeToContain.spec.js"),
+    path.resolve(__dirname, "expectBindingPathToBe.spec.js"),
+    path.resolve(__dirname, "expectBindingContextPathToBe.spec.js"),
+    path.resolve(__dirname, "expectTextToBe.spec.js"),
+    path.resolve(__dirname, "expectValueToBe.spec.js"),
+    path.resolve(__dirname, "expectValueToBeDefined.spec.js"),
+    path.resolve(__dirname, "expectToBeNotEnabled.spec.js"),
+    path.resolve(__dirname, "expectToBeEnabled.spec.js"),
+    path.resolve(__dirname, "expectToBeVisible.spec.js"),
+    path.resolve(__dirname, "expectToBeNotVisible.spec.js"),
+    path.resolve(__dirname, "expectToBeVisibleInViewport.spec.js"),
+    path.resolve(__dirname, "expectMessageToastTextToBe.spec.js")
   ],
 
   exclude: [],
