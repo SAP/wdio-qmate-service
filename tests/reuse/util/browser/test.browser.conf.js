241923d0a803b8de73a03810608466a4f45ab03d
Herald Antony <herald.maria.antony@sap.com>
2021-11-23 16:56:58 +0530

    add util.browser.getCurrentUrl method

--- tests/reuse/util/browser/test.browser.conf.js
+++ tests/reuse/util/browser/test.browser.conf.js
@@ -8,7 +8,8 @@ exports.config = merge(qmateConfiguration.config, {
   specs: [
     path.resolve(__dirname, "logCurrentUrl.spec.js"),
     path.resolve(__dirname, "getBrowserName.spec.js"),
-    path.resolve(__dirname, "getUI5Version.spec.js")
+    path.resolve(__dirname, "getUI5Version.spec.js"),
+    path.resolve(__dirname, "getCurrentUrl.spec.js")
   ],
   exclude: [],
 
