246395e005ee04674c805df08e2274abddf5441d
Herald Antony <herald.maria.antony@sap.com>
2021-12-01 14:28:02 +0530

    add missing functions to make ui5 and nonUi5 consistent

---
+++ tests/reuse/ui5/userInteraction/dragAndDrop.spec.js
@@ -0,0 +1,34 @@
+const { handleCookiesConsent } = require("../../../helper/utils");
+
+describe("userInteraction - dragAndDrop", function () {
+
+  const sourceSelector = {
+    "elementProperties": {
+      "viewName": "sap.f.sample.GridListDragAndDrop.V",
+      "metadata": "sap.f.GridListItem",
+      "bindingContextPath": "/items/0"
+    }
+  };
+
+  const targetSelector = {
+    "elementProperties": {
+      "viewName": "sap.f.sample.GridListDragAndDrop.V",
+      "metadata": "sap.f.GridListItem",
+      "bindingContextPath": "/items/5"
+    }
+  };
+
+  it("Preparation", async function () {
+    await browser.navigateTo("https://sapui5.hana.ondemand.com/sdk/#/entity/sap.f.GridList/sample/sap.f.sample.GridListDragAndDrop");
+    await handleCookiesConsent();
+  });
+
+  it("Execution", async function () {
+    await ui5.userInteraction.dragAndDrop(sourceSelector, targetSelector, 0, 0, 1000);
+  });
+
+  it("Verification", async function () {
+    // TODO
+  });
+});
+
