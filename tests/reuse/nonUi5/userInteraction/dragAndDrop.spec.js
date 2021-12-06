faef649a055ce327e4722ae5052e6d4de12b0f7d
Margarita Krivorot <margarita.krivorot@sap.com>
2021-12-06 14:35:37 +0100

    Add verbose dragAndDrop tests:
    - Add successful test for example from WDIO page
    - Add failing dragAndDrop example

--- tests/reuse/nonUi5/userInteraction/dragAndDrop.spec.js
+++ tests/reuse/nonUi5/userInteraction/dragAndDrop.spec.js
@@ -1,8 +1,28 @@
+"use strict";
+describe("userInteraction - successful dragAndDrop", async function () {
+  it("Preparation", async function () {
+    await common.navigation.navigateToUrl("http://localhost:34005/dragAndDropWdioExample.html");
+  });
 
-describe("userInteraction - dragAndDrop", async function () {
+  it("Execution", async function () {
+    const draggable = await nonUi5.element.getById("draggable");
+    const droppable = await nonUi5.element.getById("droppable");
+    const innerText = await nonUi5.element.getValue(droppable);
+    await common.assertion.expectEqual(innerText, "Drop here");
+    await nonUi5.userInteraction.dragAndDrop(draggable, droppable);
+  });
+
+  it("Verification", async function () {
+    const dropped = await nonUi5.element.getById("droppable");
+    const text = await nonUi5.element.getValue(dropped);
+    common.assertion.expectEqual(text, "Dropped!");
+  });
+});
+
+describe("userInteraction - successful dragAndDrop", async function () {
   let sourceText;
   it("Preparation", async function () {
-    await common.navigation.navigateToUrl("http://localhost:34005/dragAndDrop.html");
+    await common.navigation.navigateToUrl("http://localhost:34005/dragAndDropFailing.html");
   });
 
   it("Execution", async function () {
@@ -14,9 +34,11 @@ describe("userInteraction - dragAndDrop", async function () {
   });
 
   it("Verification", async function () {
-    //TODO - drag and drop fails
+    // NOTE - this drag and drop implementation fails.
+    // From WDIO documentation: "The functionality of this command highly depends on the way drag and drop is implemented in your app..." https://webdriver.io/docs/api/element/dragAndDrop/
     const targetElement = await nonUi5.element.getById("dropTargetDiv");
     const text = await targetElement.getText();
-    // common.assertion.expectEqual(text, sourceText);
+    common.assertion.expectEqual(text, ""); // instead of text === sourceText
   });
 });
+
