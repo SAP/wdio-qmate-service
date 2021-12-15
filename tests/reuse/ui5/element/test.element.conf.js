fa801fe4cd6342368596f387cbe25a4af8f72ef3
I503232 <benjamin.warth@sap.com>
2021-12-15 19:27:45 +0100

    fix tests

--- tests/reuse/ui5/element/test.element.conf.js
+++ tests/reuse/ui5/element/test.element.conf.js
@@ -1,28 +1,21 @@
 const path = require("path");
 const merge = require("deepmerge");
-const qmateConfiguration = require("../../../helper/configurations/chrome.conf");
+const qmateConfiguration = require("../../../helper/configurations/chrome.headless.conf");
 exports.config = merge(qmateConfiguration.config, {
   maxInstances: 6,
   bail: 1,
   baseUrl: "https://sapui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html",
 
   specs: [
-    // path.resolve(__dirname, "getBindingValue.spec.js"),
-    // path.resolve(__dirname, "getDisplayed.spec.js"),
-    // path.resolve(__dirname, "getAllDisplayed.spec.js"),
-    // path.resolve(__dirname, "getByText.spec.js"),
+    path.resolve(__dirname, "getBindingValue.spec.js"),
+    path.resolve(__dirname, "getDisplayed.spec.js"),
+    path.resolve(__dirname, "getAllDisplayed.spec.js"),
+    path.resolve(__dirname, "getByText.spec.js"),
     path.resolve(__dirname, "getByParent.spec.js"),
     path.resolve(__dirname, "getByChild.spec.js"),
-    // path.resolve(__dirname, "getId.spec.js"),
-    // path.resolve(__dirname, "getPropertyValue.spec.js"),
-    // path.resolve(__dirname, "highlight.spec.js"),
-    // path.resolve(__dirname, "isVisible.spec.js")
-  ],
-
-  exclude: [],
-
-
-  mochaOpts: {
-    timeout: 2000000,
-  },
+    path.resolve(__dirname, "getId.spec.js"),
+    path.resolve(__dirname, "getPropertyValue.spec.js"),
+    path.resolve(__dirname, "highlight.spec.js"),
+    path.resolve(__dirname, "isVisible.spec.js")
+  ]
 });
