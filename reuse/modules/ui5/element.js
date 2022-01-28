e2f681f94531d04b82ef498e78ecdaeec625f754
D056896 <marvin.gruessinger@sap.com>
2022-01-28 09:59:47 +0100

    remove uneccessary coding

--- reuse/modules/ui5/element.js
+++ reuse/modules/ui5/element.js
@@ -198,24 +198,7 @@ const Element = function () {
     try {
       let elem = await this.getDisplayed(selector, index, timeout);
       let attrValue = await elem.getUI5Property(property);
-
-      // if (!attrValue &&
-      //   selector.elementProperties &&
-      //   selector.elementProperties.metadata &&
-      //   selector.elementProperties.metadata.includes("SFBMultiInput")) {
-      //   const token = {
-      //     "elementProperties": {
-      //       "metadata": "sap.m.Token",
-      //       "ancestorProperties": selector.elementProperties
-      //     }
-      //   };
-      //   elem = await this.getDisplayed(token, index, timeout);
-      //   attrValue = await elem.getUI5Property("text");
-      //   attrValue = attrValue.replace("=", "");
-      // }
-
-      // Fallback because we always considered the data-properties rather than the native ones
-      // If this is removed it can have an impact on the behavior of the tool comparing with the predecessors
+      
       if (!attrValue) {
         attrValue = await this.getInnerAttribute(elem, "data-" + property);
       }
