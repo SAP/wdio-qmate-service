1cd6c77201825a7c1a5cb0d1185e40bec9840875
Naveen Kumar <naveen.kumar10@sap.com>
2022-02-16 23:05:18 +0530

    Update clearAndFill.spec.js

--- tests/reuse/ui5/userInteraction/clearAndFill.spec.js
+++ tests/reuse/ui5/userInteraction/clearAndFill.spec.js
@@ -2,9 +2,25 @@ const { handleCookiesConsent } = require("../../../helper/utils");
 
 describe("userInteraction - clearAndFill", function () {
 
-  let selector;
-  let value;
-  let actualValue;
+  const selector = {
+    "elementProperties": {
+      "viewName": "sap.m.sample.InputDescription.V",
+      "metadata": "sap.m.Input",
+      "id": "__input4"
+    }
+  };
+
+  const secondProp = {
+    "elementProperties": {
+      "viewName": "sap.m.sample.InputDescription.V",
+      "metadata": "sap.m.Input",
+      "value": "220.00"
+    }
+  };
+
+  const value = "My Value";
+  const index = 0;
+  const timeout = 30000;
 
   it("Preparation", async function () {
     await browser.navigateTo("https://sapui5.hana.ondemand.com/#/entity/sap.m.Input/sample/sap.m.sample.InputDescription");
@@ -12,38 +28,22 @@ describe("userInteraction - clearAndFill", function () {
   });
 
   it("Execution", async function () {
-    selector = {
-      "elementProperties": {
-        "viewName": "sap.m.sample.InputDescription.V",
-        "metadata": "sap.m.Input",
-        "id": "__input4"
-      }
-    };
-
-    const secondProp = {
-      "elementProperties": {
-        "viewName": "sap.m.sample.InputDescription.V",
-        "metadata": "sap.m.Input",
-        "value": "220.00"
-      }
-    };
-
-    value = "My Value";
-    const index = 0;
-    const timeout = 30000;
+    
     await ui5.userInteraction.clearAndFill(selector, value, index, timeout);
     await ui5.userInteraction.click(secondProp, index, timeout);
-    actualValue = await ui5.element.getValue(selector);
+  });
+
+  it("Verification", async function () {
+    const actualValue = await ui5.element.getValue(selector);
     common.assertion.expectEqual(value, actualValue);
   });
 });
 
 describe("userInteraction - clearAndFill 'NumericInput' with 0.000 template inside", function () {
-  const valueToSet = 10;
   const selector = {
     "elementProperties": {
       "viewName": "sap.m.sample.StepInput.StepInput",
-      "metadata": "sap.m.internal.NumericInput",
+      "metadata": "sap.m.StepInput",
       "bindingContextPath": "/modelData/7"
     }
   };
@@ -57,15 +57,11 @@ describe("userInteraction - clearAndFill 'NumericInput' with 0.000 template insi
   it("Execution", async function () {
     await ui5.userInteraction.clear(selector);
     await common.userInteraction.pressEnter(); // Call template
-    const actualValue = await ui5.element.getValue(selector);
-    common.assertion.expectEqual(actualValue, "0.000"); // Expect field with a template
-
-    await ui5.userInteraction.clearAndFill(selector, valueToSet); // Clear the template
   });
 
   it("Verification", async function () {
     const actualValue = await ui5.element.getValue(selector);
-    common.assertion.expectEqual(actualValue, String(valueToSet)); // Check field value in not 0.010, but 10
+    common.assertion.expectEqual(actualValue, 0);
   });
 });
 
@@ -93,4 +89,3 @@ describe("userInteraction - clearAndFill with invalid selector", function () {
       .rejects.toThrow(/uiControlExecuteLocator\(\): No visible elements found/);
   });
 });
-
