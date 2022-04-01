12629629449511acf315b6d7f166248d38d7978c
Uladzislau Holeu <uladzislau.holeu@sap.com>
2022-04-01 19:13:56 +0300

    restore perfomance namespace

--- src/reuse/modules/util/Util.ts
+++ src/reuse/modules/util/Util.ts
@@ -3,7 +3,7 @@ import console, {Console} from './console';
 import file, { File } from './file';
 import formatter, { Formatter } from './formatter';
 import functionModule, { FunctionModule } from './function';
-// import performance, {Performance} from './performance';
+import performance, {Performance} from './performance';
 import system, {System} from './system';
 
 export class Util {
@@ -12,7 +12,7 @@ export class Util {
     file: File = file
     formatter: Formatter = formatter
     function: FunctionModule = functionModule
-    // perfomance: Performance = performance
+    perfomance: Performance = performance
     system: System = system
 }
 
