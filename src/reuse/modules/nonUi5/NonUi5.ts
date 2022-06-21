a5f4c6a286d57af6fe72e31fdf234b0eb68ec432
Uladzislau Dauhashei <u.dauhashei@sap.com>
2022-06-21 14:33:46 +0200

    added nonUi5.navigation.navigateToApplication

--- src/reuse/modules/nonUi5/NonUi5.ts
+++ src/reuse/modules/nonUi5/NonUi5.ts
@@ -1,10 +1,12 @@
 import assertion, { Assertion } from "./assertion";
 import element, { ElementModule } from "./element";
+import navigation, { Navigation } from "./navigation";
 import userInteraction, { UserInteraction } from "./userInteraction";
 
 export class NonUi5 {
     assertion: Assertion = assertion
     element: ElementModule = element
+    navigation: Navigation = navigation
     userInteraction: UserInteraction = userInteraction
 }
 
