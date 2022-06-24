039987e1a81f70fdff8efdb94a84402e069290c9
I503232 <benjamin.warth@sap.com>
2022-06-24 17:07:14 +0200

    rework tests

--- tests/reuse/ui5/navigationBar/expectPageTitle.spec.js
+++ tests/reuse/ui5/navigationBar/expectPageTitle.spec.js
@@ -1,8 +1,8 @@
 "use strict";
 
-describe("navigationBar - expectPageTitle equal 'Home' after login", function () {
+describe("navigationBar - expectPageTitle - Home QS9", function () {
   it("Preparation", async function () {
-    browser.config.baseUrl = "https://super-sensitive.domain.name/ui";
+    util.browser.setBaseUrl("https://super-sensitive.domain.name/ui");
   });
 
   it("Execution", async function () {
@@ -12,7 +12,52 @@ describe("navigationBar - expectPageTitle equal 'Home' after login", function ()
 
   it("Verification", async function () {
     await ui5.navigationBar.expectPageTitle("Home");
-    await expect(ui5.navigationBar.expectPageTitle("PurchaseOrder"))
+  });
+});
+
+describe("navigationBar - expectPageTitle - Home QS9 - Error case", function () {
+  it("Preparation", async function () {
+    util.browser.setBaseUrl("https://super-sensitive.domain.name/ui");
+  });
+
+  it("Execution", async function () {
+    await ui5.navigation.navigateToApplication("Shell-home", false);
+    await ui5.session.loginFiori("PURCHASER");
+  });
+
+  it("Verification", async function () {
+    await expect(ui5.navigationBar.expectPageTitle("Not Home"))
+      .rejects.toThrow(/uiControlExecuteLocator\(\): No visible elements found/);
+  });
+});
+
+describe("navigationBar - expectPageTitle - Home HBR", function () {
+  it("Preparation", async function () {
+    util.browser.setBaseUrl("https://super-sensitive.domain.name/ui");
+  });
+
+  it("Execution", async function () {
+    await ui5.navigation.navigateToApplication("Shell-home", false);
+    await ui5.session.loginFiori("PURCHASER");
+  });
+
+  it("Verification", async function () {
+    await ui5.navigationBar.expectPageTitle("Home");
+  });
+});
+
+describe("navigationBar - expectPageTitle - Home HBR - Error case", function () {
+  it("Preparation", async function () {
+    util.browser.setBaseUrl("https://super-sensitive.domain.name/ui");
+  });
+
+  it("Execution", async function () {
+    await ui5.navigation.navigateToApplication("Shell-home", false);
+    await ui5.session.loginFiori("PURCHASER");
+  });
+
+  it("Verification", async function () {
+    await expect(ui5.navigationBar.expectPageTitle("Not Home"))
       .rejects.toThrow(/uiControlExecuteLocator\(\): No visible elements found/);
   });
 });
