46573c9ab131865f42ebc79c92eb170821d61a7e
I503232 <benjamin.warth@sap.com>
2022-06-24 15:20:19 +0200

    remove legacy reuse tests

--- tests/legacyReuse/ui5/specs/assertion/expectTrue.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-describe("assertion - expectTrue", function () {
-  it("Execution and Verification", function () {
-    ui5.common.assertion.expectTrue(true);
-    expect(() => ui5.common.assertion.expectTrue(false)).toThrow(/Expected\w*|\d*true/);
-    expect(() => ui5.common.assertion.expectTrue("true")).toThrow(/Expected\w*|\d*true/);
-  });
-});
