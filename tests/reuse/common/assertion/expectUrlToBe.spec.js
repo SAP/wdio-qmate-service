571f0decd82c727a6626265f2a62ee1789921822
D056896 <marvin.gruessinger@sap.com>
2022-07-04 14:29:32 +0200

    improve and fix tests

--- tests/reuse/common/assertion/expectUrlToBe.spec.js
+++ tests/reuse/common/assertion/expectUrlToBe.spec.js
@@ -1,8 +1,7 @@
 "use strict";
-// How to change baseUrl from configuration file:
-// https://webdriver.io/docs/browserobject.html#configurations-versus-options
-describe("assertion - expectUrlToBe simple url (before login)", function () {
-  const url = "https://super-sensitive.domain.name/ui";
+
+const url = "https://openui5.hana.ondemand.com/test-resources/sap/m/demokit/iconExplorer/webapp/index.html";
+describe("assertion - expectUrlToBe - 'openui5.....'", function () {
 
   it("Preparation", async function () {
     await browser.navigateTo(url);
@@ -13,32 +12,9 @@ describe("assertion - expectUrlToBe simple url (before login)", function () {
   });
 });
 
-describe("assertion - expectUrlToBe after login with sap client and language (unhappy case)", function () {
-  const url = "https://super-sensitive.domain.name/ui";
+describe("assertion - expectUrlToBe - error case", function () {
   it("Preparation", async function () {
-    browser.config.baseUrl = url;
-  });
-
-  it("Execution", async function () {
-    await ui5.navigation.navigateToApplication("Shell-home", true);
-    await ui5.session.loginFiori("PURCHASER");
-  });
-
-  it("Verification", async function () {
-    const urlExp = "https://super-sensitive.domain.name/ui#Shell-home";
-    // this check "await common.assertion.expectUrlToBe(urlExp);" is not working (from time to time) - during login url can be
-    // https://super-sensitive.domain.name/ui?_sap-hash=JTIzU2hlbGwtaG9tZQ&sap-system-login=X&sap-system-login-cookie=X&sap-contextid=SID:ANON:ldai1qs9_QS9_00:E6H6esENrZdnhPd1G7VjCTa0jUDMw377cB8hPHuC-ATT
-    // need to wait for stabilization?
-    // await expect(common.assertion.expectUrlToBe(urlExp)).rejects.toThrow(/sap-hash/);
-    const currentUrl = await browser.getUrl();
-    console.log(`Url after login: ${currentUrl}`);
-  });
-});
-
-
-describe("assertion - expectUrlToBe wrong url (unhappy case)", function () {
-  it("Preparation", async function () {
-    browser.config.baseUrl = "https://super-sensitive.domain.name/ui";
+    util.browser.setBaseUrl("https://super-sensitive.domain.name/ui");
   });
 
   it("Execution and Verification", async function () {
