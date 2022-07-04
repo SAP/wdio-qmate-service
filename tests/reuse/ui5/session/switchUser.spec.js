64e00ee05aceca1e093197a0b4e838c1461f219d
D056896 <marvin.gruessinger@sap.com>
2022-07-04 14:51:24 +0200

    change tests pws

--- tests/reuse/ui5/session/switchUser.spec.js
+++ tests/reuse/ui5/session/switchUser.spec.js
@@ -4,14 +4,14 @@ describe("session - switchUser in fiori", function () {
   it("Preparation", async function () {
     util.browser.setBaseUrl("https://super-sensitive.domain.name/ui");
     await common.navigation.navigateToUrl(browser.config.baseUrl);
-    await ui5.session.login("PURCHASER");
+    await ui5.session.login("PURCHASER", "super-duper-sensitive-pw");
     await ui5.navigation.navigateToApplication("Shell-home", true);
     await ui5.navigation.closePopups();
     await ui5.navigationBar.expectShellHeader();
   });
 
   it("Execution", async function () {
-    await ui5.session.switchUser("AP_ACCOUNTANT");
+    await ui5.session.switchUser("AP_ACCOUNTANT", "super-duper-sensitive-pw");
     await ui5.navigation.navigateToApplication("Shell-home", true);
     await ui5.navigation.closePopups();
   });
@@ -29,7 +29,7 @@ describe("session - switchUser in sapCloud", function () {
   it("Preparation", async function () {
     util.browser.setBaseUrl("https://super-sensitive.domain.name/ui");
     await ui5.navigation.navigateToApplication("Shell-home", true);
-    await ui5.session.loginSapCloud("PURCHASER");
+    await ui5.session.loginSapCloud("PURCHASER", "super-duper-sensitive-pw");
     await ui5.navigationBar.expectShellHeader();
   });
 
