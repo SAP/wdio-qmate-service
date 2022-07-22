53dc759d4c8dc3aac01f46bac2fa10625fd315b5
I503232 <benjamin.warth@sap.com>
2022-07-22 11:28:45 +0200

    cleanup config profiles

--- tests/helper/configurations/chrome.headless.conf.js
+++ tests/helper/configurations/chrome.headless.conf.js
@@ -1,16 +1,13 @@
 const merge = require("deepmerge");
 const baseConfig = require("./base.conf.js");
+
 exports.config = merge(baseConfig.config, {
-  //
-  // Override default path ('/wd/hub') for chromedriver service.
   path: "/",
 
   capabilities: [{
     browserName: "chrome",
     acceptInsecureCerts: true,
     "goog:chromeOptions": {
-      // to run chrome headless the following flags are required
-      // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
       args: [
         "--output=/dev/null",
         "--log-level=3",
@@ -33,24 +30,11 @@ exports.config = merge(baseConfig.config, {
       }
     }
   }],
-  //
-  // Test runner services
-  // Services take over a specific job you don't want to take care of. They enhance
-  // your test setup with almost no effort. Unlike plugins, they don't add new
-  // commands. Instead, they hook themselves up into the test process.
+  
   services: [
     ["chromedriver", {
       port: 4444,
       chromedriverCustomPath: process.env.CHROME_DRIVER
     }]
-  ],
-
-  //
-  // Options to be passed to Mocha.
-  // See the full list at http://mochajs.org/
-  mochaOpts: {
-    // Retry tests 1 times
-    //retries: 1
-  },
-
+  ]
 });
