59217ad9fd9a43507e7f0502f77832f698aa0da4
I503232 <benjamin.warth@sap.com>
2022-07-28 18:41:54 +0200

    update docs

--- src/reuse/modules/ui5/session.ts
+++ src/reuse/modules/ui5/session.ts
@@ -17,7 +17,7 @@ export class Session {
    * @example await ui5.session.login("PURCHASER");
    * @example await ui5.session.login("JOHN_DOE", "abc123!", true);
    */
-  async login (username: string, password = "Welcome1!", verify = false, timeout = process.env.QMATE_CUSTOM_TIMEOUT || 30000) {
+  async login(username: string, password = "Welcome1!", verify = false, timeout = process.env.QMATE_CUSTOM_TIMEOUT || 30000) {
     if (browser.config && browser.config.params &&
       browser.config.params.auth && browser.config.params.auth.formType === "skip") {
       util.console.warn("Login is skipped since 'formType' is set to 'skip'");
