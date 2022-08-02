ea7a5907e7524932c22ecf2cae50bcde0674e195
Jan Werkmann <jan.werkmann@sap.com>
2022-08-02 15:48:23 +0200

    add unit tests

---
+++ tests/reuse/ui5/navigation/closePopups.spec.js
@@ -0,0 +1,23 @@
+"use strict";
+
+describe("navigation - closePopups (S4)", function () {
+  const system = "qs9-715.wdf.sap.corp";
+
+  it("Execution and Verification", async function () {
+    await ui5.navigation.closePopups(30000);
+    await ui5.navigation.navigateToSystemAndApplication(system, "Shell-home", false);
+    await ui5.session.loginFiori("PURCHASER");
+    await util.browser.sleep(30000);
+  });
+
+  it("Verification and Clean Up", async function () {
+    // When the popup is not closed, logout will not work
+    try {
+      await ui5.session.logout();
+    } catch (e) {
+      // Reload Session so the Cleanup is done correctly for the next test
+      await browser.reloadSession();
+      throw new Error(e);
+    }
+  });
+});
