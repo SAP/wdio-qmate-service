7e8a68eb719bd9293c3e1fe29de04df1bc672086
I503232 <benjamin.warth@sap.com>
2022-08-04 11:36:16 +0200

    improve code, remove useless popup

--- tests/reuse/ui5/navigation/closePopups.spec.js
+++ tests/reuse/ui5/navigation/closePopups.spec.js
@@ -1,12 +1,10 @@
 "use strict";
 
 describe("navigation - closePopups", function () {
-  const system = "hbr-710.wdf.sap.corp";
-
   it("Preparation", async function () {
-    await ui5.navigation.navigateToSystemAndApplication(system, "SupplierInvoice-create", false);
+    await ui5.navigation.navigateToSystemAndApplication("hbr-710.wdf.sap.corp", "SupplierInvoice-create", false);
     await ui5.session.login("AP_ACCOUNTANT");
-    // await util.browser.sleep(20000);
+    await util.browser.sleep(20000);
   });
 
   it("Execution", async function () {
