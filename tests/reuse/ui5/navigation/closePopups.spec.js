c91f8ad2837fc713843527563ca6e402503d6b73
I503232 <benjamin.warth@sap.com>
2022-08-05 14:09:53 +0200

    add check for displayed in stabilization

--- tests/reuse/ui5/navigation/closePopups.spec.js
+++ tests/reuse/ui5/navigation/closePopups.spec.js
@@ -2,7 +2,7 @@
 
 describe("navigation - closePopups", function () {
   it("Preparation", async function () {
-    await ui5.navigation.navigateToSystemAndApplication("hbr-710.wdf.sap.corp", "SupplierInvoice-create", false);
+    await ui5.navigation.navigateToSystemAndApplication("hbr-710.wdf.sap.corp", "Shell-Home", false);
     await ui5.session.login("AP_ACCOUNTANT");
   });
 
