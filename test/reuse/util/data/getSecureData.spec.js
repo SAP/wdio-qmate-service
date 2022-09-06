f6bd753d826d8386f186169f0345f6a787af0d01
Benjamin Warth <60264179+DIFSRIP@users.noreply.github.com>
2022-09-06 17:47:02 +0200

    enhance data privacy for multiple landscapes (#51)
    * log where it used the keys from
    
    * support decrypt for alternative values
    
    * support alternative values for decryption
    
    * bump version

--- test/reuse/util/data/getSecureData.spec.js
+++ test/reuse/util/data/getSecureData.spec.js
@@ -6,21 +6,27 @@ describe("data - getSecureData", function() {
     data = util.data.getSecureData("test");
   });
 
-  it("Verification (1)", function () {
+  it("Verification 1 - hashed value", function () {
     const dataExp = "super-duper-sensitive-pw";
     const dataAct = data.session.password;
     common.assertion.expectEqual(dataAct, dataExp);
   });
 
-  it("Verification (2)", function () {
+  it("Verification 2 - plain value", function () {
     const dataExp = 42;
     const dataAct = data.test.number;
     common.assertion.expectEqual(dataAct, dataExp);
   });
 
-  it("Verification (3)", function () {
+  it("Verification 3 - nested value", function () {
     const dataExp = "TEST";
     const dataAct = data.test.inner.value;
+    common.assertion.expectEqual(dataAct, dataExp);
+  });
+
+  it("Verification 4 - alternatives from different keys", function () {
+    const dataExp = "super-duper-sensitive-pw";
+    const dataAct = data.alternatives;
     common.assertion.expectEqual(dataAct, dataExp);
   });
 
