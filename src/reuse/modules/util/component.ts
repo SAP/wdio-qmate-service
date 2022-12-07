4f46a21339e720cacbc6fc609821106993a580c5
Moritz Knaut <moritz.knaut@sap.com>
2022-12-07 17:25:06 +0100

    Fix prep filter

--- src/reuse/modules/util/component.ts
+++ src/reuse/modules/util/component.ts
@@ -9,7 +9,7 @@ export class Component {
   async loadEntryPoint(): Promise<any> {
     // Will be called in component scripts
     const componentSpec = runtime.specs.filter((spec: string) => {
-      return !spec.includes(".prep.");
+      return !spec.includes("prep.");
     })[0];
     const componentSpecIdentifier = path.basename(componentSpec).replaceAll(".spec.js", "");
 
@@ -19,7 +19,7 @@ export class Component {
   async storeEntryPoint(data: any): Promise<void> {
     // Will be called in prep scripts
     const componentSpec = runtime.specs.filter((spec: string) => {
-      return !spec.includes(".prep.");
+      return !spec.includes("prep.");
     })[0];
     const componentSpecIdentifier = path.basename(componentSpec).replaceAll(".spec.js", "");
 
