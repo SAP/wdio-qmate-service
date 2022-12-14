46924a66e7fffa5f491625091ba8b44fd65d8792
Herald Antony <herald.maria.antony@sap.com>
2022-12-14 21:06:54 +0530

    add tests for new util.data and util.component reuse functions

---
+++ test/reuse/util/component/storeEntryPoint.spec.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+describe("component - storeEntryPoint", function () {
+  const filename = "storeEntryPoint.entrypoint.json";
+  const data = { purchaseOrder: "123456" };
+
+  it("Execution", async function () {
+    await util.component.storeEntryPoint(data);
+  });
+
+  it("Verification", async function () {
+    const dataAct = await util.data.readDataFromFile(path.resolve(__dirname, filename));
+    common.assertion.expectEqual(dataAct, data);
+  });
+
+  it("Cleanup", async function () {
+    fs.unlinkSync(path.resolve(__dirname, filename));
+  });
+
+});
---
+++ test/reuse/util/data/writeDataToFile.spec.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+
+describe("data - writeDataToFile", function () {
+  const filename = "data/test-out.json";
+  const data = { purchaseOrder: "123456" };
+
+  it("Execution", async function () {
+    await util.data.writeDataToFile(filename, data);
+  });
+
+  it("Verification", async function () {
+    const dataAct = await util.data.readDataFromFile(filename);
+    common.assertion.expectEqual(dataAct, data);
+  });
+
+  it("Cleanup", async function () {
+    fs.unlinkSync(path.resolve(__dirname, filename));
+  });
+
+});
+
+describe("data - writeDataToFile - error case", function () {
+
+  const data = { purchaseOrder: "123456" };
+
+  it("Execution and Verification", async function () {
+    await expect(util.data.writeDataToFile("no-such-folder/test-out.json", data))
+      .rejects.toThrow("Function 'writeDataToFile' failed:");
+  });
+
+});
