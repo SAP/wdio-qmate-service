07fc99e017def6b3fcddff7b4222ec8208a5e645
D056896 <marvin.gruessinger@sap.com>
2023-01-20 14:53:29 +0100

    add iframe switch for logon

--- src/reuse/modules/nonUi5/session.ts
+++ src/reuse/modules/nonUi5/session.ts
@@ -11,10 +11,13 @@ export class Session {
  * @description Login for SAP NetWebGUI form and specific username and password.
  * @param {String} username - The username.
  * @param {String} [password] - The password.
+ * @param {String} [iframeCssSelector] - The specific iframe selector the login form is contained.
+
  * @param {Boolean} [clickContinue=true] - Specifies if the function will press continue if applicable.
  * @example await nonUi5.session.loginSapNetWeaver("john", "abc123!");
  */
-  async loginSapNetWeaver(username: string, password?: string, clickContinue = true) {
+  async loginSapNetWeaver(username: string, password: string, clickContinue = true, iframeCssSelector = "iframe") {
+    await util.browser.switchToIframe(iframeCssSelector);
     await ui5.session.loginCustom(username, password, "#sap-user", "#sap-password", "#LOGON_BUTTON");
     if (clickContinue) {
       await util.function.executeOptional(async () => {
@@ -22,6 +25,7 @@ export class Session {
         await nonUi5.userInteraction.click(continueButton);
       });
     }
+    await util.browser.switchToDefaultContent();
   }
 
 }
