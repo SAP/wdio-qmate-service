c6e2f324624d2eacf599bd8d06c9e0a118248a5c
D056896 <marvin.gruessinger@sap.com>
2023-01-20 14:12:41 +0100

    add session.ts module for nonUi5

---
+++ src/reuse/modules/nonUi5/session.ts
@@ -0,0 +1,28 @@
+"use strict";
+/**
+ * @class session
+ * @memberof nonUi5
+ */
+export class Session {
+// =================================== LOGIN ===================================
+/**
+ * @function loginSapNetWeaver
+ * @memberOf nonUi5.session
+ * @description Login for SAP NetWebGUI form and specific username and password.
+ * @param {String} username - The username.
+ * @param {String} [password] - The password.
+ * @param {Boolean} [clickContinue=true] - Specifies if the function will press continue if applicable.
+ * @example await nonUi5.session.loginSapNetWeaver("john", "abc123!");
+ */
+  async loginSapNetWeaver(username: string, password?: string, clickContinue = true) {
+    await ui5.session.loginCustom(username, password, "#sap-user", "#sap-password", "#LOGON_BUTTON");
+    if (clickContinue) {
+      await util.function.executeOptional(async () => {
+        const continueButton = await nonUi5.element.getByCss("DIV[id*='CONTINUE_BUTTON']", 0, 5000);
+        await nonUi5.userInteraction.click(continueButton);
+      });
+    }
+  }
+
+}
+export default new Session();
