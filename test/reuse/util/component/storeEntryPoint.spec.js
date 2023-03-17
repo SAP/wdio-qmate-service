5d06f868763f86a887a34b6af610ff2002ddabb1
Herald Antony <herald.maria.antony@sap.com>
2023-03-17 12:23:15 +0530

    fix storeEntryPoint test to prevent race condition with loadEntryPoint when run in parallel

--- test/reuse/util/component/storeEntryPoint.spec.js
+++ test/reuse/util/component/storeEntryPoint.spec.js
@@ -2,26 +2,6 @@ const fs = require("fs");
 const path = require("path");
 const os = require("os");
 
-describe("component - storeEntryPoint", function () {
-  const filename = "storeEntryPoint.entrypoint.json";
-  const folderPath = "entrypoints";
-  const data = { purchaseOrder: "123456" };
-
-  it("Execution", async function () {
-    await util.component.storeEntryPoint(data);
-  });
-
-  it("Verification", async function () {
-    const dataAct = await util.data.readDataFromFile(path.resolve(folderPath, filename));
-    common.assertion.expectEqual(dataAct, data);
-  });
-
-  it("Cleanup", async function () {
-    await fs.promises.rm(folderPath, {recursive: true, force: true});
-  });
-
-});
-
 describe("component - storeEntryPoint - custom folder path", function () {
   const filename = "storeEntryPoint.entrypoint.json";
   const folderPath = path.resolve(os.tmpdir(), "entrypointsStore");
@@ -48,6 +28,30 @@ describe("component - storeEntryPoint - error case", function () {
   it("Execution & Verification", async function () {
     await expect(util.component.storeEntryPoint(undefined, folderPath))
       .rejects.toThrow("Function 'writeDataToFile' failed:");
+  });
+
+  it("Cleanup", async function () {
+    await fs.promises.rm(folderPath, {recursive: true, force: true});
+  });
+
+});
+
+// this test has been moved down, and sleep added to
+// avoid race condition with loadEntryPoint tests
+// since both use the same folder
+describe("component - storeEntryPoint", function () {
+  const filename = "storeEntryPoint.entrypoint.json";
+  const folderPath = "entrypoints";
+  const data = { purchaseOrder: "123456" };
+
+  it("Execution", async function () {
+    await util.browser.sleep(1000);
+    await util.component.storeEntryPoint(data);
+  });
+
+  it("Verification", async function () {
+    const dataAct = await util.data.readDataFromFile(path.resolve(folderPath, filename));
+    common.assertion.expectEqual(dataAct, data);
   });
 
   it("Cleanup", async function () {
