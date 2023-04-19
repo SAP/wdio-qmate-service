b7b4a49134f8a733f17c8c18b1e17541c10a5770
Uladzislau Dauhashei <u.dauhashei@sap.com>
2023-04-19 14:01:56 +0200

    fixed parsing args

--- src/reuse/helper/jsonDocGen.js
+++ src/reuse/helper/jsonDocGen.js
@@ -123,8 +123,8 @@ function getReturnType(tags) {
 function mapTagToArgument(tag) {
   return {
     name: tag.name,
-    type: (tag.type && tag.type.name && tag.type.name.toLowerCase()) || "string",
-    default: tag.default ? tag.default : null
+    type: getArgumentType(tag),
+    default: getArgumentDefaultValue(tag)
   };
 }
 
@@ -136,4 +136,45 @@ function parseReturnTypeFromReturnTag(tag) {
     return tag.type.expression.name.toLowerCase();
   }
   return undefined;
+}
+
+function getArgumentType(tag) {
+  if (cannotParseArgumentTypeFromTag(tag)) {
+    return "string";
+  }
+  if (canParseArgumentTypeFromTagName(tag)) {
+    return parseArgumentTypeFromTagName(tag);
+  } else if (canParseArgumentTypeFromTagExpression(tag)) {
+    return parseArgumentTypeFromTagExpression(tag);
+  }
+}
+
+function getArgumentDefaultValue(tag) {
+  return tag.default ? tag.default : null;
+}
+
+function cannotParseArgumentTypeFromTag(tag) {
+  return !canParseArgumentTypeFromTag(tag);
+}
+
+function canParseArgumentTypeFromTag(tag) {
+  return tag.type
+    && (canParseArgumentTypeFromTagName(tag)
+      || canParseArgumentTypeFromTagExpression(tag));
+}
+
+function canParseArgumentTypeFromTagName(tag) {
+  return !!tag.type.name;
+}
+
+function parseArgumentTypeFromTagName(tag) {
+  return tag.type.name.toLowerCase();
+}
+
+function canParseArgumentTypeFromTagExpression(tag) {
+  return tag.type.expression && tag.type.expression.name;
+}
+
+function parseArgumentTypeFromTagExpression(tag) {
+  return tag.type.expression.name.toLowerCase();
 }
