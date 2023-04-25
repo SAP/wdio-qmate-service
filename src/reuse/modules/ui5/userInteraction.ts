e5190dca2d327d303f8109f1418c4c1251d34c0e
Jan Werkmann <janwerkmann@gmail.com>
2023-04-25 14:56:05 +0200

    remove userinteraction.focus

--- src/reuse/modules/ui5/userInteraction.ts
+++ src/reuse/modules/ui5/userInteraction.ts
@@ -636,33 +636,6 @@ export class UserInteraction {
   }
 
   /**
-   * @function focus
-   * @memberOf ui5.userInteraction
-   * @description Focuses on the element with the given selector to get it into view. If focus is not possible scrollToElement is used.
-   * @param {Object} selector - The selector describing the element.
-   * @param {Number} [index=0] - The index of the selector (in case there are more than one elements visible at the same time).
-   * @param {Number} [timeout=30000] - The timeout to wait (ms).
-   * @example await ui5.userInteraction.focus(selector);
-   * @example await ui5.userInteraction.focus(selector, 0, 5000);
-   */
-  async focus(selector: any, index = 0, timeout = process.env.QMATE_CUSTOM_TIMEOUT || 30000) {
-    const elem = await ui5.element.getDisplayed(selector, index, timeout);
-    const id = await elem.getAttribute("id");
-    const focused = await browser.execute(function (id: string) {
-        const elem = sap.ui.getCore().getElementById(id);
-        if(elem && elem.focus){
-            sap.ui.getCore().getElementById(id).focus();
-            return true;
-        } else{
-            return false;
-        }
-    }, id);
-    if(!focused){
-        this.scrollToElement(selector, index, "center", timeout);
-    }
-  }
-
-  /**
    * @function selectAll
    * @memberOf ui5.userInteraction
    * @description Performs "select all" (ctrl + a) at the element with the given selector.
