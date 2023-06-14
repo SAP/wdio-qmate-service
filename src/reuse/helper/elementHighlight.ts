0cdda6af55ab7c65564756f32cb5cdd902bc0a04
Reddy <ananth.reddy@sap.com>
2023-06-14 12:02:03 +0530

    defined type of higlight config

--- src/reuse/helper/elementHighlight.ts
+++ src/reuse/helper/elementHighlight.ts
@@ -2,6 +2,13 @@
 
 import { VerboseLoggerFactory } from "./verboseLogger";
 
+interface ElementHighlightConfig {
+  enable: boolean;
+  duration?: number;
+  color?: string;
+  [key: string]: any;
+}
+
 /**
  * @class ElementHighlight
  * @memberof helper
@@ -18,38 +25,41 @@ export class ElementHighlight {
    * @param {string} actionName - action name is required
    * @example await elementHighlight.getElementHighlightData("click");
    */
-  async getElementHighlightData(actionName: string): Promise<any> {
+  async getElementHighlightData(actionName: string): Promise<ElementHighlightConfig> {
     const vl = this.vlf.initLog(this.getElementHighlightData);
 
-    let elementHighlightConfig = browser.config.params.highlightElements;
-    let elementHighlightDefaultConfig: { enable: boolean; color: string; duration: number } = {
+    let elementHighlightDefaultConfig = {
       enable: false,
-      color: "red",
-      duration: 1000
+      duration: 1000,
+      color: "red"
     };
 
-    if (elementHighlightConfig && elementHighlightConfig.enable) {
-      let actions = elementHighlightConfig.actions;
+    if (browser.config.params) {
+      let elementHighlightConfig = browser.config.params.highlightElements;
 
-      if (!actions) throw new Error("Please specify the actions key in element highlight config object");
+      if (elementHighlightConfig && elementHighlightConfig.enable) {
+        let actions = elementHighlightConfig.actions;
 
-      if (Array.isArray(actions)) {
-        for (let action of actions) {
-          if (!(typeof action == "string" && typeof actionName == "string"))
-            throw new Error(`Please provide the method name '${action}/${actionName}' in string format`);
+        if (!actions) throw new Error("Please specify the actions key in element highlight config object");
 
-          if (action.toLowerCase().trim().includes(actionName.trim())) {
+        if (Array.isArray(actions)) {
+          for (let action of actions) {
+            if (!(typeof action == "string" && typeof actionName == "string"))
+              throw new Error(`Please provide the method name '${action}/${actionName}' in string format`);
+
+            if (action.toLowerCase().trim().includes(actionName.trim())) {
+              elementHighlightConfig = this._getElementHighlightColorAndDuration(elementHighlightConfig, elementHighlightDefaultConfig);
+              return this._getElementHighlightConfig(true, elementHighlightConfig.color, elementHighlightConfig.duration);
+            }
+          }
+        } else if (typeof actions === "string" && typeof actionName === "string") {
+          if (actions.toLowerCase().trim().includes(actionName.trim())) {
             elementHighlightConfig = this._getElementHighlightColorAndDuration(elementHighlightConfig, elementHighlightDefaultConfig);
             return this._getElementHighlightConfig(true, elementHighlightConfig.color, elementHighlightConfig.duration);
           }
+        } else {
+          throw new Error(`Please provide the method name '${actions}/${actionName}' in string format`);
         }
-      } else if (typeof actions === "string" && typeof actionName === "string") {
-        if (actions.toLowerCase().trim().includes(actionName.trim())) {
-          elementHighlightConfig = this._getElementHighlightColorAndDuration(elementHighlightConfig, elementHighlightDefaultConfig);
-          return this._getElementHighlightConfig(true, elementHighlightConfig.color, elementHighlightConfig.duration);
-        }
-      } else {
-        throw new Error(`Please provide the method name '${actions}/${actionName}' in string format`);
       }
     }
     return elementHighlightDefaultConfig;
@@ -57,7 +67,7 @@ export class ElementHighlight {
 
   // =================================== HELPER ===================================
 
-  private _getElementHighlightConfig(enabled: boolean, colorName: string, timeout: number): any {
+  private _getElementHighlightConfig(enabled: boolean, colorName: string, timeout: number): ElementHighlightConfig {
     return {
       enable: enabled,
       color: colorName,
@@ -65,7 +75,10 @@ export class ElementHighlight {
     };
   }
 
-  private _getElementHighlightColorAndDuration(elementHighlightConfig: any, elementHighlightDefaultConfig: any): any {
+  private _getElementHighlightColorAndDuration(
+    elementHighlightConfig: ElementHighlightConfig,
+    elementHighlightDefaultConfig: ElementHighlightConfig
+  ): ElementHighlightConfig {
     elementHighlightConfig.color = elementHighlightConfig.hasOwnProperty("color")
       ? elementHighlightConfig.color
       : elementHighlightDefaultConfig.color;
