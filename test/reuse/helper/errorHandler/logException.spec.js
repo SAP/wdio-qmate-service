7151f9acfb608aa3fa371eb331aaa04212dcf418
Ananthreddy3004 <132659201+Ananthreddy3004@users.noreply.github.com>
2023-08-03 18:14:08 +0530

    Implemented the generic exception class handler (#187)
    * Implemented the generic exception class handler
    
    * fixed the review comments
    
    * fixed the review comments
    
    * fixed the review comments
    
    * given the proper variable naming convention
    
    ---------
    
    Co-authored-by: Benjamin Warth <60264179+DIFSRIP@users.noreply.github.com>

---
+++ test/reuse/helper/errorHandler/logException.spec.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const ErrorHandler = require("../../../../lib/reuse/helper/errorHandler").default;
+
+describe("errorHandler - logException - displaying customized message without stacktrace", async function () {
+  it("Execution & Verification", async function () {
+    const errorHandler = new ErrorHandler(false);
+    await expect(async () => {
+      errorHandler.logException(new Error("Divide by zero error."));
+    }).rejects.toThrow("Error: Function 'at' failed with : Divide by zero error.");
+  });
+});
+
+describe("errorHandler - logException - displaying customized message with stacktrace by passing the constructor value", async function () {
+  const errorHandler = new ErrorHandler(true);
+  it("Execution & Verification", async function () {
+    await expect(async function display() {
+      errorHandler.logException(new Error("File not found exception"));
+    }).rejects.toThrow(/Error: Function 'display' failed with : File not found exception/);
+  });
+});
+
+describe("errorHandler - logException - displaying customized message with stacktrace without default constructor value", async function () {
+  const errorHandler = new ErrorHandler();
+  it("Execution & Verification", async function () {
+    await expect(async function display() {
+      errorHandler.logException(new Error("Array index out of bound exception"));
+    }).rejects.toThrow(/Error: Function 'display' failed with : Array index out of bound exception/);
+  });
+});
+
+describe("errorHandler - logException - displaying generic error message", async function () {
+  it("Execution & Verification", async function () {
+    const errorHandler = new ErrorHandler(false);
+    await expect(async () => {
+      errorHandler.logException();
+    }).rejects.toThrow("Error: Failed due to the exception occurred at the block");
+  });
+});
