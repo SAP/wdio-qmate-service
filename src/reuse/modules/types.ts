48aced8be6659c4fd8f844410b379e99cf2899ea
Santhosh Krishnamurthy <santhoshks2312@gmail.com>
2023-08-14 16:59:52 +0530

    Update types.ts

--- src/reuse/modules/types.ts
+++ src/reuse/modules/types.ts
@@ -1,6 +1,8 @@
 export type AlignmentValues = "start" | "center" | "end" | "nearest";
+export type BehaviorValues = "smooth" | "instant" | "auto";
 
 export type AlignmentOptions = {
   block: AlignmentValues;
   inline: AlignmentValues;
+  behavior: BehaviorValues;
 };
