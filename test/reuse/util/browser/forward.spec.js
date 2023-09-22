dc01de97447c7b93fb441687c7c2c04e55255c3e
Reddy <ananth.reddy@sap.com>
2023-09-22 14:48:52 +0530

    implemented the browser forward

---
+++ test/reuse/util/browser/forward.spec.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const browser = require("../../../../lib/reuse/modules/util/browser");
+const { handleCookiesConsent } = require("../../../helper/utils");
+
+describe("Expect browser url to be equal with 'startUrl' after browser forward", function () {
+  it("Preparation", async function () {
+    await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/1.99.0/");
+    await handleCookiesConsent();
+    const selector = {
+      elementProperties: {
+        viewName: "sap.ui.documentation.sdk.view.App",
+        metadata: "sap.m.IconTabFilter",
+        text: [
+          {
+            path: "i18n>APP_TABHEADER_ITEM_DOCUMENTATION"
+          }
+        ]
+      }
+    };
+    await ui5.userInteraction.click(selector);
+  });
+
+  it("Execution & Verification", async function () {
+    await util.browser.forward();
+    const url = await util.browser.getCurrentUrl();
+    util.console.log(`Current url is : ${url}`);
+    common.assertion.expectEqual("https://sapui5.hana.ondemand.com/1.99.0/#/topic", url);
+  });
+});
