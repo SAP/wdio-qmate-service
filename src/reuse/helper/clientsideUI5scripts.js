813cdf019447cc4a07e57d22a44bdface4ce0af6
Jan Werkmann <janwerkmann@gmail.com>
2023-10-31 13:34:19 +0100

    Update clientsideUI5scripts.js

--- src/reuse/helper/clientsideUI5scripts.js
+++ src/reuse/helper/clientsideUI5scripts.js
@@ -4,6 +4,8 @@ var functions = {};
 
 functions.execQUnits = function (mScriptParams, done) {
   if (QUnit) {
+    // reset the QUnit config such that the test can run a second time
+    QUnit.config.current = undefined;
     QUnit.start();
     QUnit.done(function (details) {
       const msg = "Total: " + details.total + "," + " Failed: " + details.failed + "," + " Passed: " + details.passed + "," + " Runtime: " + details.runtime;
