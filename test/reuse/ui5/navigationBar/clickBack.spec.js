b59e2dc59f18f41c5f9304b551456185b3deda9c
I503232 <benjamin.warth@sap.com>
2024-05-28 11:31:36 +0200

    fix navigationBar tests

--- test/reuse/ui5/navigationBar/clickBack.spec.js
+++ test/reuse/ui5/navigationBar/clickBack.spec.js
@@ -1,9 +1,16 @@
 "use strict";
 
 describe("navigationBar - clickBack", async function () {
-
   it("Preparation", async function () {
-    await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/test-resources/sap/ui/demoapps/demokit/rta/fiori-elements/test/index.html#masterDetail-display");
+    await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/test-resources/sap/ui/demoapps/demokit/rta/fiori-elements/test/index.html#Shell-home");
+    const selector = {
+      elementProperties: {
+        viewName: "sap.ushell.components.tiles.cdm.applauncher.StaticTile",
+        metadata: "sap.m.GenericTile",
+        bindingContextPath: "/pages/0/sections/0/visualizations/0"
+      }
+    };
+    await ui5.userInteraction.click(selector);
   });
 
   it("Execution", async function () {
@@ -13,18 +20,14 @@ describe("navigationBar - clickBack", async function () {
   it("Verification", async function () {
     await ui5.navigationBar.expectPageTitle("Home");
   });
-
 });
 
 describe("navigationBar - clickBack - error case", function () {
-
   it("Preparation", async function () {
     await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/test-resources/sap/ui/demoapps/demokit/rta/fiori-elements/test/index.html#Shell-home");
   });
 
   it("Execution & Verification", async function () {
-    await expect(ui5.navigationBar.clickBack())
-      .rejects.toThrowError("Function 'clickBack' failed with:");
+    await expect(ui5.navigationBar.clickBack()).rejects.toThrowError("Function 'clickBack' failed with:");
   });
-
 });
