7c00751b895fef1b637552ed62c9fd235f84a82a
Uladzislau Dauhashei <v.dolgoshey@gmail.com>
2024-07-08 17:47:17 +0200

    added spec to test the watch mode

---
+++ test/watchMode/restartInWatchMode.spec.js
@@ -0,0 +1,23 @@
+describe("watchMode - test case runs after being restarted in watch mode", function() {
+
+    simulateRestartInWatchMode();
+
+    it("Step 01: Navigate to URL", async function() {
+        await common.navigation.navigateToUrl(browser.config.baseUrl);
+    });
+
+    it("Step 02: Click 'accessories' StandardListItem", async function() {
+        const selector = {
+            "elementProperties": {
+                "viewName": "sap.ui.demo.cart.view.Home",
+                "metadata": "sap.m.StandardListItem",
+                "bindingContextPath": "/ProductCategories*'AC')"
+            }
+        };
+        await ui5.userInteraction.click(selector);
+    });
+});
+
+function simulateRestartInWatchMode() {
+    browser = undefined;
+}
