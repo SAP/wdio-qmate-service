33e4d70d824700965f4c15091e0ed554c2f21310
Uladzislau Dauhashei <v.dolgoshey@gmail.com>
2024-07-08 17:58:32 +0200

    added test to test watch mode to the package.json

--- test/watchMode/config.js
+++ test/watchMode/config.js
@@ -6,7 +6,7 @@ const profile = require("../helper/configurations/chrome.headless.conf");
 exports.config = merge(profile.config, {
   baseUrl: "https://sapui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html?sap-ui-theme=sap_horizon",
   specs: [
-    path.resolve(__dirname, "restartInWatchMode.spec.js")
+    path.resolve(__dirname, "navigateToUrlAndClick.spec.js")
   ],
   beforeSession: () => {
     simulateRestartInWatchMode()
