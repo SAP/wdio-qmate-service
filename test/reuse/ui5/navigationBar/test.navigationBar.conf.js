8da6c6d1a3ae37afa702286248c9bdfba325e7b7
Uladzislau Dauhashei <v.dolgoshey@gmail.com>
2024-07-08 15:09:11 +0200

    fixed tests for ui5->navigationBar

--- test/reuse/ui5/navigationBar/test.navigationBar.conf.js
+++ test/reuse/ui5/navigationBar/test.navigationBar.conf.js
@@ -6,7 +6,7 @@ exports.config = merge(profile.config, {
   maxInstances: 4,
   specFileRetries: 2,
 
-  baseUrl: "https://sapui5.hana.ondemand.com/test-resources/sap/ui/demoapps/demokit/rta/fiori-elements/test/index.html#Shell-home",
+  baseUrl: "https://sapui5.hana.ondemand.com/1.96.27/test-resources/sap/ui/demoapps/demokit/rta/fiori-elements/test/index.html#Shell-home",
 
   specs: [
     path.resolve(__dirname, "clickBack.spec.js"),
