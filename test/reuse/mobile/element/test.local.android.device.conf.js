505c9b03c341b7e7561efbecb6644c3ae514b067
Siri Kumar Puttagunta <siri.kumar.puttagunta@sap.com>
2024-09-17 16:15:40 +0530

    Revert -2

--- test/reuse/mobile/element/test.local.android.device.conf.js
+++ test/reuse/mobile/element/test.local.android.device.conf.js
@@ -4,8 +4,11 @@ const profile = require("../../../helper/configurations/mobile.conf");
 
 exports.config = merge(profile.config, {
     specs: [
+        // ToDo: define location for spec files here
+        //'./test/reuse/mobile/*.js'
         path.resolve(__dirname, "isPresent.spec.js"),
     ],
+    // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
     ],
@@ -18,7 +21,11 @@ exports.config = merge(profile.config, {
             "appium:app": path.join(process.cwd(), "app/sap-ibx-v1.12.0-1-debug.apk"),
             "appium:connectHardwareKeyboard": true,
             "appium:newCommandTimeout": 3600,
-            "appium:name": 'Mobile App Automation wdio'
+            "appium:name": 'Mobile App Automation wdio',
+            //"appium:phoneOnly": true,
+            //"appium:noReset": true,
+            //"appium:fullReset": true
+            //"autoGrantPermissions": true
         }
     ],
     logLevel: 'debug',
@@ -26,16 +33,107 @@ exports.config = merge(profile.config, {
         webdriver: 'info',
         webdriverio: 'info',
         '@wdio/appium-service': 'info'
+        // cle'@wdio/applitools-service': 'info'
     },
     services: [
-        ['appium', {
+        /*['chromedriver', {
+            logFileName: 'wdio-chromedriver.log',
+            outputDir: './logs',
+            args: ['--silent']
+        }],*/
+        /*['appium', {
             logPath: './logs',
             logFileName: 'wdio-appium.log',
-        }]
+            //command: 'appium'  //This will use the global Appium for execution, which is installed in local machine.
+        }]*/
     ],
     reporters: [
-        'spec'
+        'spec',
+        /* ['allure',
+             {
+                 outputDir: './allure-results',
+                 disableWebdriverStepsReporting: true,
+                 disableWebdriverScreenshotsReporting: false,
+             }
+         ]*/
     ],
+
+    /**
+     * Gets executed once before all workers get launched.
+     * @param {object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     */
+    // onPrepare: function (config, capabilities) {
+    // },
+    /**
+     * Gets executed before a worker process is spawned and can be used to initialize specific service
+     * for that worker as well as modify runtime environments in an async fashion.
+     * @param  {string} cid      capability id (e.g 0-0)
+     * @param  {object} caps     object containing capabilities for session that will be spawn in the worker
+     * @param  {object} specs    specs to be run in the worker process
+     * @param  {object} args     object that will be merged with the main configuration once worker is initialized
+     * @param  {object} execArgv list of string arguments passed to the worker process
+     */
+    // onWorkerStart: function (cid, caps, specs, args, execArgv) {
+    // },
+    /**
+     * Gets executed just after a worker process has exited.
+     * @param  {string} cid      capability id (e.g 0-0)
+     * @param  {number} exitCode 0 - success, 1 - fail
+     * @param  {object} specs    specs to be run in the worker process
+     * @param  {number} retries  number of retries used
+     */
+    // onWorkerEnd: function (cid, exitCode, specs, retries) {
+    // },
+    /**
+     * Gets executed just before initialising the webdriver session and test framework. It allows you
+     * to manipulate configurations depending on the capability or spec.
+     * @param {object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that are to be run
+     * @param {string} cid worker id (e.g. 0-0)
+     */
+    // beforeSession: function (config, capabilities, specs, cid) {
+    // },
+    /**
+     * Gets executed before test execution begins. At this point you can access to all global
+     * variables like `browser`. It is the perfect place to define custom commands.
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs        List of spec file paths that are to be run
+     * @param {object}         browser      instance of created browser/device session
+     */
+    // before: function (capabilities, specs) {
+    // },
+    /**
+     * Runs before a WebdriverIO command gets executed.
+     * @param {string} commandName hook command name
+     * @param {Array} args arguments that command would receive
+     */
+    // beforeCommand: function (commandName, args) {
+    // },
+    /**
+     * Hook that gets executed before the suite starts
+     * @param {object} suite suite details
+     */
+    // beforeSuite: function (suite) {
+    // },
+    /**
+     * Function to be executed before a test (in Mocha/Jasmine) starts.
+     */
+    // beforeTest: function (test, context) {
+    // },
+    /**
+     * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
+     * beforeEach in Mocha)
+     */
+    // beforeHook: function (test, context, hookName) {
+    // },
+    /**
+     * Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
+     * afterEach in Mocha)
+     */
+    // afterHook: function (test, context, { error, result, duration, passed, retries }, hookName) {
+    // },
     /**
      * Function to be executed after a test (in Mocha/Jasmine only)
      * @param {object}  test             test object
@@ -50,5 +148,72 @@ exports.config = merge(profile.config, {
         if (!passed) {
             await browser.takeScreenshot();
         }
-    }
+    },
+
+    /**
+     * Hook that gets executed after the suite has ended
+     * @param {object} suite suite details
+     */
+    // afterSuite: function (suite) {
+    // },
+    /**
+     * Runs after a WebdriverIO command gets executed
+     * @param {string} commandName hook command name
+     * @param {Array} args arguments that command would receive
+     * @param {number} result 0 - command success, 1 - command error
+     * @param {object} error error object if any
+     */
+    // afterCommand: function (commandName, args, result, error) {
+    // },
+    /**
+     * Gets executed after all tests are done. You still have access to all global variables from
+     * the test.
+     * @param {number} result 0 - test pass, 1 - test fail
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that ran
+     */
+    // after: function (result, capabilities, specs) {
+    // },
+    /**
+     * Gets executed right after terminating the webdriver session.
+     * @param {object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that ran
+     */
+    // afterSession: function (config, capabilities, specs) {
+    // },
+    /**
+     * Gets executed after all workers got shut down and the process is about to exit. An error
+     * thrown in the onComplete hook will result in the test run failing.
+     * @param {object} exitCode 0 - success, 1 - fail
+     * @param {object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {<Object>} results object containing test results
+     */
+    // onComplete: function(exitCode, config, capabilities, results) {
+    // },
+    /**
+    * Gets executed when a refresh happens.
+    * @param {string} oldSessionId session ID of the old session
+    * @param {string} newSessionId session ID of the new session
+    */
+    // onReload: function(oldSessionId, newSessionId) {
+    // }
+    /**
+    * Hook that gets executed before a WebdriverIO assertion happens.
+    * @param {object} params information about the assertion to be executed
+    */
+    // beforeAssertion: function(params) {
+    // }
+    /**
+    * Hook that gets executed after a WebdriverIO assertion happened.
+    * @param {object} params information about the assertion that was executed, including its results
+    */
+    // afterAssertion: function(params) {
+    // }
+
+    /*
+    onPrepare: function () {
+        console.log("Merged Configuration: ", JSON.stringify(this, null, 2));
+    }*/
 });
