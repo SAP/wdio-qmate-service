2c3431047c56deb82eccd43204325e8729cdebd6
sirikumarsap <siri.kumar.puttagunta@sap.com>
2024-10-23 18:26:33 +0530

    Initial reusable functions for Mobile Automation (#294)
    * Initial reusable functions for Mobile Native Automation
    
    * Review Comments are added
    
    * Changes as per review comments
    
    * Update browser.ts
    
    * Update test.local.android.device.conf.js
    
    * Change the isIOS method name to Apple standred iOS
    
    * Revert the test.local.android.device.conf.js
    
    * Revert
    
    * Revert -2
    
    * Revert the file
    
    * Delete test/reuse/mobile/element/test.local.android.device.conf.js
    
    * Delete test/reuse/mobile/element/isPresent.spec.js
    
    * Change in the package.json
    
    * Review comments are addressed in package.json
    
    * Review comments are addressed in package-lock.json
    
    * Review comments are addressed in package-lock.json
    
    * As part of review comments, changed the code
    
    * As part of review comments, changed the code in browser.ts
    
    * Change in the installApp  method
    
    * Adding few more useful string keyCode names
    
    * Adding few more useful string keyCode names
    
    * Adding Appium service into devDependencies
    
    * Reverting package-lock.json

---
+++ src/reuse/modules/mobile/android.ts
@@ -0,0 +1,91 @@
+import { Element } from "../../../../@types/wdio";
+import { VerboseLoggerFactory } from "../../helper/verboseLogger";
+import ErrorHandler from "../../helper/errorHandler";
+/**
+ * @class Android
+ * @memberof Mobile
+ */
+export class Android {
+  private vlf = new VerboseLoggerFactory("mobile", "android");
+  private ErrorHandler = new ErrorHandler();
+
+  /**
+   * @function pressKeyByName
+   * @memberOf mobile.android
+   * @description Simulate pressing a hardware key on the android device (e.g., back button, home button, etc.),
+   * @param {string} keyName - The name of the key (e.g., "back", "home", "volumeUp", etc.)
+   * @example
+   * await mobile.device.pressKeyByName("back");
+   * await mobile.device.pressKeyByName("home");
+   * await mobile.device.pressKeyByName("volumeUp");
+   * await mobile.device.pressKeyByName("volumeDown");
+   * await mobile.device.pressKeyByName("volumeMute");
+   * await mobile.device.pressKeyByName("power");
+   * await mobile.device.pressKeyByName("enter");
+   * await mobile.device.pressKeyByName("space");
+   * await mobile.device.pressKeyByName("delete");
+   * await mobile.device.pressKeyByName("menu");
+   * await mobile.device.pressKeyByName("search");
+   * await mobile.device.pressKeyByName("camera");
+   * await mobile.device.pressKeyByName("focus");
+   * await mobile.device.pressKeyByName("notification");
+   * await mobile.device.pressKeyByName("call");
+   * await mobile.device.pressKeyByName("calendar");
+   * await mobile.device.pressKeyByName("recent");
+   * await mobile.device.pressKeyByName("settings");
+   */
+  async pressKeyByName(keyName: string): Promise<void> {
+    const vl = this.vlf.initLog(this.pressKeyByName);
+
+    const keyCodeMap: { [key: string]: number } = {
+      back: 4,
+      home: 3,
+      volumeUp: 24,
+      volumeDown: 25,
+      VolumeMute: 164,
+      power: 26,
+      enter: 66,
+      space: 62,
+      delete: 67,
+      menu: 82,
+      search: 84,
+      camera: 27,
+      focus: 80,
+      notification: 83,
+      call: 5,
+      calendar: 208,
+      recent: 312,
+      settings: 176
+    };
+
+    try {
+      const keyCode = keyCodeMap[keyName.toLowerCase()];
+      if (keyCode !== undefined) {
+        vl.log(`Pressing key: ${keyName} (code: ${keyCode})`);
+        await browser.pressKeyCode(keyCode);
+      } else {
+        throw new Error(`Key code for "${keyName}" not found.`);
+      }
+    } catch (error) {
+      return this.ErrorHandler.logException(error, "Error: During pressKeyByName in Android", true);
+    }
+  }
+
+  /**
+   * @function pressKeyByCode
+   * @memberOf mobile.android
+   * @description Simulate pressing a hardware key on the android device (e.g., back button, home button, etc.),
+   * @param {string} keyCode - The code of the key (e.g., "back", "home", "volumeUp", etc.)
+   * @example
+   */
+  async pressKeyByCode(keyCode: Number): Promise<void> {
+    const vl = this.vlf.initLog(this.pressKeyByCode);
+    try {
+      vl.log(`Pressing key by code: ${keyCode}`);
+      await browser.pressKeyCode(keyCode);
+    } catch (error) {
+      return this.ErrorHandler.logException(error, "Error: During pressKeyByCode in Android", true);
+    }
+  }
+}
+export default new Android();
---
+++ src/reuse/modules/mobile/userInteraction.ts
@@ -0,0 +1,67 @@
+import { Element } from "../../../../@types/wdio";
+import { VerboseLoggerFactory } from "../../helper/verboseLogger";
+import ErrorHandler from "../../helper/errorHandler";
+/**
+ * @class userInteraction
+ * @memberof Mobile
+ */
+export class UserInteraction {
+  private vlf = new VerboseLoggerFactory("mobile", "UserInteraction");
+  private ErrorHandler = new ErrorHandler();
+
+  /**
+   * @function tap
+   * @memberOf mobile.userInteraction
+   * @description Tap's on the mobile element.
+   * @param {Element | string} element - The element or CSS selector describing the element.
+   * @param {Number} [timeout=30000] - The timeout to wait (ms).
+   * @example const elem = await mobile.userInteraction.tap(elem);
+   */
+  async tap(element: Element, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000) {
+    const vl = this.vlf.initLog(this.tap);
+
+    try {
+      vl.log("Expecting element to be displayed and enabled");
+      await Promise.all([
+        expect(element).toBeDisplayed({
+          wait: timeout,
+          interval: 100,
+          message: `Timeout '${+timeout / 1000}s' by waiting for element is displayed.`
+        }),
+        expect(element).toBeEnabled({
+          wait: timeout,
+          interval: 100,
+          message: `Timeout '${+timeout / 1000}s' by waiting for element is enabled.`
+        })
+      ]);
+      vl.log("Clicking the element");
+      await element.click();
+      vl.log("Given element is successfully taped on the mobile Ui");
+    } catch (error) {
+      this.ErrorHandler.logException(error);
+    }
+  }
+
+  /**
+   * @function check
+   * @memberOf mobile.userInteraction
+   * @description Checks the given checkbox.
+   * @param {Element} element - The element or CSS selector describing the element.
+   * @example await mobile.userInteraction.check(selector);
+   */
+  async check(element: Element) {
+    const vl = this.vlf.initLog(this.check);
+
+    try {
+      const isSelected: boolean = await mobile.element.isSelected(element);
+      if (!isSelected) {
+        await this.tap(element);
+      } else {
+        vl.log("Checkbox already selected.");
+      }
+    } catch (error) {
+      this.ErrorHandler.logException(error);
+    }
+  }
+}
+export default new UserInteraction();
