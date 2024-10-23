42c6dc52f4d03371518642d465098452bbf2c373
I503232 <benjamin.warth@sap.com>
2024-10-23 15:40:36 +0200

    fix eslint

--- test/helper/configurations/mobile.conf.js
+++ test/helper/configurations/mobile.conf.js
@@ -2,14 +2,15 @@ const merge = require("deepmerge");
 const baseConfig = require("./base.conf.js");
 
 exports.config = merge(baseConfig.config, {
-
   port: 4723,
 
   services: [
-    ['appium', {
-      logPath: './logs',
-      logFileName: 'qmate-appium.log',
-    }]
+    [
+      "appium",
+      {
+        logPath: "./logs",
+        logFileName: "qmate-appium.log"
+      }
+    ]
   ]
-
 });
