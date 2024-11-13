f18b1a19ceec063eaa5cf5075bfa7107ccacfd38
D056896 <marvin.gruessinger@sap.com>
2024-11-13 14:12:26 +0100

    add meaningful constant name for test

--- test/reuse/util/userSettings/setDateFormatFromUserSettings.spec.js
+++ test/reuse/util/userSettings/setDateFormatFromUserSettings.spec.js
@@ -1,6 +1,8 @@
 const data = require("./data/data.json");
 
 describe("userSettings", function () {
+  const DATE_FORMAT_LENGTH = 10;
+
   describe("setDateFormatFromUserSettings.spec - passing date format", function () {
 
     it("Preparation: Set systemUrl ", async function () {
@@ -12,21 +14,22 @@ describe("userSettings", function () {
       const date = await common.date.getToday("yyyy/mm/dd");
       const userDate = await common.date.getToday(process.env.USER_SETTINGS_DATE_FORMAT);
       //Ensure returned date matches 10 digits
-      common.assertion.expectEqual(date.length, 10);
-      common.assertion.expectEqual(userDate.length, 10);
+      common.assertion.expectEqual(date.length, DATE_FORMAT_LENGTH);
+      common.assertion.expectEqual(userDate.length, DATE_FORMAT_LENGTH);
       //Ensure returned date is a valid date
       common.assertion.expectEqual(new Date(date), new Date(userDate));
     });
   });
 
   describe("setDateFormatFromUserSettings.spec - do not pass format, defaulted env(process.env.USER_SETTINGS_DATE_FORMAT) is taken", function () {
+
     it("Execution & Verification ", async function () {
       await util.userSettings.setDateFormatFromUserSettings(util.data.decrypt(data.username), util.data.decrypt(data.password));
       const date = await common.date.getToday("yyyy/mm/dd");
       const userDate = await common.date.getToday();
       //Ensure returned date matches 10 digits
-      common.assertion.expectEqual(date.length, 10);
-      common.assertion.expectEqual(userDate.length, 10);
+      common.assertion.expectEqual(date.length, DATE_FORMAT_LENGTH);
+      common.assertion.expectEqual(userDate.length, DATE_FORMAT_LENGTH);
       //Ensure returned date is a valid date
       common.assertion.expectEqual(new Date(date), new Date(userDate));
     });
