ab6ba61591d7b6455ea92151686f5d75247938be
D056896 <marvin.gruessinger@sap.com>
2024-12-05 14:23:04 +0100

    docs: add documentation for userSettings module

--- src/reuse/modules/flp/userSettings.ts
+++ src/reuse/modules/flp/userSettings.ts
@@ -4,16 +4,16 @@ import { VerboseLoggerFactory } from "../../helper/verboseLogger";
 
 /**
  * @class userSettings
- * @memberof util
+ * @memberof flp
  */
 
 export class UserSettings {
   private vlf = new VerboseLoggerFactory("util", "userSettings");
   private _srvInstance = null;
 
-  private async initS4UserSettingService(user: string, password: string): Promise<any> {
+  private async _initS4UserSettingService(user: string, password: string): Promise<any> {
     if (!this._srvInstance) {
-      const vl = this.vlf.initLog(await this.initS4UserSettingService);
+      const vl = this.vlf.initLog(await this._initS4UserSettingService);
       const params = browser.config.params;
       if (params?.systemUrl) {
         try {
@@ -31,30 +31,65 @@ export class UserSettings {
     }
   }
 
+  /**
+   * @function setLanguageFromUserSettings
+   * @memberOf flp.userSettings
+   * @description Sets the environment variable 'USER_SETTINGS_LANG_KEY' language from user settings.
+   * @param {String} user - The username for authentication.
+   * @param {String} password - The password for authentication.
+   * @example await flp.userSettings.setLanguageFromUserSettings("user", "password");
+   * @returns {Promise<void>} A promise that resolves when the language has been set.
+   */
   public async setLanguageFromUserSettings(user: string, password: string) {
     const vl = this.vlf.initLog(this.setLanguageFromUserSettings);
-    await this.initS4UserSettingService(user, password);
+    await this._initS4UserSettingService(user, password);
     process.env.USER_SETTINGS_LANG_KEY = await this._getLanguageResponse(this._srvInstance);
     util.console.info(`Language Key: ${process.env.USER_SETTINGS_LANG_KEY} was set.`);
   }
 
+  /**
+   * @function setDateFormatFromUserSettings
+   * @memberOf flp.userSettings
+   * @description Sets the environment variable 'USER_SETTINGS_DATE_FORMAT' date format from user settings.
+   * @param {String} user - The username for authentication.
+   * @param {String} password - The password for authentication.
+   * @example await flp.userSettings.setDateFormatFromUserSettings("user", "password");
+   * @returns {Promise<void>} A promise that resolves when the date format has been set.
+   */
   public async setDateFormatFromUserSettings(user: string, password: string) {
     const vl = this.vlf.initLog(this.setDateFormatFromUserSettings);
-    await this.initS4UserSettingService(user, password);
+    await this._initS4UserSettingService(user, password);
     process.env.USER_SETTINGS_DATE_FORMAT = await this._getDateFormatsResponse(this._srvInstance); //removes: the whitespace characters 0-* and the brackets including the content of the brackets.
     util.console.info(`Date Format: ${process.env.USER_SETTINGS_DATE_FORMAT} was set.`);
   }
-
+  /**
+   * @function setDateFormatFromUserSettings
+   * @memberOf flp.userSettings
+   * @description Sets the environment variable 'USER_SETTINGS_DATE_FORMAT' date format from user settings.
+   * @param {String} user - The username for authentication.
+   * @param {String} password - The password for authentication.
+   * @example await flp.userSettings.setDateFormatFromUserSettings("user", "password");
+   * @returns {Promise<void>} A promise that resolves when the date format has been set.
+   */
   public async setTimeFormatFromUserSettings(user: string, password: string) {
     const vl = this.vlf.initLog(this.setTimeFormatFromUserSettings);
-    await this.initS4UserSettingService(user, password);
+    await this._initS4UserSettingService(user, password);
     process.env.USER_SETTINGS_TIME_FORMAT = await this._getTimeFormatResponse(this._srvInstance);
     util.console.info(`Time Format: ${process.env.USER_SETTINGS_TIME_FORMAT} was set.`);
   }
 
+  /**
+   * @function setTimeZoneFromUserSettings
+   * @memberOf flp.userSettings
+   * @description Sets the environment variable 'USER_SETTINGS_TIME_ZONE' time zone from user settings.
+   * @param {String} user - The username for authentication.
+   * @param {String} password - The password for authentication.
+   * @example await flp.userSettings.setTimeZoneFromUserSettings("user", "password");
+   * @returns {Promise<void>} A promise that resolves when the time zone has been set.
+   */
   public async setTimeZoneFromUserSettings(user: string, password: string) {
     const vl = this.vlf.initLog(this.setTimeZoneFromUserSettings);
-    await this.initS4UserSettingService(user, password);
+    await this._initS4UserSettingService(user, password);
     const res = await service.odata.get(this._srvInstance, "UserProfileProperties", { id: "TIME_ZONE", shellType: "FLP" });
     process.env.USER_SETTINGS_TIME_ZONE = await this._getTimeZoneResponse(this._srvInstance);
     util.console.info(`Time Zone: ${process.env.USER_SETTINGS_TIME_ZONE} was set.`);
@@ -62,7 +97,7 @@ export class UserSettings {
 
   public async setNumberFormatFromUserSettings(user: string, password: string) {
     const vl = this.vlf.initLog(this.setNumberFormatFromUserSettings);
-    await this.initS4UserSettingService(user, password);
+    await this._initS4UserSettingService(user, password);
     process.env.USER_SETTINGS_NUMBER_FORMAT = await this._getNumberFormatResponse(this._srvInstance);
     util.console.info(`Number Format: ${process.env.USER_SETTINGS_NUMBER_FORMAT} was set.`);
   }
@@ -70,7 +105,7 @@ export class UserSettings {
   public async setS4UserSettings(user: string, password: string) {
     const vl = this.vlf.initLog(this.setS4UserSettings);
     try {
-      await this.initS4UserSettingService(user, password);
+      await this._initS4UserSettingService(user, password);
       await this.setDateFormatFromUserSettings(user, password);
       await this.setLanguageFromUserSettings(user, password);
       await this.setNumberFormatFromUserSettings(user, password);
@@ -83,25 +118,25 @@ export class UserSettings {
 
   public async getLanguageFromUserSettings(user: string, password: string): Promise<string> {
     const vl = this.vlf.initLog(this.getLanguageFromUserSettings);
-    await this.initS4UserSettingService(user, password);
+    await this._initS4UserSettingService(user, password);
     return await this._getLanguageResponse(this._srvInstance);
   }
 
   public async getDateFormatFromUserSettings(user: string, password: string): Promise<string> {
     const vl = this.vlf.initLog(this.getDateFormatFromUserSettings);
-    await this.initS4UserSettingService(user, password);
+    await this._initS4UserSettingService(user, password);
     return await this._getDateFormatsResponse(this._srvInstance);
   }
 
   public async getTimeFormatFromUserSettings(user: string, password: string): Promise<string> {
     const vl = this.vlf.initLog(this.getTimeFormatFromUserSettings);
-    await this.initS4UserSettingService(user, password);
+    await this._initS4UserSettingService(user, password);
     return await this._getTimeFormatResponse(this._srvInstance);
   }
 
   public async getTimeZoneFromUserSettings(user: string, password: string): Promise<string> {
     const vl = this.vlf.initLog(this.getTimeZoneFromUserSettings);
-    await this.initS4UserSettingService(user, password);
+    await this._initS4UserSettingService(user, password);
     return await this._getTimeZoneResponse(this._srvInstance);
   }
 
