fb12b7b6356091356468908866afcd3711c06961
Siri Kumar Puttagunta <siri.kumar.puttagunta@sap.com>
2024-12-05 13:58:42 +0530

    Update src/reuse/modules/mobile/device.ts
    Co-authored-by: Uladzislau Dauhashei <v.dolgoshey@gmail.com>

--- src/reuse/modules/mobile/device.ts
+++ src/reuse/modules/mobile/device.ts
@@ -74,7 +74,7 @@ export class Device {
    * @memberof mobile.device
    * @description Switch to the specified( WEBVIEW | NATIVE_APP ) context if available.
    * @param {string} [targetContext='WEBVIEW'] The name of the target context.
-   * @param {number} timeout Maximum time to wait for the web context to appear (default: 5000ms).
+   * @param {number} [timeout=5000] Maximum time to wait for the web context to appear, milliseconds.
    * @returns {Promise<boolean>} Returns 'true' if the context is successfully switched, otherwise 'false'.
    * @example
    * await mobile.device.switchToContext();
