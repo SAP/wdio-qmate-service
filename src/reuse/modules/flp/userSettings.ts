bf0e04800065f8177b43303b8cdba1442e2894e0
D056896 <marvin.gruessinger@sap.com>
2024-12-06 10:18:38 +0100

    doc: complete documentation for every function

--- src/reuse/modules/flp/userSettings.ts
+++ src/reuse/modules/flp/userSettings.ts
@@ -11,7 +11,17 @@ export class UserSettings {
   private vlf = new VerboseLoggerFactory("util", "userSettings");
   private _srvInstance = null;
 
-  private async _initS4UserSettingService(user: string, password: string): Promise<any> {
+  /**
+   * @private
+   * @function _initS4UserSettingService
+   * @memberOf flp.userSettings
+   * @description Initializes the S4 User Setting Service.
+   * @param {String} user - The username for authentication.
+   * @param {String} password - The password for authentication.
+   * @example await flp.userSettings._initS4UserSettingService("user", "password");
+   * @returns {Promise<void>} A promise that resolves when the service has been initialized.
+   */
+  private async _initS4UserSettingService(user: string, password: string): Promise<void> {
     if (!this._srvInstance) {
       const vl = this.vlf.initLog(await this._initS4UserSettingService);
       const params = browser.config.params;
@@ -95,6 +105,15 @@ export class UserSettings {
     util.console.info(`Time Zone: ${process.env.USER_SETTINGS_TIME_ZONE} was set.`);
   }
 
+  /**
+   * @function setNumberFormatFromUserSettings
+   * @memberOf flp.userSettings
+   * @description Sets the environment variable 'USER_SETTINGS_NUMBER_FORMAT' number format from user settings.
+   * @param {String} user - The username for authentication.
+   * @param {String} password - The password for authentication.
+   * @example await flp.userSettings.setNumberFormatFromUserSettings("user", "password");
+   * @returns {Promise<void>} A promise that resolves when the number format has been set.
+   */
   public async setNumberFormatFromUserSettings(user: string, password: string) {
     const vl = this.vlf.initLog(this.setNumberFormatFromUserSettings);
     await this._initS4UserSettingService(user, password);
@@ -102,6 +121,15 @@ export class UserSettings {
     util.console.info(`Number Format: ${process.env.USER_SETTINGS_NUMBER_FORMAT} was set.`);
   }
 
+  /**
+   * @function setS4UserSettings
+   * @memberOf flp.userSettings
+   * @description Sets the user settings for S4.
+   * @param {String} user - The username for authentication.
+   * @param {String} password - The password for authentication.
+   * @example await flp.userSettings.setS4UserSettings("user", "password");
+   * @returns {Promise<void>} A promise that resolves when the user settings have been set.
+   */
   public async setS4UserSettings(user: string, password: string) {
     const vl = this.vlf.initLog(this.setS4UserSettings);
     try {
@@ -116,35 +144,90 @@ export class UserSettings {
     }
   }
 
+  /**
+   * @function getLanguageFromUserSettings
+   * @memberOf flp.userSettings
+   * @description Gets the language from user settings.
+   * @param {String} user - The username for authentication.
+   * @param {String} password - The password for authentication.
+   * @example const language = await flp.userSettings.getLanguageFromUserSettings("user", "password");
+   * @returns {Promise<String>} The language from user settings.
+   */
   public async getLanguageFromUserSettings(user: string, password: string): Promise<string> {
     const vl = this.vlf.initLog(this.getLanguageFromUserSettings);
     await this._initS4UserSettingService(user, password);
     return await this._getLanguageResponse(this._srvInstance);
   }
 
+  /**
+   * @function getDateFormatFromUserSettings
+   * @memberOf flp.userSettings
+   * @description Gets the date format from user settings.
+   * @param {String} user - The username for authentication.
+   * @param {String} password - The password for authentication.
+   * @example const dateFormat = await flp.userSettings.getDateFormatFromUserSettings("user", "password");
+   * @returns {Promise<String>} The date format from user settings.
+   */
   public async getDateFormatFromUserSettings(user: string, password: string): Promise<string> {
     const vl = this.vlf.initLog(this.getDateFormatFromUserSettings);
     await this._initS4UserSettingService(user, password);
     return await this._getDateFormatsResponse(this._srvInstance);
   }
 
+  /**
+   * @function getTimeFormatFromUserSettings
+   * @memberOf flp.userSettings
+   * @description Gets the time format from user settings.
+   * @param {String} user - The username for authentication.
+   * @param {String} password - The password for authentication.
+   * @example const timeFormat = await flp.userSettings.getTimeFormatFromUserSettings("user", "password");
+   * @returns {Promise<String>} The time format from user settings.
+   */
   public async getTimeFormatFromUserSettings(user: string, password: string): Promise<string> {
     const vl = this.vlf.initLog(this.getTimeFormatFromUserSettings);
     await this._initS4UserSettingService(user, password);
     return await this._getTimeFormatResponse(this._srvInstance);
   }
 
+  /**
+   * @function getTimeZoneFromUserSettings
+   * @memberOf flp.userSettings
+   * @description Gets the time zone from user settings.
+   * @param {String} user - The username for authentication.
+   * @param {String} password - The password for authentication.
+   * @example const timeZone = await flp.userSettings.getTimeZoneFromUserSettings("user", "password");
+   * @returns {Promise<String>} The time zone from user settings.
+   */
   public async getTimeZoneFromUserSettings(user: string, password: string): Promise<string> {
     const vl = this.vlf.initLog(this.getTimeZoneFromUserSettings);
     await this._initS4UserSettingService(user, password);
     return await this._getTimeZoneResponse(this._srvInstance);
   }
 
+  /**
+   * @function getNumberFormatFromUserSettings
+   * @memberOf flp.userSettings
+   * @description Gets the number format from user settings.
+   * @param {String} user - The username for authentication.
+   * @param {String} password - The password for authentication.
+   * @example const numberFormat = await flp.userSettings.getNumberFormatFromUserSettings("user", "password");
+   * @returns {Promise<String>} The number format from user settings.
+   */
   public async getNumberFormatFromUserSettings(user: string, password: string): Promise<string> {
     const vl = this.vlf.initLog(this.getNumberFormatFromUserSettings);
     return await this._getNumberFormatResponse(this._srvInstance);
   }
 
+  /**
+   * @private
+   * @function _getTimeZoneResponse
+   * @memberOf flp.userSettings
+   * @description Gets the time zone from user settings.
+   * @param {Object} srvInstance - The service instance.
+   * @example const timeZone = await flp.userSettings._getTimeZoneResponse(srvInstance);
+   * @returns {Promise<String>} The time zone from user settings.
+   * @throws {Error} Thrown if the time zone could not be retrieved.
+   */
   private async _getTimeZoneResponse(srvInstance: any): Promise<string> {
     try {
       const res = await service.odata.get(srvInstance, "UserProfileProperties", { id: "TIME_ZONE", shellType: "FLP" });
@@ -158,6 +241,16 @@ export class UserSettings {
     }
   }
 
+  /**
+   * @private
+   * @function _getTimeFormatResponse
+   * @memberOf flp.userSettings
+   * @description Gets the time format from user settings.
+   * @param {Object} srvInstance - The service instance.
+   * @example const timeFormat = await flp.userSettings._getTimeFormatResponse(srvInstance);
+   * @returns {Promise<String>} The time format from user settings.
+   * @throws {Error} Thrown if the time format could not be retrieved.
+   */
   private async _getTimeFormatResponse(srvInstance: any): Promise<string> {
     try {
       const res = await service.odata.get(srvInstance, "UserProfileProperties", { id: "TIME_FORMAT", shellType: "FLP" });
@@ -172,6 +265,16 @@ export class UserSettings {
     }
   }
 
+  /**
+   * @private
+   * @function _getDateFormatsResponse
+   * @memberOf flp.userSettings
+   * @description Gets the date format from user settings.
+   * @param {Object} srvInstance - The service instance.
+   * @example const dateFormat = await flp.userSettings._getDateFormatsResponse(srvInstance);
+   * @returns {Promise<String>} The date format from user settings.
+   * @throws {Error} Thrown if the date format could not be retrieved.
+   */
   private async _getDateFormatsResponse(srvInstance: any): Promise<string> {
     try {
       const res = await service.odata.get(srvInstance, "UserProfileProperties", { id: "DATE_FORMAT", shellType: "FLP" });
@@ -186,6 +289,16 @@ export class UserSettings {
     }
   }
 
+  /**
+   * @private
+   * @function _getLanguageResponse
+   * @memberOf flp.userSettings
+   * @description Gets the language from user settings.
+   * @param {Object} srvInstance - The service instance.
+   * @example const language = await flp.userSettings._getLanguageResponse(srvInstance);
+   * @returns {Promise<String>} The language from user settings.
+   * @throws {Error} Thrown if the language could not be retrieved.
+   */
   private async _getLanguageResponse(srvInstance: any): Promise<string> {
     try {
       const res = await service.odata.get(srvInstance, "UserProfileProperties", { id: "LANGUAGE", shellType: "FLP" });
@@ -199,6 +312,16 @@ export class UserSettings {
     }
   }
 
+  /**
+   * @private
+   * @function _getNumberFormatResponse
+   * @memberOf flp.userSettings
+   * @description Gets the number format from user settings.
+   * @param {Object} srvInstance - The service instance.
+   * @example const numberFormat = await flp.userSettings._getNumberFormatResponse(srvInstance);
+   * @returns {Promise<String>} The number format from user settings.
+   * @throws {Error} Thrown if the number format could not be retrieved.
+   */
   private async _getNumberFormatResponse(srvInstance: any): Promise<string> {
     try {
       const res = await service.odata.get(srvInstance, "UserProfileProperties", { id: "NUMBER_FORMAT", shellType: "FLP" });
