9556dfa2b500808005908904a5cf798c91661d35
iC4rds <dstaud2005@gmail.com>
2024-12-06 12:03:45 +0100

    fix: implement azure get git root

--- src/scripts/stats/getConfigurationInformation.ts
+++ src/scripts/stats/getConfigurationInformation.ts
@@ -2,6 +2,30 @@ import { execSync } from "child_process";
 import shajs from "sha.js";
 import path from "path";
 
+function azureGetGitRoot(): string {
+  if (process.env.BUILD_REPOSITORY_LOCALPATH) {
+    return process.env.BUILD_REPOSITORY_LOCALPATH
+  } else {
+    throw Error();
+  }
+} 
+
+function getGitRoot(): string {
+  try {
+    return execSync("git rev-parse --show-toplevel").toString().trim();
+  } catch (error) {
+    // Intentionally left blank
+  }
+
+  try {
+    return azureGetGitRoot()
+  } catch (error) {
+    // Intentionally left blank
+  }
+  
+  throw Error();
+} 
+
 export function getConfigurationHash(): string {
   const FALLBACK_NO_CONFIG_HASH = "FALLBACK_NO_CONFIG_HASH";
   const FALLBACK_NO_GIT_PATH = "FALLBACK_NO_GIT_PATH";
@@ -10,7 +34,7 @@ export function getConfigurationHash(): string {
   if (process.env.CONFIG_PATH) {
     const configPath = process.env.CONFIG_PATH;
     try {
-      const gitRoot = execSync("git rev-parse --show-toplevel").toString().trim();
+      const gitRoot = getGitRoot();
       const relativePath = path.relative(gitRoot, configPath);
       try {
         return shajs("sha256").update(relativePath).digest("hex");
--- src/scripts/stats/getRepositoryInformation.ts
+++ src/scripts/stats/getRepositoryInformation.ts
@@ -1,25 +1,36 @@
 import { execSync } from "child_process";
 import shajs from "sha.js";
 
-export function isCwdGitRepo(): boolean {
-  try {
-    execSync("git rev-parse --is-inside-work-tree", { stdio: "ignore" });
-    return true;
-  } catch (error) {
-    return false;
+function azureGetGitRemoteUrl(): string {
+  if (process.env.BUILD_REPOSITORY_URI) {
+    return process.env.BUILD_REPOSITORY_URI
+  } else {
+    throw Error();
   }
 }
 
+function getGitRemoteUrl(): string {
+  try {
+    return execSync("git config --get remote.origin.url").toString().trim();
+  } catch (error) {
+    // Intentionally left blank
+  }
+
+  try {
+    return azureGetGitRemoteUrl()
+  } catch (error) {
+    // Intentionally left blank
+  }
+
+  throw Error();
+}
+
 export function getCwdGitRemoteUrlHash(): string {
-  const FALLBACK_NO_GIT_REPOSITORY = "FALLBACK_NO_GIT_REPOSITORY";
   const FALLBACK_NO_GIT_ORIGIN_REMOTE = "FALLBACK_NO_GIT_ORIGIN_REMOTE";
   const FALLBACK_HASHING_FAILED = "FALLBACK_HASHING_FAILED";
 
-  if (!isCwdGitRepo()) {
-    return FALLBACK_NO_GIT_REPOSITORY;
-  }
   try {
-    const remoteUrl = execSync("git config --get remote.origin.url").toString().trim();
+    const remoteUrl = getGitRemoteUrl();
     try {
       const remoteUrlHash = shajs("sha256").update(remoteUrl).digest("hex");
       return remoteUrlHash;
