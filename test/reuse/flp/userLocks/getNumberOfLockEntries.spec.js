70343259e8fd6447eba974c543156a74d74e35b0
D056896 <marvin.gruessinger@sap.com>
2024-12-09 14:22:02 +0100

    test: add creating a lock on table in userLocks module test

--- test/reuse/flp/userLocks/getNumberOfLockEntries.spec.js
+++ test/reuse/flp/userLocks/getNumberOfLockEntries.spec.js
@@ -8,6 +8,7 @@ describe("userLocks", function () {
 
     it("Preparation: Set systemUrl ", async function () {
       browser.config.params.systemUrl = util.data.decrypt(data.systemUrl);
+      browser.config.baseUrl = util.data.decrypt(data.baseUrl);
       user = util.data.decrypt(data.username);
       pw = util.data.decrypt(data.password);
     });
