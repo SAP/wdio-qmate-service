19bda5d8a4d8c3b24d1a79637458be61ce9aa11e
iC4rds <dstaud2005@gmail.com>
2024-12-17 14:12:20 +0100

    refactor: format document

--- src/scripts/stats/updateUsage.ts
+++ src/scripts/stats/updateUsage.ts
@@ -1,6 +1,6 @@
 import { Agent, fetch } from "undici";
 
-export async function updateQmateUsage(id: string, usageData: { result: string}) {
+export async function updateQmateUsage(id: string, usageData: { result: string }) {
   const urlUsage = `https://stats.qmate.proc.only.sap/api/usage/qmate/${id}`;
   try {
     const response = await fetch(urlUsage, {
@@ -20,6 +20,6 @@ export async function updateQmateUsage(id: string, usageData: { result: string})
       // Intentionally ignore
     }
   } catch (error) {
-     // Intentionally ignore
+    // Intentionally ignore
   }
 }
