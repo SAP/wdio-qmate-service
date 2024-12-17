fd8ce7a9f3ab262dcb1441bc8b166cba3ae18080
Herald Maria Antony <herald.maria.antony@sap.com>
2024-12-17 17:23:58 +0530

    feat: migrate to webdriver v9, fix tests

--- test/core/functional/nonUi5/getWithDomElements.test.js
+++ test/core/functional/nonUi5/getWithDomElements.test.js
@@ -6,7 +6,7 @@ describe("webdriver.io page", function () {
 
   it("step1: check name is Accessories - use UI5 Control properties", async function () {
     const parent = await $("#container-cart---homeView--categoryList-listUl");
-    const elem = (await parent.$$('li[id*="homeView--categoryList"]').getElements())[0];
+    const elem = await parent.$$('li[id*="homeView--categoryList"]')[0].getElement();
     await browser.waitUntil(
       async () => {
         try {
@@ -28,7 +28,7 @@ describe("webdriver.io page", function () {
 
   it("step2: check name Accessories - use UI5 Aggregation Control properties", async function () {
     const parent = await $("#container-cart---homeView--categoryList-listUl");
-    const elem = (await parent.$$('li[id*="homeView--categoryList"]'))[0];
+    const elem = await parent.$$('li[id*="homeView--categoryList"]')[0].getElement();
     await browser.waitUntil(
       async () => {
         try {
@@ -50,8 +50,7 @@ describe("webdriver.io page", function () {
 
   it("step3: check element's Binding context path using chain $.$$ ", async function () {
     const parent = await $("#container-cart---homeView--categoryList-listUl");
-    const elems = (await parent.$$('li[id*="homeView--categoryList"]'));
-    const elem = elems[0];
+    const elem = await parent.$$('li[id*="homeView--categoryList"]')[0].getElement();
     await browser.waitUntil(
       async () => {
         try {
@@ -72,7 +71,7 @@ describe("webdriver.io page", function () {
 
   it("step4: step4: check Associations in categories - use chain $.$$ and UI5 Association Control properties ", async function () {
     const parent = await $("#container-cart---homeView--categoryList-listUl");
-    const elem = (await parent.$$('li[id*="homeView--categoryList"]'))[0];
+    const elem = await parent.$$('li[id*="homeView--categoryList"]')[0].getElement();
     await browser.waitUntil(
       async () => {
         try {
@@ -92,7 +91,7 @@ describe("webdriver.io page", function () {
 
   it("step5: get wrong Aggregation Control properties and throw error", async function () {
     const parent = await $("#container-cart---homeView--categoryList-listUl");
-    const elem = (await parent.$$('li[id*="homeView--categoryList"]'))[0];
+    const elem = await parent.$$('li[id*="homeView--categoryList"]')[0].getElement();
     await browser.waitUntil(
       async () => {
         try {
@@ -113,8 +112,7 @@ describe("webdriver.io page", function () {
 
   it("step6: check element's Binding context path using chain $.$$ and get wrong element property", async function () {
     const parent = await $("#container-cart---homeView--categoryList-listUl");
-    const elems = (await parent.$$('li[id*="homeView--categoryList"]'));
-    const elem = elems[0];
+    const elem = await parent.$$('li[id*="homeView--categoryList"]')[0].getElement();
     await browser.waitUntil(
       async () => {
         try {
