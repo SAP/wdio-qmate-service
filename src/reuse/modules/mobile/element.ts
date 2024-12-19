fe45622cbc07de98f6abfb92e00b1ecc89a8905c
Siri Kumar Puttagunta <siri.kumar.puttagunta@sap.com>
2024-12-19 19:11:32 +0530

    Adding few more lib methods for Mobile (#306)
    * Adding few more lib methods from Mobile side
    
    * Update src/reuse/modules/mobile/device.ts
    
    Co-authored-by: Uladzislau Dauhashei <v.dolgoshey@gmail.com>
    
    * Update src/reuse/modules/mobile/device.ts
    
    Co-authored-by: Uladzislau Dauhashei <v.dolgoshey@gmail.com>
    
    * Update src/reuse/modules/mobile/device.ts
    
    Co-authored-by: Uladzislau Dauhashei <v.dolgoshey@gmail.com>
    
    * Update src/reuse/modules/mobile/device.ts
    
    Co-authored-by: Uladzislau Dauhashei <v.dolgoshey@gmail.com>
    
    * Update src/reuse/modules/mobile/device.ts
    
    Co-authored-by: Uladzislau Dauhashei <v.dolgoshey@gmail.com>
    
    * Update src/reuse/modules/mobile/device.ts
    
    Co-authored-by: Uladzislau Dauhashei <v.dolgoshey@gmail.com>
    
    * Update src/reuse/modules/mobile/device.ts
    
    Co-authored-by: Uladzislau Dauhashei <v.dolgoshey@gmail.com>
    
    * Update src/reuse/modules/mobile/device.ts
    
    Co-authored-by: Benjamin Warth <60264179+DIFSRIP@users.noreply.github.com>
    
    * Update src/reuse/modules/mobile/userInteraction.ts
    
    Co-authored-by: Uladzislau Dauhashei <v.dolgoshey@gmail.com>
    
    * Update src/reuse/modules/mobile/userInteraction.ts
    
    Co-authored-by: Uladzislau Dauhashei <v.dolgoshey@gmail.com>
    
    * Update src/reuse/modules/mobile/userInteraction.ts
    
    Co-authored-by: Uladzislau Dauhashei <v.dolgoshey@gmail.com>
    
    * Addressed the review comments_1
    
    * Addressed the review comments for switchToContext without searching for the target context twice
    
    * Addressed the review comments and added new method 'isPlatformSupported'
    
    * Changed in description for few methods
    
    * Seperate  the switch context method
    
    * Addressing review comments
    
    * Changed as per the review comments
    
    * Updated the method name as 'getTargetContextIfAvailable'
    
    * Updated the method name as 'getTargetContextIfAvailable'
    
    * Updated the Exeption msg in method
    
    * Corrected the typo error for method name waitToBeEnabled
    
    * Corrected the typo error for method names in the comment as well
    
    * Change in the structure of the JSDoc description
    
    * Change the conditon to non-falsy value
    
    ---------
    
    Co-authored-by: Uladzislau Dauhashei <v.dolgoshey@gmail.com>
    Co-authored-by: Benjamin Warth <60264179+DIFSRIP@users.noreply.github.com>

--- src/reuse/modules/mobile/element.ts
+++ src/reuse/modules/mobile/element.ts
@@ -1,6 +1,7 @@
 import { Element } from "../../../../@types/wdio";
 import { VerboseLoggerFactory } from "../../helper/verboseLogger";
 import ErrorHandler from "../../helper/errorHandler";
+import { resolveMobileSelectorOrElement } from "../../helper/elementResolving";
 
 /**
  * @class element
@@ -14,11 +15,11 @@ export class ElementModule {
    * @function isVisible
    * @memberof mobile.element
    * @description Returns a boolean if the mobile element is visible to the user.
-   * @param {Object} element - The Mobile Ui element.
-   * @param {Boolean} [strict=true] - If strict mode is enabled it will only return "true" if the element is visible on the mobile view and within the viewport.
+   * @param {Element} element - The Mobile Ui element.
+   * @param {boolean} [strict=true] - If strict mode is enabled it will only return "true" if the element is visible on the mobile view and within the viewport.
    * If "false", it will be sufficient if the element is visible on the view but not inside the current viewport.
-   * @returns {Boolean} Returns true or false.
-   * @example const elem = await mobile.element.isVisible("button01");
+   * @returns {boolean} Returns true or false.
+   * @example
    * await mobile.element.isVisible(elem);
    */
   async isVisible(element: Element, strict: boolean = true): Promise<boolean> {
@@ -38,8 +39,8 @@ export class ElementModule {
    * @function isPresent
    * @memberof mobile.element
    * @description Returns a boolean if the element is present at the DOM or not. It might be hidden.
-   * @param {Object} elem - The element.
-   * @returns {Boolean} Returns true or false.
+   * @param {Element} element - The element.
+   * @returns {boolean} Returns true or false.
    * @example
    * await mobile.element.isPresent(elem);
    */
@@ -53,18 +54,26 @@ export class ElementModule {
    * @memberof mobile.element
    * @description Waits until the element with the given selector is present.
    * @param {Object} selector - The CSS selector describing the element.
-   * @param {Number} [timeout=30000] - The timeout to wait (ms).
-   * @example await mobile.element.waitToBePresent(".input01");
-   * @example await mobile.element.waitToBePresent("#button12");
-   * @example await mobile.element.waitToBePresent("p:first-child");
+   * @param {number} [timeout = 30000] - The timeout to wait (ms).
+   * @returns {boolean} Returns true or false.
+   * @example
+   * await mobile.element.waitToBePresent(".input01");
+   * await mobile.element.waitToBePresent("#button12");
+   * await mobile.element.waitToBePresent("p:first-child");
    */
-  async waitToBePresent(selector: any, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000): Promise<void> {
+  async waitToBePresent(selector: any, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000): Promise<boolean> {
     const vl = this.vlf.initLog(this.waitToBePresent);
     try {
       vl.log(`wdio.waitForExist invocation for selector ${selector}`);
-      await $(selector).waitForExist({ timeout: timeout });
+      await $(selector).waitForExist({
+        timeout: timeout,
+        interval: 100,
+        timeoutMsg: `Timeout '${+timeout / 1000}s' by waiting for element is present.`
+      });
+      return true;
     } catch (error) {
       this.ErrorHandler.logException(error);
+      return false;
     }
   }
 
@@ -73,18 +82,26 @@ export class ElementModule {
    * @memberof mobile.element
    * @description Waits until the element with the given selector is visible.
    * @param {Object} selector - The CSS selector describing the element.
-   * @param {Number} [timeout=30000] - The timeout to wait (ms).
-   * @example await mobile.element.waitToBeVisible(".input01");
-   * @example await mobile.element.waitToBeVisible("#button12");
-   * @example await mobile.element.waitToBeVisible("p:first-child");
+   * @param {number} [timeout=30000] - The timeout to wait (ms).
+   * @returns {boolean} Returns true or false.
+   * @example
+   * await mobile.element.waitToBeVisible(".input01");
+   * await mobile.element.waitToBeVisible("#button12");
+   * await mobile.element.waitToBeVisible("p:first-child");
    */
-  async waitToBeVisible(selector: any, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000) {
+  async waitToBeVisible(selector: any, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000): Promise<boolean> {
     const vl = this.vlf.initLog(this.waitToBeVisible);
     try {
       vl.log(`wdio.waitForDisplayed invocation for selector ${selector}`);
-      await $(selector).waitForDisplayed({ timeout: timeout });
+      await $(selector).waitForDisplayed({
+        timeout: timeout,
+        interval: 100,
+        timeoutMsg: `Timeout '${+timeout / 1000}s' by waiting for element is displayed.`
+      });
+      return true;
     } catch (error) {
       this.ErrorHandler.logException(error);
+      return false;
     }
   }
 
@@ -93,18 +110,26 @@ export class ElementModule {
    * @memberof mobile.element
    * @description Waits until the element with the given selector is clickable.
    * @param {Object} selector - The CSS selector describing the element.
-   * @param {Number} [timeout=30000] - The timeout to wait (ms).
-   * @example await mobile.element.waitToBeClickable(".input01");
-   * @example await mobile.element.waitToBeClickable("#button12");
-   * @example await mobile.element.waitToBeClickable("p:first-child");
+   * @param {number} [timeout=30000] - The timeout to wait (ms).
+   * @returns {boolean} Returns true or false.
+   * @example
+   * await mobile.element.waitToBeClickable(".input01");
+   * await mobile.element.waitToBeClickable("#button12");
+   * await mobile.element.waitToBeClickable("p:first-child");
    */
-  async waitToBeClickable(selector: any, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000) {
+  async waitToBeClickable(selector: any, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000): Promise<boolean> {
     const vl = this.vlf.initLog(this.waitToBeClickable);
     try {
       vl.log(`wdio.waitForClickable invocation for selector ${selector}`);
-      await $(selector).waitForClickable({ timeout: timeout });
+      await $(selector).waitForClickable({
+        timeout: timeout,
+        interval: 100,
+        timeoutMsg: `Timeout '${+timeout / 1000}s' by waiting for element is clickable.`
+      });
+      return true;
     } catch (error) {
       this.ErrorHandler.logException(error);
+      return false;
     }
   }
 
@@ -112,14 +137,44 @@ export class ElementModule {
    * @function isSelected
    * @memberof mobile.element
    * @description Returns a boolean if the element (e.g. checkbox) is selected.
-   * @param {Object} elem - The element.
-   * @returns {boolean}
-   * @example const elem = await mobile.element.getById("elem01");
+   * @param {Element | string} elementOrSelector - The element.
+   * @returns {boolean} Returns true or false.
+   * @example
    * const isSelected = await mobile.element.isSelected(elem);
    */
-  async isSelected(elem: Element): Promise<boolean> {
+  async isSelected(elementOrSelector: Element | string): Promise<boolean> {
     const vl = this.vlf.initLog(this.isSelected);
-    return elem.isSelected();
+
+    const element = await resolveMobileSelectorOrElement(elementOrSelector);
+    return await element.isSelected();
+  }
+
+  /**
+   * @function waitToBeEnabled
+   * @memberof mobile.element
+   * @description Waits until the element with the given selector is present.
+   * @param {Object} selector - The CSS selector describing the element.
+   * @param {number} [timeout=30000] - The timeout to wait (ms).
+   * @returns {boolean} Returns true or false.
+   * @example
+   * await mobile.element.waitToBeEnabled(".input01");
+   * await mobile.element.waitToBeEnabled("#button12");
+   * await mobile.element.waitToBeEnabled("p:first-child");
+   */
+  async waitToBeEnabled(selector: any, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000): Promise<boolean> {
+    const vl = this.vlf.initLog(this.waitToBeEnabled);
+    try {
+      vl.log(`wdio.waitTotoBeEnabled invocation for selector ${selector}`);
+      await $(selector).toBeEnabled({
+        timeout: timeout,
+        interval: 100,
+        timeoutMsg: `Timeout '${+timeout / 1000}s' by waiting for element is enabled.`
+      });
+      return true;
+    } catch (error) {
+      this.ErrorHandler.logException(error);
+      return false;
+    }
   }
 }
 export default new ElementModule();
--- src/reuse/modules/mobile/userInteraction.ts
+++ src/reuse/modules/mobile/userInteraction.ts
@@ -1,6 +1,7 @@
 import { Element } from "../../../../@types/wdio";
 import { VerboseLoggerFactory } from "../../helper/verboseLogger";
 import ErrorHandler from "../../helper/errorHandler";
+import { resolveMobileSelectorOrElement } from "../../helper/elementResolving";
 
 /**
  * @class userInteraction
@@ -14,32 +15,25 @@ export class UserInteraction {
    * @function tap
    * @memberof mobile.userInteraction
    * @description Tap's on the mobile element.
-   * @param {Element | string} element - The element or CSS selector describing the element.
-   * @param {Number} [timeout=30000] - The timeout to wait (ms).
-   * @example const elem = await mobile.userInteraction.tap(elem);
+   * @param {Element | string} elementOrSelector - The element (e.g., accessibility ID, XPath) selectors describing the element.
+   * @param {Number} [timeout = 30000] - The timeout to wait(ms)
+   * @example
+   * await mobile.userInteraction.tap(elem);
+   * await mobile.userInteraction.tap(elem, 20000);
    */
-  async tap(element: Element, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000) {
+  async tap(elementOrSelector: Element | string, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000): Promise<void> {
     const vl = this.vlf.initLog(this.tap);
 
     try {
-      vl.log("Expecting element to be displayed and enabled");
-      await Promise.all([
-        expect(element).toBeDisplayed({
-          wait: timeout,
-          interval: 100,
-          message: `Timeout '${+timeout / 1000}s' by waiting for element is displayed.`
-        }),
-        expect(element).toBeEnabled({
-          wait: timeout,
-          interval: 100,
-          message: `Timeout '${+timeout / 1000}s' by waiting for element is enabled.`
-        })
-      ]);
-      vl.log("Clicking the element");
+      const element = await resolveMobileSelectorOrElement(elementOrSelector);
+
+      vl.log("Waiting for the element to become enabled and visible within the specified timeout");
+      await Promise.all([mobile.element.waitToBeVisible(element, timeout), mobile.element.waitToBeEnabled(element, timeout)]);
+      vl.log("Tapping on the element");
       await element.click();
       vl.log("Given element is successfully taped on the mobile Ui");
     } catch (error) {
-      this.ErrorHandler.logException(error);
+      this.ErrorHandler.logException(error, `Error: element still not tapable after ${timeout} ms`, true);
     }
   }
 
@@ -47,21 +41,178 @@ export class UserInteraction {
    * @function check
    * @memberof mobile.userInteraction
    * @description Checks the given checkbox.
-   * @param {Element} element - The element or CSS selector describing the element.
-   * @example await mobile.userInteraction.check(selector);
+   * @param {Element | string} elementOrSelector - The element (e.g., accessibility ID, XPath) selectors describing the element.
+   * @param {number} [timeout = 30000] - The timeout to wait(ms)
+   * @example
+   * await mobile.userInteraction.check(element);
+   * await mobile.userInteraction.check(element, 20000);
    */
-  async check(element: Element) {
+  async check(elementOrSelector: Element | string, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000): Promise<void> {
     const vl = this.vlf.initLog(this.check);
 
     try {
+      const element = await resolveMobileSelectorOrElement(elementOrSelector);
+
+      vl.log("Waiting for the element to become enabled and visible within the specified timeout");
+      await Promise.all([mobile.element.waitToBeVisible(element, timeout), mobile.element.waitToBeEnabled(element, timeout)]);
       const isSelected: boolean = await mobile.element.isSelected(element);
       if (!isSelected) {
         await this.tap(element);
+        vl.log("Given element is successfully checked on the mobile Ui");
       } else {
         vl.log("Checkbox already selected.");
       }
     } catch (error) {
-      this.ErrorHandler.logException(error);
+      this.ErrorHandler.logException(error, `Error: element still not able to check after ${timeout} ms`, true);
+    }
+  }
+
+  /**
+   * @function uncheck
+   * @memberOf mobile.userInteraction
+   * @description Unchecks the given checkbox.
+   * @param {Element | string} elementOrSelector - The element (e.g., accessibility ID, XPath) selectors describing the element.
+   * @param {number} [timeout = 30000] - The timeout to wait(ms)
+   * @example
+   * await mobile.userInteraction.uncheck(elementOrSelector);
+   * await mobile.userInteraction.uncheck(elementOrSelector, 20000);
+   */
+  async uncheck(elementOrSelector: Element | string, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000): Promise<void> {
+    const vl = this.vlf.initLog(this.uncheck);
+
+    try {
+      const element = await resolveMobileSelectorOrElement(elementOrSelector);
+
+      vl.log("Waiting for the element to become enabled and visible within the specified timeout");
+      await Promise.all([mobile.element.waitToBeVisible(element, timeout), mobile.element.waitToBeEnabled(element, timeout)]);
+      const isSelected: boolean = await mobile.element.isSelected(element);
+      if (isSelected) {
+        await this.tap(element);
+        vl.log("Given element is successfully uncheck on the mobile Ui");
+      } else {
+        vl.log("Checkbox already unchecked.");
+      }
+    } catch (error) {
+      this.ErrorHandler.logException(error, `Error: element still not able to uncheck after ${timeout} ms`, true);
+    }
+  }
+
+  /**
+   * @function doubleTap
+   * @memberof mobile.userInteraction
+   * @description Double Tap's on the mobile element.
+   * @param {Element | string} elementOrSelector - The element (e.g., accessibility ID, XPath) selectors describing the element.
+   * @param {number} [timeout = 30000] - The timeout to wait(ms)
+   * @returns {Promise<void>}
+   * @example
+   * await mobile.userInteraction.doubleTap(elem);
+   * await mobile.userInteraction.doubleTap(elem, 2000);
+   */
+  async doubleTap(elementOrSelector: Element | string, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000): Promise<void> {
+    const vl = this.vlf.initLog(this.doubleTap);
+
+    try {
+      const element = await resolveMobileSelectorOrElement(elementOrSelector);
+
+      vl.log("Waiting for the element to become enabled and visible within the specified timeout");
+      await Promise.all([mobile.element.waitToBeVisible(element, timeout), mobile.element.waitToBeEnabled(element, timeout)]);
+      vl.log("Double taping on the element");
+      await element.doubleClick();
+      vl.log("Given element is successfully double taped on the mobile Ui");
+    } catch (error) {
+      this.ErrorHandler.logException(error, `Error: element still not able to double tap after ${timeout} ms`, true);
+    }
+  }
+
+  /**
+   * @function fill
+   * @memberof mobile.userInteraction
+   * @description Enter a string value into a mobile input field.
+   * @param {Element | string} elementOrSelector - The element (e.g., accessibility ID, XPath) selectors describing the element.
+   * @param {string} value - The string value to be entered.
+   * @param {number} [timeout = 30000] - The timeout to wait (ms).
+   * @returns {Promise<void>}
+   * @example
+   * await mobile.userInteraction.fill(element);
+   * await mobile.userInteraction.fill(element, 2000);
+   */
+  async fill(elementOrSelector: Element | string, value: string, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000): Promise<void> {
+    const vl = this.vlf.initLog(this.fill);
+
+    try {
+      const element = await resolveMobileSelectorOrElement(elementOrSelector);
+
+      vl.log("Waiting for the element to become enabled and visible within the specified timeout");
+      await Promise.all([mobile.element.waitToBeVisible(element, timeout), mobile.element.waitToBeEnabled(element, timeout)]);
+
+      // Set the value
+      vl.log("set text on the element");
+      await element.setValue(value);
+      vl.log(`Entered value "${value}" into input box by selector "${element}`);
+    } catch (error) {
+      this.ErrorHandler.logException(error, `Error: element still not able to fill the value after ${timeout} ms`, true);
+    }
+  }
+
+  /**
+   * @function clearAndFill
+   * @memberof mobile.userInteraction
+   * @description Enter a string into the mobile input field; it will clear the box before submission.
+   * @param {Element | string} elementOrSelector - The element (e.g., accessibility ID, XPath) selectors describing the element.
+   * @param {string} value - The string value to be entered.
+   * @param {number} [timeout = 30000] - The timeout to wait (ms).
+   * @returns {Promise<void>}
+   * @example
+   * await mobile.userInteraction.clearAndFill(element);
+   * await mobile.userInteraction.clearAndFill(element, 2000);
+   */
+  async clearAndFill(elementOrSelector: Element | string, value: string, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000): Promise<void> {
+    const vl = this.vlf.initLog(this.clearAndFill);
+
+    try {
+      const element = await resolveMobileSelectorOrElement(elementOrSelector);
+
+      vl.log("Waiting for the element to become enabled and visible within the specified timeout");
+      await Promise.all([mobile.element.waitToBeVisible(element, timeout), mobile.element.waitToBeEnabled(element, timeout)]);
+
+      // Clear the input box.
+      await element.clearValue();
+      vl.log("Clear the existing text on the given element");
+
+      // Set the value
+      vl.log("set value on the input element");
+      await element.setValue(value);
+      vl.log(`Entered value "${value}" into input box by selector "${element}`);
+    } catch (error) {
+      this.ErrorHandler.logException(error, `Error: element still not able to fill the value after ${timeout} ms`, true);
+    }
+  }
+
+  /**
+   * @function clear
+   * @memberof mobile.userInteraction
+   * @description Clear a string value into a mobile input field.
+   * @param {Element | string} elementOrSelector - The element (e.g., accessibility ID, XPath) selectors describing the element.
+   * @param {number} [timeout = 30000] - The timeout to wait (ms).
+   * @returns {Promise<void>}
+   * @example
+   * await mobile.userInteraction.clear(element);
+   * await mobile.userInteraction.clear(element, 2000);
+   */
+  async clear(elementOrSelector: Element | string, timeout: number = parseFloat(process.env.QMATE_CUSTOM_TIMEOUT!) || 30000): Promise<void> {
+    const vl = this.vlf.initLog(this.clear);
+
+    try {
+      const element = await resolveMobileSelectorOrElement(elementOrSelector);
+
+      vl.log("Waiting for the element to become enabled and visible within the specified timeout");
+      await Promise.all([mobile.element.waitToBeVisible(element, timeout), mobile.element.waitToBeEnabled(element, timeout)]);
+
+      vl.log("Clearing the existing text on the given element");
+      await element.clearValue();
+      vl.log("Cleared the existing text on the given element");
+    } catch (error) {
+      this.ErrorHandler.logException(error, `Error: element still not able to clear the value after ${timeout} ms`, true);
     }
   }
 }
