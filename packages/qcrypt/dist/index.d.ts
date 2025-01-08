20defb08dd7cd19dc80bc0938030f408bd657468
I503232 <benjamin.warth@sap.com>
2025-01-08 10:09:00 +0100

    refactor: use minified version of qcrypt

--- packages/qcrypt/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Encrypter from "./classes/Encrypter";
-import Decrypter from "./classes/Decrypter";
-import KeyGenerator from "./classes/KeyGenerator";
-export { Decrypter, Encrypter, KeyGenerator };
