ef1acd4382f99f4334f08d4446e744fe17586593
heraldantony <1908870+heraldantony@users.noreply.github.com>
2025-03-12 20:59:13 +0530

    fix: ui5.userInteraction.fill when index > 0 (#354)
    Co-authored-by: Herald Maria Antony <herald.maria.antony@sap.com>

--- test/reuse/ui5/userInteraction/fill.spec.js
+++ test/reuse/ui5/userInteraction/fill.spec.js
@@ -106,3 +106,32 @@ describe("userInteraction - fill element with empty value", function () {
       .rejects.toThrow("Function 'fill' failed with: Please provide an element and value(datatype - number/string) as arguments.");
   });
 });
+
+describe("userInteraction - fill element, index > 0", function () {
+
+  let value;
+  let actualValue;
+
+  it("Preparation", async function () {
+    await browser.navigateTo("https://sapui5.hana.ondemand.com/resources/sap/ui/documentation/sdk/index.html?sap-ui-xx-sample-id=sap.m.sample.InputAssisted&sap-ui-xx-sample-lib=sap.m&sap-ui-xx-sample-origin=.&sap-ui-xx-dk-origin=https://sapui5.hana.ondemand.com");
+    await handleCookiesConsent();
+  });
+
+  it("Execution", async function () {
+    const selector = {
+      "elementProperties": {
+        "viewName": "sap.m.sample.InputAssisted.V",
+        "metadata": "sap.m.Input"
+      }
+    };
+    value = "Track Mouse";
+    const index = 1;
+    const timeout = 30000;
+    await ui5.userInteraction.fill(selector, value, index, timeout);
+    actualValue = await ui5.element.getValue(selector, index, timeout);
+  });
+
+  it("Verification", function () {
+    common.assertion.expectEqual(actualValue, "Track Mouse");
+  });
+});
