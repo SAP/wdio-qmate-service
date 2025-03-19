b3cc4e52bdd1c7e2c21736acfb4a4644d95895e4
Uladzislau Dauhashei <v.dolgoshey@gmail.com>
2025-03-19 10:59:10 +0100

    test: add error case

--- test/reuse/ui5/assertion/expectCssPropertyValueToBe.spec.js
+++ test/reuse/ui5/assertion/expectCssPropertyValueToBe.spec.js
@@ -4,7 +4,7 @@ const { handleCookiesConsent } = require("../../../helper/utils");
 describe("assertion - expectCssPropertyValueToBe", function () {
   it("Preparation", async function () {
     await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/1.96.27/test-resources/sap/m/demokit/cart/webapp/index.html?sap-ui-theme=sap_fiori_3#/categories");
-    // await handleCookiesConsent();
+    await handleCookiesConsent();
   });
 
   it("Execution and Verification", async function () {
@@ -16,5 +16,24 @@ describe("assertion - expectCssPropertyValueToBe", function () {
       }
     };
     await ui5.assertion.expectCssPropertyValueToBe(selector, "visibility", "visible");
+  });
+});
+
+describe("assertion - expectCssPropertyValueToBe - error", function () {
+  it("Preparation", async function () {
+    await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/1.96.27/test-resources/sap/m/demokit/cart/webapp/index.html?sap-ui-theme=sap_fiori_3#/categories");
+    await handleCookiesConsent();
+  });
+
+  it("Execution and Verification", async function () {
+    const selector = {
+      elementProperties: {
+        viewName: "sap.ui.demo.cart.view.Home",
+        metadata: "sap.m.StandardListItem",
+        bindingContextPath: "/ProductCategories*'LT')"
+      }
+    };
+    await expect(ui5.assertion.expectCssPropertyValueToBe(selector, "visibility", "wrong"))
+      .rejects.toThrow(/Expected.*wrong.*|Received.*visible/);
   });
 });
