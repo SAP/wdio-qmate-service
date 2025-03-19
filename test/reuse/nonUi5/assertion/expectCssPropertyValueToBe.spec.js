bb345f2b01b66b7235449ac074fd94644a77abbe
Uladzislau Dauhashei <v.dolgoshey@gmail.com>
2025-03-19 10:21:09 +0100

    test: add error cases

--- test/reuse/nonUi5/assertion/expectCssPropertyValueToBe.spec.js
+++ test/reuse/nonUi5/assertion/expectCssPropertyValueToBe.spec.js
@@ -4,11 +4,12 @@ const { handleCookiesConsent } = require("../../../helper/utils");
 describe("assertion - expectCssPropertyValueToBe - element", function () {
   it("Preparation", async function () {
     await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/1.96.27/test-resources/sap/m/demokit/cart/webapp/index.html?sap-ui-theme=sap_fiori_3#/categories");
-    // await handleCookiesConsent();
+    await handleCookiesConsent();
   });
 
   it("Execution and Verification", async function () {
-    const product = await nonUi5.element.getByXPath("//div[contains(text(),'Laptops')]");
+    const selector = "//div[contains(text(),'Laptops')]";
+    const product = await nonUi5.element.getByXPath(selector);
     await nonUi5.assertion.expectCssPropertyValueToBe(product, "visibility", "visible");
   });
 });
@@ -16,7 +17,7 @@ describe("assertion - expectCssPropertyValueToBe - element", function () {
 describe("assertion - expectCssPropertyValueToBe - selector", function () {
   it("Preparation", async function () {
     await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/1.96.27/test-resources/sap/m/demokit/cart/webapp/index.html?sap-ui-theme=sap_fiori_3#/categories");
-    // await handleCookiesConsent();
+    await handleCookiesConsent();
   });
 
   it("Execution and Verification", async function () {
@@ -25,6 +26,29 @@ describe("assertion - expectCssPropertyValueToBe - selector", function () {
   });
 });
 
-// fail case for element
+describe("assertion - expectCssPropertyValueToBe - element - error", function () {
+  it("Preparation", async function () {
+    await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/1.96.27/test-resources/sap/m/demokit/cart/webapp/index.html?sap-ui-theme=sap_fiori_3#/categories");
+    await handleCookiesConsent();
+  });
 
-// fail case for selector
+  it("Execution and Verification", async function () {
+    const selector = "//div[contains(text(),'Laptops')]";
+    const product = await nonUi5.element.getByXPath(selector);
+    await expect(nonUi5.assertion.expectCssPropertyValueToBe(product, "visibility", "wrong"))
+      .rejects.toThrow(/Expected.*wrong.*|Received.*visible/);
+  });
+});
+
+describe("assertion - expectCssPropertyValueToBe - selector - error", function () {
+  it("Preparation", async function () {
+    await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/1.96.27/test-resources/sap/m/demokit/cart/webapp/index.html?sap-ui-theme=sap_fiori_3#/categories");
+    await handleCookiesConsent();
+  });
+
+  it("Execution and Verification", async function () {
+    const selector = "//div[contains(text(),'Laptops')]";
+    await expect(nonUi5.assertion.expectCssPropertyValueToBe(selector, "visibility", "wrong"))
+      .rejects.toThrow(/Expected.*wrong.*|Received.*visible/);
+  });
+});
