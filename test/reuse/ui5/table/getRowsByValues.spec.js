7a6545b79295a5d184f0bea850a1aa4e1c0393c9
D056896 <marvin.gruessinger@sap.com>
2025-05-12 17:04:49 +0200

    add: skeleton for test 'getRowsByValues'

---
+++ test/reuse/ui5/table/getRowsByValues.spec.js
@@ -0,0 +1,59 @@
+// "use strict";
+// const {
+//   handleCookiesConsent
+// } = require("../../../helper/utils");
+
+
+describe("table - getRowsByValues - smartTable - single value as a String", function () {
+
+  it("Preparation", async function () {
+
+  });
+
+
+  it("Step 00: Execution ", async function () {
+
+  });
+
+
+  it("Step 00: Validation", async function () {
+
+  });
+
+  describe("table - getRowsByValues - smartTable - single value as an Array", function () {
+
+    it("Preparation", async function () {
+
+    });
+
+
+    it("Step 00: Execution ", async function () {
+
+    });
+
+
+    it("Step 00: Validation", async function () {
+
+    });
+
+  });
+
+  describe("table - getRowsByValues - smartTable - multiple values as an Array", function () {
+
+    it("Preparation", async function () {
+
+    });
+
+
+    it("Step 00: Execution ", async function () {
+
+    });
+
+
+    it("Step 00: Validation", async function () {
+
+    });
+
+  });
+
+});
