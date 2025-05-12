84e48a8a80ed9dee68eaf1de4296ae7fa1135c33
D056896 <marvin.gruessinger@sap.com>
2025-05-12 17:05:36 +0200

    style: add TODOs

--- test/reuse/ui5/table/getRowsByValues.spec.js
+++ test/reuse/ui5/table/getRowsByValues.spec.js
@@ -7,32 +7,37 @@
 describe("table - getRowsByValues - smartTable - single value as a String", function () {
 
   it("Preparation", async function () {
-
+    // TODO: 
   });
 
 
   it("Step 00: Execution ", async function () {
+    // TODO: 
 
   });
 
 
-  it("Step 00: Validation", async function () {
+  it("Step 00: Verification", async function () {
+    // TODO: 
 
   });
 
   describe("table - getRowsByValues - smartTable - single value as an Array", function () {
 
     it("Preparation", async function () {
+      // TODO: 
 
     });
 
 
     it("Step 00: Execution ", async function () {
+      // TODO: 
 
     });
 
 
-    it("Step 00: Validation", async function () {
+    it("Step 00: Verification", async function () {
+      // TODO: 
 
     });
 
@@ -41,16 +46,19 @@ describe("table - getRowsByValues - smartTable - single value as a String", func
   describe("table - getRowsByValues - smartTable - multiple values as an Array", function () {
 
     it("Preparation", async function () {
+      // TODO: 
 
     });
 
 
     it("Step 00: Execution ", async function () {
+      // TODO: 
 
     });
 
 
-    it("Step 00: Validation", async function () {
+    it("Step 00: Verification", async function () {
+      // TODO: 
 
     });
 
