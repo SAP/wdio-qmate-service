26dd0bbaa87ea7becc1a2aac11226b7e6c67b016
Benjamin Warth <60264179+DIFSRIP@users.noreply.github.com>
2025-05-14 10:49:15 +0200

    Table get total number of rows (#369)
    * feat: implement function to get total number of rows of ui5 table
    
    Co-authored-by: Arora, Umang <umang.arora+sap@sap.com>
    
    * test: add tests for getTotalNumberOfRows
    
    Co-authored-by: Arora, Umang <umang.arora+sap@sap.com>
    
    * feat: refactor number calculation based on nested selector
    
    Co-authored-by: Arora, Umang <umang.arora+sap@sap.com>
    
    * refactor: move the count logic to a helper function, return 0 if no cells found, add further test
    
    Co-authored-by: Arora, Umang <umang.arora+sap@sap.com>
    
    * refactor: move selector resolving to private helper function
    
    Co-authored-by: Arora, Umang <umang.arora+sap@sap.com>
    
    * docs: add jsdoc annotations
    
    * test: add disabled test for local testing
    
    ---------
    
    Co-authored-by: Arora, Umang <umang.arora+sap@sap.com>

--- test/reuse/ui5/table/test.table.conf.js
+++ test/reuse/ui5/table/test.table.conf.js
@@ -9,6 +9,7 @@ exports.config = merge(profile.config, {
   specs: [
     path.resolve(__dirname, "clickSettingsButton.spec.js"),
     path.resolve(__dirname, "sortColumnAscending.spec.js"),
-    path.resolve(__dirname, "sortColumnDescending.spec.js")
+    path.resolve(__dirname, "sortColumnDescending.spec.js"),
+    path.resolve(__dirname, "getTotalNumberOfRows.spec.js")
   ]
 });
