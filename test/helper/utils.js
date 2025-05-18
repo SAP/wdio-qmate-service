7e337ad7a0e1dc4d53ade411224e5c825b70ed13
Jan Werkmann <janwerkmann@gmail.com>
2025-05-18 15:33:03 +0200

    Update utils.js

--- test/helper/utils.js
+++ test/helper/utils.js
@@ -13,7 +13,7 @@ exports.handleCookiesConsent = async function handleCookiesConsent() {
     const newCookiesConsentDialog = "button[id='truste-consent-button']";
     await browser.waitUntil(
       async () => {
-        try{
+        try {
           await Promise.any([
             ui5.userInteraction.click(oldCookiesConsentDialog, 0, 0),
             nonUi5.userInteraction.click(newCookiesConsentDialog, 0, 0)
