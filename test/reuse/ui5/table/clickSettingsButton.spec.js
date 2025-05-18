40623a436c856a3aa46b4af9977d2cc642946293
I501549 <jan.werkmann@sap.com>
2025-05-19 01:12:25 +0200

    undo cookie click change

--- test/reuse/ui5/table/clickSettingsButton.spec.js
+++ test/reuse/ui5/table/clickSettingsButton.spec.js
@@ -38,7 +38,7 @@ describe("table - clickSettingsButton - smartTable with tableSelector", function
 
   it("Preparation", async function () {
     await browser.url("https://sapui5.hana.ondemand.com/#/entity/sap.ui.comp.smarttable.SmartTable/sample/sap.ui.comp.sample.smarttable.mtable");
-    // await handleCookiesConsent();
+    await handleCookiesConsent();
     await util.browser.switchToIframe("[id='sampleFrame']");
   });
 
