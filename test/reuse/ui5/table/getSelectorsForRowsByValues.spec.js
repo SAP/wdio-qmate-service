3c92473cfb3e783d33616fa55879705d4066969e
I503232 <benjamin.warth@sap.com>
2025-05-21 09:55:08 +0200

    test: adjust test for single execution

--- test/reuse/ui5/table/getSelectorsForRowsByValues.spec.js
+++ test/reuse/ui5/table/getSelectorsForRowsByValues.spec.js
@@ -1,9 +1,7 @@
 "use strict";
-const {
-  handleCookiesConsent
-} = require("../../../helper/utils");
+const { handleCookiesConsent } = require("../../../helper/utils");
 
-let rows;
+let rowSelectors;
 const tableSelector = {
   elementProperties: {
     viewName: "sap.ui.comp.sample.smarttable.mtable.SmartTable",
@@ -13,7 +11,6 @@ const tableSelector = {
 };
 
 describe("table - getSelectorsForRowsByValues - smartTable - single value as a String", function () {
-
   it("Preparation", async function () {
     await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/#/entity/sap.ui.comp.smarttable.SmartTable/sample/sap.ui.comp.sample.smarttable.mtable");
     await handleCookiesConsent();
@@ -22,126 +19,134 @@ describe("table - getSelectorsForRowsByValues - smartTable - single value as a S
 
   it("Execution", async function () {
     const customerNameValue = "HäuHoh Huch GmbH";
-    rows = await ui5.table.getSelectorsForRowsByValues(tableSelector, customerNameValue);
+    rowSelectors = await ui5.table.getSelectorsForRowsByValues(tableSelector, customerNameValue);
   });
-
 
   it("Verification", async function () {
     const selector = {
-      "elementProperties": {
-        "metadata": "sap.ui.comp.navpopover.SmartLink",
-        "viewName": "sap.ui.comp.sample.smarttable.mtable.SmartTable",
-        "text": [
+      elementProperties: {
+        metadata: "sap.ui.comp.navpopover.SmartLink",
+        viewName: "sap.ui.comp.sample.smarttable.mtable.SmartTable",
+        text: [
           {
-            "model": "",
-            "path": "Kunnr",
-            "value": "HAEU03D",
-            "type": "string"
+            model: "",
+            path: "Kunnr",
+            value: "HAEU03D",
+            type: "string"
           },
           {
-            "model": "",
-            "path": "Name1",
-            "value": "HäuHoh Huch GmbH",
-            "type": "string"
+            model: "",
+            path: "Name1",
+            value: "HäuHoh Huch GmbH",
+            type: "string"
           }
         ]
       },
-      ancestorProperties: rows[0].elementProperties
+      ancestorProperties: rowSelectors[0].elementProperties
     };
     await expect(ui5.element.getDisplayed(selector)).resolves.not.toThrow();
   });
 });
 
 describe("table - getSelectorsForRowsByValues - smartTable - single value as an Array", function () {
+  // it("Preparation", async function () {
+  //   await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/#/entity/sap.ui.comp.smarttable.SmartTable/sample/sap.ui.comp.sample.smarttable.mtable");
+  //   await handleCookiesConsent();
+  //   await util.browser.switchToIframe("[id='sampleFrame']");
+  // });
 
   it("Execution", async function () {
     const customerNameValue = ["ToMa SE"];
-    rows = await ui5.table.getSelectorsForRowsByValues(tableSelector, customerNameValue);
+    rowSelectors = await ui5.table.getSelectorsForRowsByValues(tableSelector, customerNameValue);
   });
-
 
   it("Verification", async function () {
     const selector = {
-      "elementProperties": {
-        "viewName": "sap.ui.comp.sample.smarttable.mtable.SmartTable",
-        "metadata": "sap.ui.comp.navpopover.SmartLink",
-        "text": [
+      elementProperties: {
+        viewName: "sap.ui.comp.sample.smarttable.mtable.SmartTable",
+        metadata: "sap.ui.comp.navpopover.SmartLink",
+        text: [
           {
-            "model": "",
-            "path": "Kunnr",
-            "value": "EMPLOYEE1",
-            "type": "string"
+            model: "",
+            path: "Kunnr",
+            value: "EMPLOYEE1",
+            type: "string"
           },
           {
-            "model": "",
-            "path": "Name1",
-            "value": "ToMa SE",
-            "type": "string"
+            model: "",
+            path: "Name1",
+            value: "ToMa SE",
+            type: "string"
           }
         ]
       },
-      ancestorProperties: rows[0].elementProperties
+      ancestorProperties: rowSelectors[0].elementProperties
     };
     await expect(ui5.element.getDisplayed(selector)).resolves.not.toThrow();
   });
-
 });
 
 describe("table - getSelectorsForRowsByValues - smartTable - multiple values as an Array, receiving multiple columns", function () {
+  // it("Preparation", async function () {
+  //   await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/#/entity/sap.ui.comp.smarttable.SmartTable/sample/sap.ui.comp.sample.smarttable.mtable");
+  //   await handleCookiesConsent();
+  //   await util.browser.switchToIframe("[id='sampleFrame']");
+  // });
 
   it("Execution", async function () {
     const customerNameValue = ["Elena KG"];
-    rows = await ui5.table.getSelectorsForRowsByValues(tableSelector, customerNameValue);
+    rowSelectors = await ui5.table.getSelectorsForRowsByValues(tableSelector, customerNameValue);
   });
-
 
   it("Verification", async function () {
     const selector1 = {
-      "elementProperties": {
-        "viewName": "sap.ui.comp.sample.smarttable.mtable.SmartTable",
-        "metadata": "sap.m.Text",
-        "text": [
+      elementProperties: {
+        viewName: "sap.ui.comp.sample.smarttable.mtable.SmartTable",
+        metadata: "sap.m.Text",
+        text: [
           {
-            "model": "",
-            "path": "Dmbtr",
-            "value": "-1500.00",
-            "type": "string"
+            model: "",
+            path: "Dmbtr",
+            value: "-1500.00",
+            type: "string"
           }
         ]
       },
-      ancestorProperties: rows[0].elementProperties
+      ancestorProperties: rowSelectors[0].elementProperties
     };
     const selector2 = {
-      "elementProperties": {
-        "viewName": "sap.ui.comp.sample.smarttable.mtable.SmartTable",
-        "metadata": "sap.m.Text",
-        "text": [
+      elementProperties: {
+        viewName: "sap.ui.comp.sample.smarttable.mtable.SmartTable",
+        metadata: "sap.m.Text",
+        text: [
           {
-            "model": "",
-            "path": "Dmbtr",
-            "value": "0.00",
-            "type": "string"
+            model: "",
+            path: "Dmbtr",
+            value: "0.00",
+            type: "string"
           }
         ]
       },
-      ancestorProperties: rows[1].elementProperties
+      ancestorProperties: rowSelectors[1].elementProperties
     };
     await expect(ui5.element.getDisplayed(selector1)).resolves.not.toThrow();
     await expect(ui5.element.getDisplayed(selector2)).resolves.not.toThrow();
   });
-
 });
 
-describe("table - getSelectorsForRowsByValues - smartTable - unhappy case - multiple values as an array, receiving no row(empty array)", function () {
+describe("table - getSelectorsForRowsByValues - smartTable - unhappy case - multiple values as an array, receiving no row (empty array)", function () {
+  // it("Preparation", async function () {
+  //   await common.navigation.navigateToUrl("https://sapui5.hana.ondemand.com/#/entity/sap.ui.comp.smarttable.SmartTable/sample/sap.ui.comp.sample.smarttable.mtable");
+  //   await handleCookiesConsent();
+  //   await util.browser.switchToIframe("[id='sampleFrame']");
+  // });
+
   it("Execution", async function () {
     const customerNameValue = ["Elena KG", "abcdef"];
-    rows = await ui5.table.getSelectorsForRowsByValues(tableSelector, customerNameValue);
+    rowSelectors = await ui5.table.getSelectorsForRowsByValues(tableSelector, customerNameValue);
   });
-
 
   it("Verification", async function () {
-    await common.assertion.expectEqual(rows.length, 0);
-
+    common.assertion.expectEqual(rowSelectors.length, 0);
   });
-
 });
