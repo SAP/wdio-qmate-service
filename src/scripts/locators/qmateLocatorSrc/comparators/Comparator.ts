8bed5b132036b06cc553a97cb12e3eb5dd2d093c
I501549 <jan.werkmann@sap.com>
2025-05-24 14:44:00 +0200

    fix tests

--- src/scripts/locators/qmateLocatorSrc/comparators/Comparator.ts
+++ src/scripts/locators/qmateLocatorSrc/comparators/Comparator.ts
@@ -6,8 +6,7 @@ export class Comparator {
   public static compareWithWildCard(sWild: string, value: any, toLowerCase: boolean = false): boolean {
     const strWild = toLowerCase ? Comparator.convertToString(sWild).trim().toLowerCase() : Comparator.convertToString(sWild).trim();
     const strValue = toLowerCase ? Comparator.convertToString(value).trim().toLowerCase() : Comparator.convertToString(value).trim();
-    const regexStr = "^" + strWild.replace(/[-\/\\^$+?.()|[\]{}]/g, '\\$&').replace(/\*/g, ".*") + "$";
-    const regex = new RegExp(regexStr);
-    return regex.test(strValue);
+    const regex = new RegExp("^" + strWild.replace(/[-\/\\^$+?.()|[\]{}]/g, '\\$&').replace(/\*/g, ".*") + "$");
+    return sWild === undefined || regex.test(strValue);
   }
 }
