bbdf857ff6b7ec20ff71587c697291b3c9476ebd
I503232 <benjamin.warth@sap.com>
2025-06-11 09:53:19 +0200

    refactor: refactor the build script

--- src/scripts/locators/qmateLocatorSrc/build.js
+++ src/scripts/locators/qmateLocatorSrc/build.js
@@ -1,33 +1,70 @@
+// Imports
 const { build } = require("esbuild");
 const prettier = require("prettier");
 const path = require("path");
+const fs = require("fs");
 
+// Configuration
 const sharedConfig = {
   entryPoints: [path.resolve(__dirname, "index.ts")],
   bundle: true,
   minify: false
 };
+const qmateLocatorPath = path.resolve(__dirname, "../qmateLocator.js");
+
+// Main Logic
 async function buildAsync(config) {
+  await _buildFile();
+
+  const fileContent = _readQmateLocatorFile();
+  const modifiedContent = _removeExportStatement(fileContent);
+  const wrappedContent = _wrapContent(modifiedContent);
+  const formatted = await _formatContent(wrappedContent);
+
+  _writeFile(formatted);
+}
+
+// Helper
+async function _buildFile() {
   await build({
     ...sharedConfig,
     platform: "browser",
     format: "esm",
-    outfile: path.resolve(__dirname, "../qmateLocator.js")
+    outfile: qmateLocatorPath
   });
-
-  // open file and wrap it in a function
-  const fs = require("fs");
-  const filedContent = fs.readFileSync(path.resolve(__dirname, "../qmateLocator.js"), "utf-8");
-  // remove the export statement "export {
-  //  locate
-  //};" at the end of the file
-  const exportRegex = /export\s+\{\s*locate\s*\};/;
-  const modifiedContent = filedContent.replace(exportRegex, "");
-  const wrappedContent = "/* eslint-disable no-undef */\n" + "/* eslint-disable no-console */\n" + "module.exports = { \n ui5All: function ui5All(ui5Selector, index, opt_parentElement) {\n" + modifiedContent + "\n" + "return locate(ui5Selector, index, opt_parentElement);\n" + "}\n}\n";
-  const options = await prettier.resolveConfig(path.resolve(__dirname, "../qmateLocator.js"));
-  const formatted = await prettier.format(wrappedContent, {
-    filepath: path.resolve(__dirname, "../qmateLocator.js")
-  });
-  fs.writeFileSync(path.resolve(__dirname, "../qmateLocator.js"), formatted);
 }
+
+function _readQmateLocatorFile() {
+  return fs.readFileSync(qmateLocatorPath, "utf-8");
+}
+
+function _removeExportStatement(fileContent) {
+  const exportRegex = /export\s+\{\s*locate\s*\};/;
+  const modifiedContent = fileContent.replace(exportRegex, "");
+  return modifiedContent;
+}
+
+function _wrapContent(fileContent) {
+  return `/* eslint-disable no-undef */
+/* eslint-disable no-console */
+module.exports = {
+  ui5All: function ui5All(ui5Selector, index, opt_parentElement) {
+    ${fileContent}
+    return locate(ui5Selector, index, opt_parentElement);
+  }
+}
+`;
+}
+
+async function _formatContent(wrappedContent) {
+  return await prettier.format(wrappedContent, {
+    filepath: qmateLocatorPath
+  });
+}
+
+function _writeFile(fileContent) {
+  fs.writeFileSync(qmateLocatorPath, fileContent);
+}
+
+// Call the build function
 buildAsync(sharedConfig);
