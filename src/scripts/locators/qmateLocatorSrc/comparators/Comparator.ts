9ca13fe2935cfb953388a767b8b5aa6b9fbfa1fc
D056896 <marvin.gruessinger@sap.com>
2025-07-01 15:52:53 +0200

    fix: wild cards did not match across new lines, adding 's-flag'

--- src/scripts/locators/qmateLocatorSrc/comparators/Comparator.ts
+++ src/scripts/locators/qmateLocatorSrc/comparators/Comparator.ts
@@ -3,7 +3,7 @@ export class Comparator {
   public static compareWithWildCard(sWild: string, value: any, toLowerCase: boolean = false): boolean {
     const strWild = toLowerCase ? Comparator.convertToString(sWild).trim().toLowerCase() : Comparator.convertToString(sWild).trim();
     const strValue = toLowerCase ? Comparator.convertToString(value).trim().toLowerCase() : Comparator.convertToString(value).trim();
-    const regex = new RegExp("^" + strWild.replace(/[-\/\\^$+?.()|[\]{}]/g, "\\$&").replace(/\*/g, ".*") + "$");
+    const regex = new RegExp("^" + strWild.replace(/[-\/\\^$+?.()|[\]{}]/g, "\\$&").replace(/\*/g, ".*") + "$", "s");
     return regex.test(strValue);
   }
 
