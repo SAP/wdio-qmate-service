d5de7ababe1850859f5213eb9474f8922cb5817e
Uladzislau Dauhashei <u.dauhashei@sap.com>
2025-07-29 10:26:56 +0200

    test: temporarily comment out passing specs

--- test/reuse/ui5/table/test.table.conf.js
+++ test/reuse/ui5/table/test.table.conf.js
@@ -7,20 +7,20 @@ exports.config = merge(profile.config, {
   specFileRetries: 2,
 
   specs: [
-    path.resolve(__dirname, "clickSettingsButton.spec.js"),
-    path.resolve(__dirname, "sortColumnAscending.spec.js"),
-    path.resolve(__dirname, "sortColumnDescending.spec.js"),
-    path.resolve(__dirname, "getTotalNumberOfRows.spec.js"),
-    path.resolve(__dirname, "getTotalNumberOfRowsByValues.spec.js"),
-    path.resolve(__dirname, "getSelectorForRowByIndex.spec.js"),
-    path.resolve(__dirname, "getSelectorsForRowsByValues.spec.js"),
-    path.resolve(__dirname, "getAllColumnValuesByName.spec.js"),
+    // path.resolve(__dirname, "clickSettingsButton.spec.js"),
+    // path.resolve(__dirname, "sortColumnAscending.spec.js"),
+    // path.resolve(__dirname, "sortColumnDescending.spec.js"),
+    // path.resolve(__dirname, "getTotalNumberOfRows.spec.js"),
+    // path.resolve(__dirname, "getTotalNumberOfRowsByValues.spec.js"),
+    // path.resolve(__dirname, "getSelectorForRowByIndex.spec.js"),
+    // path.resolve(__dirname, "getSelectorsForRowsByValues.spec.js"),
+    // path.resolve(__dirname, "getAllColumnValuesByName.spec.js"),
     path.resolve(__dirname, "openItemByIndex.spec.js"),
-    path.resolve(__dirname, "openItemByValues.spec.js"),
-    path.resolve(__dirname, "selectRowByIndex.spec.js"),
-    path.resolve(__dirname, "selectRowByValues.spec.js"),
-    path.resolve(__dirname, "selectAllRows.spec.js"),
-    path.resolve(__dirname, "deselectRowByIndex.spec.js"),
-    path.resolve(__dirname, "deselectAllRows.spec.js")
+    // path.resolve(__dirname, "openItemByValues.spec.js"),
+    // path.resolve(__dirname, "selectRowByIndex.spec.js"),
+    // path.resolve(__dirname, "selectRowByValues.spec.js"),
+    // path.resolve(__dirname, "selectAllRows.spec.js"),
+    // path.resolve(__dirname, "deselectRowByIndex.spec.js"),
+    // path.resolve(__dirname, "deselectAllRows.spec.js")
   ]
 });
