9c6a12bcc6757133ef89413fdbb42248d0135602
Uladzislau Dauhashei <u.dauhashei@sap.com>
2025-07-29 12:24:24 +0200

    test: reduce the run specs to only failed spec

--- test/reuse/ui5/table/openItemByIndex.spec.js
+++ test/reuse/ui5/table/openItemByIndex.spec.js
@@ -43,43 +43,43 @@ describe("table - openItemByIndex - smartTable - open first item", function () {
   });
 });
 
-describe("table - openItemByIndex - smartTable - open third item", function () {
+// describe("table - openItemByIndex - smartTable - open third item", function () {
 
-  it("Preparation", async function () {
-    const url = "https://sapui5.hana.ondemand.com/test-resources/sap/suite/ui/generic/template/demokit/demokit.html?responderOn=true&demoApp=sttasalesordertt#";
-    await common.navigation.navigateToUrl(url);
-    await util.browser.refresh();
-    await ui5.userInteraction.click(goButtonSelector);
-  });
+//   it("Preparation", async function () {
+//     const url = "https://sapui5.hana.ondemand.com/test-resources/sap/suite/ui/generic/template/demokit/demokit.html?responderOn=true&demoApp=sttasalesordertt#";
+//     await common.navigation.navigateToUrl(url);
+//     await util.browser.refresh();
+//     await ui5.userInteraction.click(goButtonSelector);
+//   });
 
-  it("Execution", async function () {
-    await ui5.table.openItemByIndex(smartTableSelector, 2);
-  });
+//   it("Execution", async function () {
+//     await ui5.table.openItemByIndex(smartTableSelector, 2);
+//   });
 
-  it("Verification", async function () {
-    const pageTitle = "500000007";
-    const selector = {
-      "elementProperties": {
-        "viewName": "sap.suite.ui.generic.template.ObjectPage.view.Details",
-        "metadata": "sap.uxap.ObjectPageHeader",
-        "id": "*objectPageHeader"
-      }
-    };
-    const objectTitleValue = await ui5.element.getPropertyValue(selector, "objectTitle");
-    expect(objectTitleValue).toEqual(pageTitle);
-  });
-});
+//   it("Verification", async function () {
+//     const pageTitle = "500000007";
+//     const selector = {
+//       "elementProperties": {
+//         "viewName": "sap.suite.ui.generic.template.ObjectPage.view.Details",
+//         "metadata": "sap.uxap.ObjectPageHeader",
+//         "id": "*objectPageHeader"
+//       }
+//     };
+//     const objectTitleValue = await ui5.element.getPropertyValue(selector, "objectTitle");
+//     expect(objectTitleValue).toEqual(pageTitle);
+//   });
+// });
 
-describe("table - openItemByIndex - smartTable - unhappy case - try to open not existing item", function () {
+// describe("table - openItemByIndex - smartTable - unhappy case - try to open not existing item", function () {
 
-  it("Preparation", async function () {
-    const url = "https://sapui5.hana.ondemand.com/test-resources/sap/suite/ui/generic/template/demokit/demokit.html?responderOn=true&demoApp=sttasalesordertt#";
-    await common.navigation.navigateToUrl(url);
-    await util.browser.refresh();
-    await ui5.userInteraction.click(goButtonSelector);
-  });
+//   it("Preparation", async function () {
+//     const url = "https://sapui5.hana.ondemand.com/test-resources/sap/suite/ui/generic/template/demokit/demokit.html?responderOn=true&demoApp=sttasalesordertt#";
+//     await common.navigation.navigateToUrl(url);
+//     await util.browser.refresh();
+//     await ui5.userInteraction.click(goButtonSelector);
+//   });
 
-  it("Execution & Verification", async function () {
-    await expect(ui5.table.openItemByIndex(smartTableSelector, 10)).rejects.toThrow(/No item found with index 10/);
-  });
-});
+//   it("Execution & Verification", async function () {
+//     await expect(ui5.table.openItemByIndex(smartTableSelector, 10)).rejects.toThrow(/No item found with index 10/);
+//   });
+// });
