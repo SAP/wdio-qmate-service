2a4e927ec439b9b4dc0cae277520945f1c034581
imaroz-lx <illia.maroz@leverx.com>
2026-04-02 11:37:04 +0400

    test: extend openItemByIndex tests

--- test/reuse/ui5/table/openItemByIndex.spec.js
+++ test/reuse/ui5/table/openItemByIndex.spec.js
@@ -15,7 +15,7 @@ const goButtonSelector = {
   }
 };
 
-describe("table - openItemByIndex - smartTable - open first item", function () {
+describe("table - openItemByIndex - byClick - smartTable - open first item", function () {
 
   it("Preparation", async function () {
     const url = `${BASE_URL}/test-resources/sap/suite/ui/generic/template/demokit/demokit.html?responderOn=true&demoApp=sttasalesordertt#`;
@@ -24,7 +24,7 @@ describe("table - openItemByIndex - smartTable - open first item", function () {
   });
 
   it("Execution", async function () {
-    await ui5.table.openItemByIndex(smartTableSelector, 0);
+    await ui5.table.openItemByIndex(smartTableSelector, 0, "byClick");
   });
 
   it("Verification", async function () {
@@ -42,7 +42,7 @@ describe("table - openItemByIndex - smartTable - open first item", function () {
   });
 });
 
-describe("table - openItemByIndex - smartTable - open third item", function () {
+describe("table - openItemByIndex - byClick - smartTable - open third item", function () {
 
   it("Preparation", async function () {
     const url = `${BASE_URL}/test-resources/sap/suite/ui/generic/template/demokit/demokit.html?responderOn=true&demoApp=sttasalesordertt#`;
@@ -52,7 +52,7 @@ describe("table - openItemByIndex - smartTable - open third item", function () {
   });
 
   it("Execution", async function () {
-    await ui5.table.openItemByIndex(smartTableSelector, 2);
+    await ui5.table.openItemByIndex(smartTableSelector, 2, "byClick");
   });
 
   it("Verification", async function () {
@@ -70,7 +70,7 @@ describe("table - openItemByIndex - smartTable - open third item", function () {
   });
 });
 
-describe("table - openItemByIndex - smartTable - unhappy case - try to open not existing item", function () {
+describe("table - openItemByIndex - byClick - smartTable - unhappy case - try to open not existing item", function () {
 
   it("Preparation", async function () {
     const url = `${BASE_URL}/test-resources/sap/suite/ui/generic/template/demokit/demokit.html?responderOn=true&demoApp=sttasalesordertt#`;
@@ -80,6 +80,144 @@ describe("table - openItemByIndex - smartTable - unhappy case - try to open not 
   });
 
   it("Execution & Verification", async function () {
-    await expect(ui5.table.openItemByIndex(smartTableSelector, 10)).rejects.toThrow(/No item found with index 10/);
+    await expect(ui5.table.openItemByIndex(smartTableSelector, 10, "byClick")).rejects.toThrow(/No item found with index 10/);
+  });
+});
+
+describe("table - openItemByIndex - byArrowIcon - smartTable - open first item", function () {
+
+  it("Preparation", async function () {
+    const url = `${BASE_URL}/test-resources/sap/suite/ui/generic/template/demokit/demokit.html?responderOn=true&demoApp=sttasalesordertt#`;
+    await common.navigation.navigateToUrl(url);
+    await ui5.userInteraction.click(goButtonSelector);
+  });
+
+  it("Execution", async function () {
+    await ui5.table.openItemByIndex(smartTableSelector, 0, "byArrowIcon");
+  });
+
+  it("Verification", async function () {
+    const pageTitle = "500000001";
+    const selector = {
+
+      "elementProperties": {
+        "viewName": "sap.suite.ui.generic.template.ObjectPage.view.Details",
+        "metadata": "sap.uxap.ObjectPageHeader",
+        "id": "*objectPageHeader"
+      }
+    };
+    const objectTitleValue = await ui5.element.getPropertyValue(selector, "objectTitle");
+    expect(objectTitleValue).toEqual(pageTitle);
+  });
+});
+
+describe("table - openItemByIndex - byArrowIcon - smartTable - open third item", function () {
+
+  it("Preparation", async function () {
+    const url = `${BASE_URL}/test-resources/sap/suite/ui/generic/template/demokit/demokit.html?responderOn=true&demoApp=sttasalesordertt#`;
+    await common.navigation.navigateToUrl(url);
+    await util.browser.refresh();
+    await ui5.userInteraction.click(goButtonSelector);
+  });
+
+  it("Execution", async function () {
+    await ui5.table.openItemByIndex(smartTableSelector, 2, "byArrowIcon");
+  });
+
+  it("Verification", async function () {
+    const pageTitle = "500000007";
+    const selector = {
+
+      "elementProperties": {
+        "viewName": "sap.suite.ui.generic.template.ObjectPage.view.Details",
+        "metadata": "sap.uxap.ObjectPageHeader",
+        "id": "*objectPageHeader"
+      }
+    };
+    const objectTitleValue = await ui5.element.getPropertyValue(selector, "objectTitle");
+    expect(objectTitleValue).toEqual(pageTitle);
+  });
+});
+
+describe("table - openItemByIndex - byArrowIcon - smartTable - unhappy case - try to open not existing item", function () {
+
+  it("Preparation", async function () {
+    const url = `${BASE_URL}/test-resources/sap/suite/ui/generic/template/demokit/demokit.html?responderOn=true&demoApp=sttasalesordertt#`;
+    await common.navigation.navigateToUrl(url);
+    await util.browser.refresh();
+    await ui5.userInteraction.click(goButtonSelector);
+  });
+
+  it("Execution & Verification", async function () {
+    await expect(ui5.table.openItemByIndex(smartTableSelector, 10, "byArrowIcon")).rejects.toThrow(/No item found with index 10/);
+  });
+});
+
+describe("table - openItemByIndex - byEvent - smartTable - open first item", function () {
+
+  it("Preparation", async function () {
+    const url = `${BASE_URL}/test-resources/sap/suite/ui/generic/template/demokit/demokit.html?responderOn=true&demoApp=sttasalesordertt#`;
+    await common.navigation.navigateToUrl(url);
+    await ui5.userInteraction.click(goButtonSelector);
+  });
+
+  it("Execution", async function () {
+    await ui5.table.openItemByIndex(smartTableSelector, 0, "byEvent");
+  });
+
+  it("Verification", async function () {
+    const pageTitle = "500000001";
+    const selector = {
+
+      "elementProperties": {
+        "viewName": "sap.suite.ui.generic.template.ObjectPage.view.Details",
+        "metadata": "sap.uxap.ObjectPageHeader",
+        "id": "*objectPageHeader"
+      }
+    };
+    const objectTitleValue = await ui5.element.getPropertyValue(selector, "objectTitle");
+    expect(objectTitleValue).toEqual(pageTitle);
+  });
+});
+
+describe("table - openItemByIndex - byEvent - smartTable - open third item", function () {
+
+  it("Preparation", async function () {
+    const url = `${BASE_URL}/test-resources/sap/suite/ui/generic/template/demokit/demokit.html?responderOn=true&demoApp=sttasalesordertt#`;
+    await common.navigation.navigateToUrl(url);
+    await util.browser.refresh();
+    await ui5.userInteraction.click(goButtonSelector);
+  });
+
+  it("Execution", async function () {
+    await ui5.table.openItemByIndex(smartTableSelector, 2, "byEvent");
+  });
+
+  it("Verification", async function () {
+    const pageTitle = "500000007";
+    const selector = {
+
+      "elementProperties": {
+        "viewName": "sap.suite.ui.generic.template.ObjectPage.view.Details",
+        "metadata": "sap.uxap.ObjectPageHeader",
+        "id": "*objectPageHeader"
+      }
+    };
+    const objectTitleValue = await ui5.element.getPropertyValue(selector, "objectTitle");
+    expect(objectTitleValue).toEqual(pageTitle);
+  });
+});
+
+describe("table - openItemByIndex - byEvent - smartTable - unhappy case - try to open not existing item", function () {
+
+  it("Preparation", async function () {
+    const url = `${BASE_URL}/test-resources/sap/suite/ui/generic/template/demokit/demokit.html?responderOn=true&demoApp=sttasalesordertt#`;
+    await common.navigation.navigateToUrl(url);
+    await util.browser.refresh();
+    await ui5.userInteraction.click(goButtonSelector);
+  });
+
+  it("Execution & Verification", async function () {
+    await expect(ui5.table.openItemByIndex(smartTableSelector, 10, "byEvent")).rejects.toThrow(/No item found with index 10/);
   });
 });
