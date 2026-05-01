2d2f8fd6ebfb9dacbdfed93f81c39846ac7d857b
imaroz-lx <illia.maroz@leverx.com>
2026-05-01 19:28:44 +0400

    feat: add ignore metadata list

---
+++ src/scripts/locators/qmateLocatorSrc/constants/IgnoredMetadataList.ts
@@ -0,0 +1,5 @@
+// List of sap elements metadata's that exists but actually not presented in the UI tree.
+// Elements with this metadata's should be ignored while filtering valid controls
+export const IGNORED_METADATA_LIST = [
+  "sap.ui.core.HTML"
+]
