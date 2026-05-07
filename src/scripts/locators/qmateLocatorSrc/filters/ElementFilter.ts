6ac72fd45bf5d16304c367d03aeaffad265685e8
imaroz-lx <illia.maroz@leverx.com>
2026-05-07 11:27:14 +0400

    style: apply format

--- src/scripts/locators/qmateLocatorSrc/filters/ElementFilter.ts
+++ src/scripts/locators/qmateLocatorSrc/filters/ElementFilter.ts
@@ -10,22 +10,22 @@ import { BaseFilter } from "./BaseFilter";
 import { FilterFactory } from "../utils/FilterFactory";
 import { Ui5Selector } from "../../../../reuse/modules/ui5/types/ui5.types";
 
-type Constructor<Class = {}> = new (...arg: any[]) => Class
-const filtersMap: {prop: keyof Ui5Selector, filter: Constructor<BaseFilter>}[] = [
-  {prop: "ancestorProperties", filter: AncestorFilter},
-  {prop: "descendantProperties", filter: DescendantFilter},
-  {prop: "siblingProperties", filter: SiblingFilter},
-  {prop: "parentProperties", filter: ParentFilter},
-  {prop: "childProperties", filter: ChildFilter},
-  {prop: "prevSiblingProperties", filter: PrevSiblingFilter},
-  {prop: "nextSiblingProperties", filter: NextSiblingFilter},
-]
+type Constructor<Class = {}> = new (...arg: any[]) => Class;
+const filtersMap: { prop: keyof Ui5Selector; filter: Constructor<BaseFilter> }[] = [
+  { prop: "ancestorProperties", filter: AncestorFilter },
+  { prop: "descendantProperties", filter: DescendantFilter },
+  { prop: "siblingProperties", filter: SiblingFilter },
+  { prop: "parentProperties", filter: ParentFilter },
+  { prop: "childProperties", filter: ChildFilter },
+  { prop: "prevSiblingProperties", filter: PrevSiblingFilter },
+  { prop: "nextSiblingProperties", filter: NextSiblingFilter }
+];
 
 export class ElementFilter extends BaseFilter {
   protected doFiltering(controls: UI5Control[]): UI5Control[] {
     const props = this.elementProperties as ElementProperties;
     let filteredControls = this.filterFactory.getInstance(PropertiesFilter, props).filter(controls);
-    for(const {prop, filter} of filtersMap) {
+    for (const { prop, filter } of filtersMap) {
       filteredControls = this.filterFactory.getInstance(filter, props[prop]).filter(filteredControls);
     }
     return filteredControls;
@@ -34,7 +34,7 @@ export class ElementFilter extends BaseFilter {
   public doCheckSingle(control: UI5Control): boolean {
     const props = this.elementProperties as ElementProperties;
     let pass = this.filterFactory.getInstance(PropertiesFilter, props).checkSingle(control);
-    for(const {prop, filter} of filtersMap) {
+    for (const { prop, filter } of filtersMap) {
       pass &&= this.filterFactory.getInstance(filter, props[prop]).checkSingle(control);
     }
     return pass;
@@ -44,7 +44,7 @@ export class ElementFilter extends BaseFilter {
     const filterFactory = new FilterFactory();
 
     let validUi5Controls = filterFactory.getInstance(ElementFilter, ui5Selector.elementProperties).filter(controls);
-    for(const {prop, filter} of filtersMap) {
+    for (const { prop, filter } of filtersMap) {
       validUi5Controls = filterFactory.getInstance(filter, ui5Selector[prop]).filter(validUi5Controls);
     }
     return validUi5Controls;
