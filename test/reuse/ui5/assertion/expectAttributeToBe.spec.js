ac8bad59ca7211ca8d30181f2d3e104db51cd373
imaroz-lx <illia.maroz@leverx.com>
2026-05-27 13:53:57 +0400

    Fix/ui5 assertion special chars (#565)
    * fix: normalize attribute value before compare
    
    * test: extend assertion tests
    
    * test: enable skipped tests
    
    * test: fix global variable reuse

--- test/reuse/ui5/assertion/expectAttributeToBe.spec.js
+++ test/reuse/ui5/assertion/expectAttributeToBe.spec.js
@@ -1,19 +1,19 @@
 "use strict";
-let selector;
 
 describe("assertion - expectAttributeToBe: title to be 'Laptops' (string)", function () {
+  const selector = {
+    elementProperties: {
+      viewName: "sap.ui.demo.cart.view.Home",
+      metadata: "sap.m.StandardListItem",
+      bindingContextPath: "/ProductCategories*'LT')"
+    }
+  };
+
   it("Preparation", async function () {
     await browser.url("#/categories");
   });
 
   it("Execution", async function () {
-    selector = {
-      elementProperties: {
-        viewName: "sap.ui.demo.cart.view.Home",
-        metadata: "sap.m.StandardListItem",
-        bindingContextPath: "/ProductCategories*'LT')"
-      }
-    };
   });
 
   it("Verification", async function () {
@@ -23,18 +23,19 @@ describe("assertion - expectAttributeToBe: title to be 'Laptops' (string)", func
 });
 
 describe("assertion - expectAttributeToBe wrong/null/undefined", function () {
+  const selector = {
+    elementProperties: {
+      viewName: "sap.ui.demo.cart.view.Home",
+      metadata: "sap.m.StandardListItem",
+      bindingContextPath: "/ProductCategories*'LT')"
+    }
+  };
+
   it("Preparation", async function () {
     await browser.url("#/categories");
   });
 
   it("Execution", async function () {
-    selector = {
-      elementProperties: {
-        viewName: "sap.ui.demo.cart.view.Home",
-        metadata: "sap.m.StandardListItem",
-        bindingContextPath: "/ProductCategories*'LT')"
-      }
-    };
   });
 
   it("Verification", async function () {
@@ -51,18 +52,19 @@ describe("assertion - expectAttributeToBe wrong/null/undefined", function () {
 });
 
 describe("assertion - expectAttributeToBe: 'visible' of the listItem to be true (boolean and as string)", function () {
+  const selector = {
+    elementProperties: {
+      viewName: "sap.ui.demo.cart.view.Home",
+      metadata: "sap.m.StandardListItem",
+      bindingContextPath: "/ProductCategories*'LT')"
+    }
+  };
+
   it("Preparation", async function () {
     await browser.url("#/categories");
   });
 
   it("Execution", async function () {
-    selector = {
-      elementProperties: {
-        viewName: "sap.ui.demo.cart.view.Home",
-        metadata: "sap.m.StandardListItem",
-        bindingContextPath: "/ProductCategories*'LT')"
-      }
-    };
   });
 
   it("Verification", async function () {
@@ -73,23 +75,66 @@ describe("assertion - expectAttributeToBe: 'visible' of the listItem to be true 
 });
 
 describe("assertion - expectAttributeToBe: 'busyIndicatorDelay' of the listItem to be 1000 (number and as string)", function () {
+  const selector = {
+    elementProperties: {
+      viewName: "sap.ui.demo.cart.view.Home",
+      metadata: "sap.m.StandardListItem",
+      bindingContextPath: "/ProductCategories*'LT')"
+    }
+  };
+
   it("Preparation", async function () {
     await browser.url("#/categories");
   });
 
   it("Execution", async function () {
-    selector = {
-      elementProperties: {
-        viewName: "sap.ui.demo.cart.view.Home",
-        metadata: "sap.m.StandardListItem",
-        bindingContextPath: "/ProductCategories*'LT')"
-      }
-    };
   });
   it("Verification", async function () {
     await ui5.assertion.expectAttributeToBe(selector, "busyIndicatorDelay", 1000);
     await ui5.assertion.expectAttributeToBe(selector, "busyIndicatorDelay", "1000");
     await ui5.assertion.expectAttributeToBe(selector, "busyIndicatorDelay", 1000, 0, 30000, 10000); // Test 'loadPropertyTimeout' parameter
+  });
+});
+
+describe("assertion - expectAttributeToBe: 'text' is a text with special spaces characters", function () {
+  const specialSpacesString = "-\u00A0-\u2002-\u2003-\u2007-\u2009-\u202F-";
+  const normalizedString = "- - - - - - -";
+  const selector = {
+    elementProperties: {
+      metadata: "sap.m.Title",
+      id: "container-cart---homeView--page-title"
+    }
+  };
+  
+  it("Preparation", async function () {
+    await browser.url("#/categories");
+    await browser.uiControls(selector);
+    await browser.execute(`sap.ui.getCore().byId("${selector.elementProperties.id}").setText("${specialSpacesString}")`);
+  });
+
+  it("Execution && Verification", async function () {
+    await ui5.assertion.expectAttributeToBe(selector, "text", normalizedString);
+  });
+});
+
+describe("assertion - expectAttributeToBe: 'text' is a text with invisible characters", function () {
+  const invisibleCharacters = "-\u200B-\u200C-\u200D-\uFEFF-";
+  const normalizedString = "-----";
+  const selector = {
+    elementProperties: {
+      metadata: "sap.m.Title",
+      id: "container-cart---homeView--page-title"
+    }
+  };
+  
+  it("Preparation", async function () {
+    await browser.url("#/categories");
+    await browser.uiControls(selector);
+    await browser.execute(`sap.ui.getCore().byId("${selector.elementProperties.id}").setText("${invisibleCharacters}")`);
+  });
+
+  it("Execution && Verification", async function () {
+    await ui5.assertion.expectAttributeToBe(selector, "text", normalizedString);
   });
 });
 
